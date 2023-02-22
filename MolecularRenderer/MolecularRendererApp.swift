cf8404dc2e3e13a8d66d91515c048bc4c07539cf
Philip Turner <philipturner.AR@gmail.com>
2023-02-22 16:30:22 -0500

    Add Xcode project

---
+++ MolecularRenderer/MolecularRendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  MolecularRendererApp.swift
+//  MolecularRenderer
+//
+//  Created by Philip Turner on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MolecularRendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
