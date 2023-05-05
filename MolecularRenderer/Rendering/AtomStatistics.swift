7e4515df011b32d15374ba8cf63312179fd400ec
Philip Turner <philipturner.AR@gmail.com>
2023-05-05 11:30:07 -0400

    Import official atomic radii and colors

--- MolecularRenderer/Rendering/AtomStatistics.swift
+++ MolecularRenderer/Rendering/AtomStatistics.swift
@@ -8,25 +8,59 @@
 import Metal
 import simd
 
+// Bondi (1964) convention expanded to more elements in 2009. Hydrogen and
+// lithium values are not lowered to (1.20 -> 1.10, 1.82 -> 1.81) because that
+// makes ethylene look wierd.
+// https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3658832
+// Larger than QuteMol:
+// https://github.com/zulman/qutemol/blob/master/src/AtomColor.cpp#L28
 let atomRadii: [Float16] = [
-  0, // 0
-  0.25, // 1
-  0, // 2
-  0, // 3
-  0, // 4
-  0, // 5
-  0.40, // 6
-]
+  0.00, // 0
+  1.20, // 1
+  1.40, // 2
+  1.82, // 3
+  1.53, // 4
+  1.92, // 5
+  1.70, // 6
+  1.55, // 7
+  1.52, // 8
+  1.47, // 9
+  1.54, // 10
+  2.27, // 11
+  1.73, // 12
+  1.84, // 13
+  2.10, // 14
+  1.80, // 15
+  1.80, // 16
+  1.75, // 17
+  1.88, // 18
+].map { $0 / 10 }
 
+// Jmol color scheme:
+// https://jmol.sourceforge.net/jscolors/#color_C
+// Also used by QuteMol:
+// https://github.com/zulman/qutemol/blob/master/src/AtomColor.cpp#L54
 let atomColors: [SIMD3<Float16>] = [
-  SIMD3(repeating: 0), // 0
-  SIMD3(0.900, 0.900, 0.900), // 1
-  SIMD3(repeating: 0), // 2
-  SIMD3(repeating: 0), // 3
-  SIMD3(repeating: 0), // 4
-  SIMD3(repeating: 0), // 5
-  SIMD3(0.300, 0.300, 0.300), // 6
-]
+  SIMD3(  0,   0,   0), // 0
+  SIMD3(255, 255, 255), // 1
+  SIMD3(217, 255, 255), // 2
+  SIMD3(204, 128, 255), // 3
+  SIMD3(194, 255,   0), // 4
+  SIMD3(255, 181, 181), // 5
+  SIMD3(144, 144, 144), // 6
+  SIMD3( 48,  80, 248), // 7
+  SIMD3(255,  13,  13), // 8
+  SIMD3(144, 224,  80), // 9
+  SIMD3(179, 227, 245), // 10
+  SIMD3(171,  92, 242), // 11
+  SIMD3(138, 255,   0), // 12
+  SIMD3(191, 166, 166), // 13
+  SIMD3(240, 200, 160), // 14
+  SIMD3(255, 128,   0), // 15
+  SIMD3(255, 255,  48), // 16
+  SIMD3( 31, 240,  31), // 17
+  SIMD3(128, 209, 227), // 18
+].map { $0 / 255 }
 
 struct AtomStatistics {
   private var data: SIMD4<Float16>
--- MolecularRenderer/Rendering/ExampleMolecules.swift
+++ MolecularRenderer/Rendering/ExampleMolecules.swift
@@ -8,11 +8,19 @@
 import Foundation
 
 struct ExampleMolecules {
-  static var ethylene: [Atom] {
-    let z_offset: Float = -2
+  // Structure sourced from:
+  // https://commons.wikimedia.org/wiki/File:Ethylene-CRC-MW-dimensions-2D-Vector.svg
+  static let ethylene: [Atom] = {
+    let z_offset: Float = -2 // -2
+    let c_offset_x: Float = 0.1339 / 2 // 0.20
+    let carbon_origins: [SIMD3<Float>] = [
+      SIMD3(-c_offset_x, 0, z_offset),
+      SIMD3(+c_offset_x, 0, z_offset),
+    ]
     
-    let h_offset_x: Float = 0.50
-    let h_offset_y: Float = 0.25
+    let angle: Float = (180 - 121.3) * .pi / 180
+    let h_offset_x: Float = 0.1087 * cos(angle) + c_offset_x // 0.50
+    let h_offset_y: Float = 0.1087 * sin(angle) // 0.25
     let hydrogen_origins: [SIMD3<Float>] = [
       SIMD3(-h_offset_x, -h_offset_y, z_offset),
       SIMD3(-h_offset_x, +h_offset_y, z_offset),
@@ -20,25 +28,19 @@ struct ExampleMolecules {
       SIMD3(+h_offset_x, +h_offset_y, z_offset),
     ]
     
-    let c_offset_x: Float = 0.20
-    let carbon_origins: [SIMD3<Float>] = [
-      SIMD3(-c_offset_x, 0, z_offset),
-      SIMD3(+c_offset_x, 0, z_offset),
-    ]
-    
     return hydrogen_origins.map {
       Atom(origin: $0, element: 1)
     } + carbon_origins.map {
       Atom(origin: $0, element: 6)
     }
-  }
+  }()
   
-  static var taggedEthylene: [Atom] {
+  static let taggedEthylene: [Atom] = {
     var atoms = ExampleMolecules.ethylene
     let firstHydrogen = atoms.firstIndex(where: { $0.element == 1 })!
     let firstCarbon = atoms.firstIndex(where: { $0.element == 6 })!
     atoms[firstHydrogen].flags = 0x1 | 0x2
     atoms[firstCarbon].flags = 0x1 | 0x2
     return atoms
-  }
+  }()
 }
