07f9671107bfb15111ca670d7b417a53a36e844b
Philip Turner <philipturner.AR@gmail.com>
2023-06-18 17:03:18 -0400

    More work on OpenMM bindings

--- Sources/MolecularRendererApp/Simulation/OpenMM.swift
+++ Sources/MolecularRendererApp/Simulation/OpenMM.swift
@@ -28,27 +28,49 @@ func initOpenMM() {
 
 // MARK: - OpenMM Swift Bindings
 
-// TODO: Extract this into the Swift module, rename the current "OpenMM" as
+// TODO: Extract this into a Swift module, rename the current "OpenMM" as
 // "COpenMM", and call the wrappers the new "OpenMM".
 
 class OpenMM_Object {
   var pointer: OpaquePointer
-  private var retain: Bool
+  internal var _retain: Bool
   
-  init(_ pointer: OpaquePointer?, retain: Bool = false) {
+  /// This initializer always takes ownership of the pointer.
+  init() {
+    fatalError("\(#function) not implemented.")
+  }
+  
+  /// This initializer does not take ownership of the pointer.
+  init(_ pointer: OpaquePointer?) {
     guard let pointer else {
       fatalError("OpenMM object pointer was null.")
     }
     self.pointer = pointer
-    self.retain = retain
+    self._retain = false
+  }
+  
+  /// Call this to take ownership of the object.
+  func retain() {
+    guard _retain == false else {
+      fatalError("Cannot have two owners at once.")
+    }
+    self._retain = true
+  }
+  
+  /// Call this when transferring ownership to OpenMM API that will destroy it.
+  func transfer() {
+    guard _retain else {
+      fatalError("Did not have ownership; cannot transfer ownership to OpenMM.")
+    }
+    _retain = false
   }
   
   class func destroy(_ pointer: OpaquePointer) {
-    fatalError("'destroy' not implemented.")
+    fatalError("\(#function) not implemented.")
   }
   
   deinit {
-    if retain {
+    if _retain {
       Self.destroy(pointer)
     }
   }
@@ -59,6 +81,26 @@ class OpenMM_Object {
 // Probably need something like @_exported import OpenMM_PsPerFs
 
 // MARK: - OpenMM Classes
+
+class OpenMM_Force: OpenMM_Object {
+  override class func destroy(_ pointer: OpaquePointer) {
+    OpenMM_Force_destroy(pointer)
+  }
+}
+
+class OpenMM_NonbondedForce: OpenMM_Force {
+  override init() {
+    guard let pointer = OpenMM_NonbondedForce_create() else {
+      fatalError("Could not initialize.")
+    }
+    super.init(pointer)
+    self.retain()
+  }
+  
+  override class func destroy(_ pointer: OpaquePointer) {
+    OpenMM_NonbondedForce_destroy(pointer)
+  }
+}
 
 class OpenMM_Platform: OpenMM_Object {
   override class func destroy(_ pointer: OpaquePointer) {
@@ -78,7 +120,15 @@ class OpenMM_Platform: OpenMM_Object {
     guard let _plugins else {
       return nil
     }
-    return OpenMM_StringArray(_plugins, retain: true)
+    let plugins = OpenMM_StringArray(_plugins)
+    plugins.retain()
+    return plugins
+  }
+}
+
+class OpenMM_State: OpenMM_Object {
+  override class func destroy(_ pointer: OpaquePointer) {
+    OpenMM_State_destroy(pointer)
   }
 }
 
@@ -93,10 +143,60 @@ class OpenMM_StringArray: OpenMM_Object {
   }
   
   subscript(index: Int) -> String {
-    let _element = OpenMM_StringArray_get(pointer, Int32(index))
-    guard let _element else {
-      fatalError("Index out of bounds.")
+    get {
+      let _element = OpenMM_StringArray_get(pointer, Int32(index))
+      guard let _element else {
+        fatalError("Index out of bounds.")
+      }
+      return String(cString: _element)
     }
-    return String(cString: _element)
+    // `set` not supported yet.
+  }
+}
+
+class OpenMM_System: OpenMM_Object {
+  override init() {
+    guard let pointer = OpenMM_System_create() else {
+      fatalError("Could not initialize.")
+    }
+    super.init(pointer)
+    self.retain()
+  }
+  
+  /// Transfer ownership of the `OpenMM_Force` to OpenMM before calling this.
+  // TODO: Function that adds the force.
+  
+  override class func destroy(_ pointer: OpaquePointer) {
+    OpenMM_System_destroy(pointer)
+  }
+}
+
+class OpenMM_Vec3Array: OpenMM_Object {
+  override class func destroy(_ pointer: OpaquePointer) {
+    OpenMM_Vec3Array_destroy(pointer)
+  }
+  
+  var size: Int {
+    let _size = OpenMM_Vec3Array_getSize(pointer)
+    return Int(_size)
+  }
+  
+  subscript(index: Int) -> SIMD3<Double> {
+    get {
+      let _element = OpenMM_Vec3Array_get(pointer, Int32(index))
+      guard let _element else {
+        fatalError("Index out of bounds.")
+      }
+      
+      // Cannot assume this is aligned to 4 x 8 bytes, so read each element
+      // separately. If this part becomes a bottleneck in the CPU code, we know
+      // how to fix it.
+      let _vector: OpenMM_Vec3 = _element.pointee
+      return SIMD3(_vector.x, _vector.y, _vector.z)
+    }
+    set {
+      let _vector = OpenMM_Vec3(x: newValue.x, y: newValue.y, z: newValue.z)
+      OpenMM_Vec3Array_set(pointer, Int32(index), _vector)
+    }
   }
 }
