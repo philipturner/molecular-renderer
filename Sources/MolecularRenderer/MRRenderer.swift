4217d9a3b512a0f4e38a40844910e8190ee5e985
Philip Turner <philipturner.AR@gmail.com>
2023-07-03 19:36:02 -0400

    BVH performance actually improved with macOS 14

--- Sources/MolecularRenderer/MRRenderer.swift
+++ Sources/MolecularRenderer/MRRenderer.swift
@@ -425,8 +425,8 @@ extension MRRenderer {
       [textures.color, textures.depth, textures.motion], range: 0..<3)
     
     // Dispatch an even number of threads (the shader will rearrange them).
-    let numThreadgroupsX = (upscaledSize.x + 15) / 16
-    let numThreadgroupsY = (upscaledSize.y + 15) / 16
+    let numThreadgroupsX = (intermediateSize.x + 15) / 16
+    let numThreadgroupsY = (intermediateSize.y + 15) / 16
     encoder.dispatchThreadgroups(
       MTLSizeMake(numThreadgroupsX, numThreadgroupsY, 1),
       threadsPerThreadgroup: MTLSizeMake(16, 16, 1))
