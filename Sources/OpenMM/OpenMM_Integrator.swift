80d869d60a53f8be703ee5ee10685018d73770c9
Philip Turner <philipturner.AR@gmail.com>
2023-08-04 16:41:19 -0400

    Swift bindings for OpenMM_CustomIntegrator

--- Sources/OpenMM/OpenMM_Integrator.swift
+++ Sources/OpenMM/OpenMM_Integrator.swift
@@ -26,6 +26,53 @@ public class OpenMM_Integrator: OpenMM_Object {
   }
 }
 
+public class OpenMM_CustomIntegrator: OpenMM_Integrator {
+  public convenience init(stepSize: Double) {
+    self.init(_openmm_create(stepSize, OpenMM_CustomIntegrator_create))
+    self.retain()
+  }
+  
+  public override class func destroy(_ pointer: OpaquePointer) {
+    OpenMM_CustomIntegrator_destroy(pointer)
+  }
+  
+  @discardableResult
+  public func addComputePerDof(
+    variable: String, expression: String
+  ) -> Int {
+    let index = OpenMM_CustomIntegrator_addComputePerDof(
+      pointer, variable, expression)
+    return Int(index)
+  }
+  
+  @discardableResult
+  public func addConstrainPositions() -> Int {
+    let index = OpenMM_CustomIntegrator_addConstrainPositions(pointer)
+    return Int(index)
+  }
+  
+  @discardableResult
+  public func addConstrainVelocities() -> Int {
+    let index = OpenMM_CustomIntegrator_addConstrainVelocities(pointer)
+    return Int(index)
+  }
+  
+  @discardableResult
+  public func addPerDofVariable(
+    name: String, initialValue: Double
+  ) -> Int {
+    let index = OpenMM_CustomIntegrator_addPerDofVariable(
+      pointer, name, initialValue)
+    return Int(index)
+  }
+  
+  @discardableResult
+  public func addUpdateContextState() -> Int {
+    let index = OpenMM_CustomIntegrator_addUpdateContextState(pointer)
+    return Int(index)
+  }
+}
+
 public class OpenMM_LangevinMiddleIntegrator: OpenMM_Integrator {
   public convenience init(
     temperature: Double, frictionCoeff: Double, stepSize: Double
