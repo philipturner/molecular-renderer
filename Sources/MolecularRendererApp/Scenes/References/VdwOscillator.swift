b65c5385be5cf2b98f037fdb065998c9f16c9609
Philip Turner <philipturner.AR@gmail.com>
2023-08-06 20:57:49 -0400

    Delete a ton of code

--- Sources/MolecularRendererApp/Scenes/References/VdwOscillator.swift
+++ Sources/MolecularRendererApp/Scenes/References/VdwOscillator.swift
@@ -15,19 +15,9 @@ import simd
 // lacked a thermostat, so simulations couldn't last more than a few 100 ps.
 
 struct VdwOscillator {
-//  var provider: OpenMM_AtomProvider
-//  var provider: ArrayAtomProvider
   var provider: any MRAtomProvider
   
   init() {
-    // Generate a cube, then cleave it along directions I want.
-    //
-    // Make a system that only uses (111) and (110) surfaces, filling in (100)
-    // surfaces with other things to avoid the need for surface reconstruction.
-    //
-    // Debug the geometry generator, get things in the shapes you want. After
-    // that's all debugged, send a final design through 'Diamondoid' and 'MM4'.
-    
     struct Plane {
       var origin: SIMD3<Float>
       var normal: SIMD3<Float>
@@ -132,19 +122,6 @@ struct VdwOscillator {
       }
     }
     
-    // Only using a cubic lattice for now.
-    let latticeWidth: Int = 16
-    var baseLattice: [Cell] = []
-    let baseCell = Cell()
-    for i in 0..<latticeWidth {
-      for j in 0..<latticeWidth {
-        for k in 0..<latticeWidth {
-          let offset = SIMD3(i, j, k)
-          baseLattice.append(baseCell.translated(offset: offset))
-        }
-      }
-    }
-    
     func makeCarbonCenters(cells: [Cell]) -> [SIMD3<Float>] {
       var dict: [SIMD3<Float>: Bool] = [:]
       for cell in cells {
@@ -170,120 +147,24 @@ struct VdwOscillator {
       }
     }
     
+    let latticeWidth: Int = 10
+    var baseLattice: [Cell] = []
+    let baseCell = Cell()
+    for i in 0..<latticeWidth {
+      for j in 0..<latticeWidth {
+        for k in 0..<latticeWidth {
+          let offset = SIMD3(i, j, k)
+          baseLattice.append(baseCell.translated(offset: offset))
+        }
+      }
+    }
+    
     var allCarbonCenters: [SIMD3<Float>] = []
     
-    // Not yet a plane, but able to be transformed into a plane.
-    var _100Removers: [[(SIMD3<Float>, SIMD3<Float>)]] = []
-    
-    // Start rewriting by systematically mutating different (100) planes of the
-    // cube.
-    do {
-      // This loop probably needs to extend to double the width, but check
-      // first.
-      for i in 0...latticeWidth * 4 {
-        let origin = SIMD3<Float>(0, Float(i) * 4, 0)
-        _100Removers.append([(origin, SIMD3(1, 1, 1)),
-                             (origin, SIMD3(1, -1, -1))])
-      }
-      for i in 0...latticeWidth * 4 {
-        let origin = SIMD3<Float>(0, 0, Float(i) * 4)
-        _100Removers.append([(origin, SIMD3(1, 1, -1)),
-                             (origin, SIMD3(1, -1, 1))])
-      }
-    }
-    
-    func remove100(
-      cells: [Cell],
-      planes: [Plane],
-      axis: Int,
-      locationOnAxis: Float,
-      positiveOnAxis: Bool
-    ) -> [Cell] {
-      precondition(axis >= 0 && axis <= 2, "Invalid axis.")
-      
-      var allRemovers: [[Plane]] = []
-      for var _100Remover in _100Removers {
-        var removers: [Plane] = []
-        for i in 0..<2 {
-          _100Remover[i].0.x = locationOnAxis
-          if !positiveOnAxis {
-            _100Remover[i].0.x.negate()
-          }
-          
-          func permuteVector(_ vector: SIMD3<Float>) -> SIMD3<Float> {
-            if axis == 0 {
-              return vector
-            } else if axis == 1 {
-              return SIMD3(vector.y, vector.x, vector.z)
-            } else {
-              return SIMD3(vector.y, vector.z, vector.x)
-            }
-          }
-          _100Remover[i].0 = permuteVector(_100Remover[i].0)
-          _100Remover[i].1 = permuteVector(_100Remover[i].1)
-          removers.append(Plane(_100Remover[i].0, normal: _100Remover[i].1))
-        }
-        allRemovers.append(removers)
-      }
-      
-      var desiredLocationOnAxis = locationOnAxis
-      if !positiveOnAxis {
-        desiredLocationOnAxis -= 1
-        precondition(desiredLocationOnAxis >= 0)
-      }
-     
-      var output = cells
-      for i in cells.indices {
-        // Enable this optimization after debugging.
-        #if false
-        if Float(cells[i].offset[axis]) != desiredLocationOnAxis {
-          continue
-        }
-        #endif
-        
-//        for remover in [allRemovers[5], allRemovers[6]] {
-        for remover in allRemovers {
-          output[i] = output[i].cleaved(planes: remover)
-        }
-      }
-      return output.compactMap {
-        if $0.atoms.count > 0 {
-          return $0
-        } else {
-          return nil
-        }
-      }
-    }
-    
-    
+    // Make a housing, where a solid diamond brick can fit inside it.
     let start = CACurrentMediaTime()
     do {
       var cells = baseLattice
-      let latticeCenter: Float = Float(latticeWidth) / 2
-      let latticeLength: Float = Float(latticeWidth)
-      precondition(latticeCenter == rint(latticeCenter))
-      
-      let housingThickness: Float = 0.5
-      let innerYZ_left: Float = 2
-      let innerYZ_right: Float = 4
-      let outerYZ_left: Float = innerYZ_left + housingThickness
-      let outerYZ_right: Float = innerYZ_right + housingThickness
-      
-      let holeThicknessX: Float = 1
-      let holeThicknessYZ: Float = 1
-      let holeBoundsX: [SIMD2<Float>] = [
-        SIMD2(
-          0 + holeThicknessX,
-          latticeCenter - holeThicknessX / 2
-        ),
-        SIMD2(
-          latticeCenter + holeThicknessX / 2,
-          latticeLength - holeThicknessX
-        ),
-      ]
-      let holeYZ: Float = innerYZ_right - holeThicknessYZ
-      
-      cells = remove100(cells: cells, planes: [], axis: 0, locationOnAxis: 5.5, positiveOnAxis: true)
       allCarbonCenters += makeCarbonCenters(cells: cells)
     }
     let end = CACurrentMediaTime()
@@ -296,195 +177,14 @@ struct VdwOscillator {
       MRAtom(origin: $0 * 0.357, element: 6)
     }
     print(allAtoms.count)
-//    self.provider = ArrayAtomProvider(allAtoms)
+    self.provider = ArrayAtomProvider(allAtoms)
     
-    var diamondoid = Diamondoid(atoms: allAtoms)
-    print(diamondoid.atoms.count)
-    diamondoid.removeCloseHydrogens()
-    self.provider = ArrayAtomProvider(diamondoid.atoms)
+//    var diamondoid = Diamondoid(atoms: allAtoms)
+//    print(diamondoid.atoms.count)
+//    self.provider = ArrayAtomProvider(diamondoid.atoms)
 
 //    let simulator = MM4(diamondoid: diamondoid, fsPerFrame: 20)
 //    simulator.simulate(ps: 10)
 //    provider = simulator.provider
   }
 }
-
-#if false
-func makeTetrahedron() {
-  var cells = baseLattice
-  cells = cleave(cells: cells, planes: [
-    Plane(SIMD3(0, latticeWidth, latticeWidth), normal: SIMD3(1, 1, 1)),
-  ])
-  cells = cleave(cells: cells, planes: [
-    Plane(SIMD3(0, latticeWidth, latticeWidth), normal: SIMD3(-1, 1, -1)),
-  ])
-  cells = cleave(cells: cells, planes: [
-    Plane(SIMD3(0, 0, 0), normal: SIMD3(-1, -1, 1)),
-  ])
-  cells = cleave(cells: cells, planes: [
-    Plane(SIMD3(0, 0, 0), normal: SIMD3(1, -1, -1)),
-  ])
-  allCarbonCenters += makeCarbonCenters(cells: cells)
-}
-
-func oldHousing() {
-  // Half the hole width in either dimension.
-  let holeWidthX: Float = 2.0
-  let holdWidthYZ: Float = 3.0
-  
-  do {
-    var cells = baseLattice
-    let blockOrigin = SIMD3<Float>(SIMD3(repeating: latticeWidth / 2))
-    
-    let innerBounds: [SIMD3<Float>] = [
-      SIMD3(0, 2, 2),
-      SIMD3(0, 4, -4),
-      SIMD3(0, -2, -2),
-      SIMD3(0, -4, 4),
-    ]
-    let thickness: Float = 0.5
-    let outerBounds = innerBounds.map {
-      $0 + thickness * SIMD3<Float>(
-        (sign(Float($0.x))),
-        (sign(Float($0.y))),
-        (sign(Float($0.z))))
-    }
-    
-    cells = cleave(cells: cells, planes: innerBounds.map {
-      Plane(blockOrigin + $0, normal: SIMD3(0 - $0))
-    })
-    for bound in outerBounds {
-      cells = cleave(cells: cells, planes: [
-        Plane(blockOrigin + bound, normal: SIMD3(bound))
-      ])
-    }
-    
-    // Make a remover, then translate it several times across a diagonal line.
-    // Repeat in the alternate direction to carve out pyramids, and once more
-    // on the back.
-    var outer100Removers: [[Plane]] = []
-    var hole100Removers_first: [[Plane]] = []
-    var hole100Removers_second: [[Plane]] = []
-    for flippedX in [false, true] {
-      for flippedYZ in [false, true] {
-        let planeOrigin: SIMD3<Int> = SIMD3(
-          flippedX ? 1 : latticeWidth - 1,
-          latticeWidth / 2,
-          latticeWidth / 2
-        )
-        let base100Remover: [Plane] = [
-          Plane(
-            planeOrigin,
-            normal: [
-              flippedX ? -1 : 1,
-              flippedYZ ? 1 : 1,
-              flippedYZ ? -1 : 1,
-            ]),
-          Plane(
-            planeOrigin,
-            normal: [
-              flippedX ? -1 : 1,
-              flippedYZ ? -1 : -1,
-              flippedYZ ? 1 : -1,
-            ]),
-        ]
-        
-        for translationYZ_doubled in -16...16 {
-          let translationYZ = Float(translationYZ_doubled) / 2
-          var delta: SIMD3<Float>
-          if flippedYZ {
-            delta = SIMD3(0, -translationYZ, translationYZ)
-          } else {
-            delta = SIMD3(0, translationYZ, translationYZ)
-          }
-          var remover = base100Remover
-          remover[0].origin += SIMD3(delta)
-          remover[1].origin += SIMD3(delta)
-          outer100Removers.append(remover)
-          
-          var newOrigin0_first = __tg_rint(remover[0].origin * 2) / 2
-          var newOrigin1_first = __tg_rint(remover[1].origin * 2) / 2
-          var newOrigin0_second: SIMD3<Float> = newOrigin0_first
-          var newOrigin1_second: SIMD3<Float> = newOrigin1_first
-          if newOrigin0_first.x == 1 {
-            newOrigin0_first.x = Float(latticeWidth / 2) * 0.5 + holeWidthX
-            newOrigin1_first.x = Float(latticeWidth / 2) * 0.5 + holeWidthX
-            newOrigin0_second.x = Float(latticeWidth / 2) * 1.5 + holeWidthX
-            newOrigin1_second.x = Float(latticeWidth / 2) * 1.5 + holeWidthX
-          } else if newOrigin0_first.x == Float(latticeWidth - 1) {
-            newOrigin0_first.x = Float(latticeWidth / 2) * 0.5 - holeWidthX
-            newOrigin1_first.x = Float(latticeWidth / 2) * 0.5 - holeWidthX
-            newOrigin0_second.x = Float(latticeWidth / 2) * 1.5 - holeWidthX
-            newOrigin1_second.x = Float(latticeWidth / 2) * 1.5 - holeWidthX
-          }
-          newOrigin0_first.x += 0.5
-          newOrigin1_first.x += 0.5
-          newOrigin0_second.x -= 0.5
-          newOrigin1_second.x -= 0.5
-          
-          remover[0] = Plane(newOrigin0_first, normal: remover[0].normal)
-          remover[1] = Plane(newOrigin1_first, normal: remover[1].normal)
-          hole100Removers_first.append(remover)
-          
-          remover[0] = Plane(newOrigin0_second, normal: remover[0].normal)
-          remover[1] = Plane(newOrigin1_second, normal: remover[1].normal)
-          hole100Removers_second.append(remover)
-        }
-      }
-    }
-    for remover in outer100Removers {
-      cells = cleave(cells: cells, planes: remover)
-    }
-    
-    for half in [0, 1] {
-      let holeBounds: [SIMD3<Float>] = [
-        SIMD3(Float(-holeWidthX), 0, 0),
-        SIMD3(Float(+holeWidthX), 0, 0),
-        SIMD3(0, Float(+holdWidthYZ), Float(-holdWidthYZ)),
-        SIMD3(0, Float(-holdWidthYZ), Float(+holdWidthYZ)),
-      ]
-      var holePlanes: [Plane]
-      if half == 0 {
-        holePlanes = holeBounds.map {
-          Plane(
-            SIMD3(blockOrigin.x * 0.5, blockOrigin.y, blockOrigin.z) + $0,
-            normal: SIMD3(0 - $0))
-        }
-      } else {
-        holePlanes = holeBounds.map {
-          Plane(
-            SIMD3(blockOrigin.x * 1.5, blockOrigin.y, blockOrigin.z) + $0,
-            normal: SIMD3(0 - $0))
-        }
-      }
-      
-      var holeRemovers: [[Plane]]
-      if half == 0 {
-        holeRemovers = hole100Removers_first
-      } else {
-        holeRemovers = hole100Removers_second
-      }
-      
-      for remover in holeRemovers {
-        var planes = remover + holePlanes
-        let divider = (half == 0)
-        ? Float(latticeWidth / 4)
-        : Float(latticeWidth * 3 / 4)
-        
-        if planes[0].origin.x > divider {
-          planes.append(
-            Plane([divider - 1, 0, 0], normal: [1, 0, 0]))
-        } else {
-          planes.append(
-            Plane([divider + 1, 0, 0], normal: [-1, 0, 0]))
-        }
-        cells = cleave(cells: cells, planes: planes)
-      }
-    }
-    
-    allCarbonCenters += makeCarbonCenters(cells: cells)
-  }
-}
-#endif
-
-
