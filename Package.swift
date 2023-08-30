d131c5c9f74e8f48828654c51836dfa4ad7c9899
Philip Turner <philipturner.AR@gmail.com>
2023-08-30 07:00:28 -0400

    Let the package compile on non-Apple platforms

--- Package.swift
+++ Package.swift
@@ -3,46 +3,56 @@
 
 import PackageDescription
 
+var platforms: [SupportedPlatform]? = nil
+var products: [Product] = [
+  .library(
+    name: "COpenMM",
+    targets: ["COpenMM"]),
+  .library(
+    name: "HardwareCatalog",
+    targets: ["HardwareCatalog"]),
+  .library(
+    name: "HDL",
+    targets: ["HDL"]),
+  .library(
+    name: "OpenMM",
+    targets: ["OpenMM"]),
+]
+var targets: [Target] = [
+  .target(
+    name: "COpenMM",
+    dependencies: []),
+  .target(
+    name: "HardwareCatalog",
+    dependencies: ["HDL"]),
+  .target(
+    name: "HDL",
+    dependencies: []),
+  .target(
+    name: "OpenMM",
+    dependencies: ["COpenMM"]),
+]
+
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+platforms = [
+  .macOS(.v13)
+]
+products += [
+  .library(
+    name: "MolecularRenderer",
+    targets: ["MolecularRenderer"]),
+]
+targets += [
+  .target(
+    name: "MolecularRenderer",
+    dependencies: []),
+]
+#endif
+
 let package = Package(
   name: "MolecularRenderer",
-  platforms: [
-    .macOS(.v13),
-  ],
-  products: [
-    .library(
-      name: "COpenMM",
-      targets: ["COpenMM"]),
-    .library(
-      name: "HardwareCatalog",
-      targets: ["HardwareCatalog"]),
-    .library(
-      name: "HDL",
-      targets: ["HDL"]),
-    .library(
-      name: "MolecularRenderer",
-      targets: ["MolecularRenderer"]),
-    .library(
-      name: "OpenMM",
-      targets: ["OpenMM"]),
-  ],
-  dependencies: [
-    .package(url: "https://github.com/philipturner/applegpuinfo", from: "2.0.1"),
-  ],
-  targets: [
-    .target(
-      name: "COpenMM",
-      dependencies: []),
-    .target(
-      name: "HardwareCatalog",
-      dependencies: ["HDL"]),
-    .target(
-      name: "HDL",
-      dependencies: []),
-    .target(
-      name: "MolecularRenderer",
-      dependencies: []),
-    .target(
-      name: "OpenMM",
-      dependencies: ["COpenMM"]),
-  ]
+  platforms: platforms,
+  products: products,
+  dependencies: [],
+  targets: targets
 )
