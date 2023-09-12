217a3615a8cce1ad5033e53d802746d0a9fd11bb
Philip Turner <philipturner.AR@gmail.com>
2023-09-11 20:14:37 -0400

    Efficient bond topology traversal

--- Sources/MM4/MM4Parameters.swift
+++ Sources/MM4/MM4Parameters.swift
@@ -37,6 +37,18 @@ public class MM4Parameters {
   /// The mass of each atom after hydrogen mass repartitioning.
   public internal(set) var masses: [Float]
   
+  /// Groups of atom indices that form an angle.
+  public internal(set) var angles: [SIMD3<Int32>]
+  
+  /// Groups of atom indices that form a torsion.
+  public internal(set) var torsions: [SIMD4<Int32>]
+  
+  /// Atom pairs to be excluded from vdW and electric interactions.
+  public internal(set) var nonbondedExceptions13: [SIMD2<Int32>]
+  
+  /// Atom pairs to have reduce vdW and/or electric interactions.
+  public internal(set) var nonbondedExceptions14: [SIMD2<Int32>]
+  
   /// Create a set of parameters using the specified configuration.
   public init(descriptor: MM4ParametersDescriptor) {
     // Check the bond orders.
@@ -89,6 +101,14 @@ public class MM4Parameters {
       }
     }
     
+    @inline(__always)
+    func other<T: FixedWidthInteger, U: FixedWidthInteger>(
+      atomID: T, bondID: U
+    ) -> Int32 {
+      let bond = bondsToAtomsMap[Int(bondID)]
+      return (bond[0] == atomID) ? bond[1] : bond[0]
+    }
+    
     // Assign masses using hydrogen mass repartitioning.
     masses = descriptor.atomicNumbers.map { atomicNumber in
       MM4MassParameters.global.mass(atomicNumber: atomicNumber)
@@ -101,16 +121,93 @@ public class MM4Parameters {
       guard map[0] != -1, map[1] == -1, map[2] == -1, map[3] == -1 else {
         fatalError("Hydrogen did not have exactly 1 bond.")
       }
-      let bondID = Int(map[0])
-      let bond = bondsToAtomsMap[bondID]
-      guard any(bond .== Int32(truncatingIfNeeded: atomID)) else {
-        fatalError("Bond did not contain hydrogen.")
-      }
-      let substituentID = (bond[0] != Int32(truncatingIfNeeded: atomID))
-      ? Int(bond[0]) : Int(bond[1])
-      
+      let substituentID = Int(other(atomID: atomID, bondID: map[0]))
       masses[substituentID] -= Float(descriptor.hydrogenMassRepartitioning)
     }
+    
+    // Traverse the bond topology.
+    var ringTypes: [UInt8] = []
+    var anglesMap: [SIMD3<Int32>: Bool] = [:]
+    var torsionsMap: [SIMD4<Int32>: Bool] = [:]
+    for atom1 in 0..<Int32(descriptor.atomicNumbers.count) {
+      let map1 = atomsToBondsMap[Int(atom1)]
+      var ringType: UInt8 = 6
+      defer { ringTypes.append(ringType) }
+      
+      for lane2 in 0..<4 where map1[lane2] != -1 {
+        let atom2 = other(atomID: atom1, bondID: map1[lane2])
+        let map2 = atomsToBondsMap[Int(atom2)]
+        
+        for lane3 in 0..<4 where map2[lane3] != -1 {
+          let atom3 = other(atomID: atom2, bondID: map2[lane3])
+          if atom1 == atom3 { continue }
+          
+          if atom1 < atom3 {
+            anglesMap[SIMD3(atom1, atom2, atom3)] = true
+          }
+          let map3 = atomsToBondsMap[Int(atom3)]
+          for lane4 in 0..<4 where map3[lane4] != -4 {
+            let atom4 = other(atomID: atom3, bondID: map3[lane4])
+            if atom2 == atom4 {
+              continue
+            } else if atom1 == atom4 {
+              ringType = min(3, ringType)
+              continue
+            } else if atom1 < atom4 {
+              torsionsMap[SIMD4(atom1, atom2, atom3, atom4)] = true
+            }
+            
+            let map4 = atomsToBondsMap[Int(atom4)]
+            @inline(__always)
+            func iterate(lane5: Int) -> SIMD4<Int32> {
+              let atom5 = other(atomID: atom4, bondID: map4[lane5])
+              let map5 = atomsToBondsMap[Int(atom5)]
+              var atoms6 = SIMD4<Int32>(repeating: -1)
+              for lane6 in 0..<4 {
+                atoms6[lane6] = other(atomID: atom5, bondID: map5[lane6])
+              }
+              
+              var ringType = SIMD4<Int32>(repeating: 6)
+              ringType.replace(
+                with: .init(repeating: 5), where: atom1 .== atoms6)
+              ringType.replace(
+                with: .init(repeating: 4),
+                where: .init(repeating: atom1 == atom5))
+              return ringType
+            }
+            
+            var mask1 = iterate(lane5: 0)
+            var mask2 = iterate(lane5: 1)
+            var mask3 = iterate(lane5: 2)
+            var mask4 = iterate(lane5: 3)
+            mask1.replace(with: mask2, where: mask2 .< mask1)
+            mask3.replace(with: mask4, where: mask4 .< mask3)
+            mask1.replace(with: mask3, where: mask1 .< mask3)
+            ringType = min(ringType, UInt8(truncatingIfNeeded: mask1.min()))
+          }
+        }
+      }
+    }
+    angles = anglesMap.keys.map { $0 }
+    torsions = torsionsMap.keys.map { $0 }
+    
+    // Create nonbonded exceptions.
+    var nonbondedExceptions13Map: [SIMD2<Int32>: Bool] = [:]
+    var nonbondedExceptions14Map: [SIMD2<Int32>: Bool] = [:]
+    for torsion in torsions {
+      guard torsion[0] < torsion[3] else {
+        fatalError("Torsions were not sorted.")
+      }
+      nonbondedExceptions14Map[SIMD2(torsion[0], torsion[3])] = true
+    }
+    for angle in angles {
+      guard angle[0] < angle[2] else {
+        fatalError("Angle was not sorted.")
+      }
+      nonbondedExceptions13Map[SIMD2(angle[0], angle[2])] = true
+      nonbondedExceptions14Map[SIMD2(angle[0], angle[2])] = nil
+    }
+    nonbondedExceptions13 = nonbondedExceptions13Map.keys.map { $0 }
+    nonbondedExceptions14 = nonbondedExceptions14Map.keys.map { $0 }
   }
 }
- 
