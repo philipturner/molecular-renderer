b4e17967231ef6e22a91bce14037360ef3dc6dd5
Philip Turner <philipturner.AR@gmail.com>
2023-09-18 23:23:27 -0400

    Notes for projectile

--- Sources/MolecularRendererApp/Scenes/References/Spring/Spring_Assembly.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Spring_Assembly.swift
-//  MolecularRendererApp
-//
-//  Created by Philip Turner on 9/15/23.
-//
-
-import Foundation
--- Sources/MolecularRendererApp/Scenes/References/Spring/Spring_Projectile.swift
+++ Sources/MolecularRendererApp/Scenes/References/Spring/Spring_Projectile.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+// Shape the projectile so that landing disperses as little vdW energy as
+// possible (don't want it to take the ring with it!). Make it poke into the
+// four housings sticking out of the top, and perfectly align the trajectory to
+// make that happen. Use the crystolecule assembly from the springboard to
+// measure where the protrusions should be placed.
