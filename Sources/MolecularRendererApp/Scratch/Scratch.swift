64c76d1c8ca1f37d3e9872ed03ed0f88d5037fb0
Philip Turner <philipturner.AR@gmail.com>
2024-01-18 21:19:30 -0500

    This NOR gate works

--- Sources/MolecularRendererApp/Scratch/Scratch.swift
+++ Sources/MolecularRendererApp/Scratch/Scratch.swift
@@ -6,8 +6,16 @@ import MM4
 import Numerics
 
 func createGeometry() -> [[Entity]] {
-  let logicHousing = LogicHousing()
-  var logicRod = LogicRod()
+  let logicHousing = LogicHousing(parity: false)
+  var logicHousing2 = LogicHousing(parity: true)
+  for i in logicHousing2.topology.atoms.indices {
+    var position = logicHousing2.topology.atoms[i].position
+    position = SIMD3(position.z, position.y, position.x)
+    position.x += 0.3567 * 7.5
+    logicHousing2.topology.atoms[i].position = position
+  }
+  
+  var logicRod = LogicRod(length: 20)
   for i in logicRod.topology.atoms.indices {
     var position = logicRod.topology.atoms[i].position
     position = SIMD3(position.z, position.y, position.x)
@@ -16,28 +24,35 @@ func createGeometry() -> [[Entity]] {
     position.y -= 0.050
     logicRod.topology.atoms[i].position = position
   }
-  var logicRod2 = logicRod
+  var logicRod2 = LogicRod(length: 30)
   for i in logicRod2.topology.atoms.indices {
     var position = logicRod2.topology.atoms[i].position
+    position = SIMD3(position.z, position.y, position.x)
+    position += 0.3567 * SIMD3(3.0, 2.5, -3)
+    position.x += 0.030
+    position.y -= 0.050
+    
     position = SIMD3(position.z, position.y, position.x)
     position.y = 0.3567 * 10 - position.y
     logicRod2.topology.atoms[i].position = position
   }
-  
-  for i in logicRod.topology.atoms.indices {
-    var position = logicRod.topology.atoms[i].position
-    position += 0.3567 * SIMD3(0, 0, -3)
-    logicRod.topology.atoms[i].position = position
+  var logicRod3 = logicRod
+  for i in logicRod3.topology.atoms.indices {
+    var position = logicRod3.topology.atoms[i].position
+    position.x += 0.3567 * 7.5
+    logicRod3.topology.atoms[i].position = position
   }
   
   var topologies = [
     logicHousing.topology,
+    logicHousing2.topology,
     logicRod.topology,
-    logicRod2.topology
+    logicRod2.topology,
+    logicRod3.topology
   ]
-//  return [topologies.flatMap(\.atoms)]
   var topologyRanges: [Range<Int>] = []
   var topologyCursor: Int = 0
+//  return [topologies.flatMap(\.atoms)]
   
   var parameters: MM4Parameters?
   for topology in topologies {
@@ -45,13 +60,11 @@ func createGeometry() -> [[Entity]] {
     descriptor.atomicNumbers = topology.atoms.map(\.atomicNumber)
     descriptor.bonds = topology.bonds
     let params = try! MM4Parameters(descriptor: descriptor)
-    
     if parameters == nil {
       parameters = params
     } else {
       parameters!.append(contentsOf: params)
     }
-    
     let atomStart = topologyCursor
     topologyCursor += topology.atoms.count
     let atomEnd = topologyCursor
@@ -64,40 +77,59 @@ func createGeometry() -> [[Entity]] {
   forceField.positions = topologies.flatMap { $0.atoms.map(\.position) }
   forceField.minimize()
   
-  var externalForces = forceField.positions.map { $0 * 0 }
-  for i in topologyRanges[1] {
-    externalForces[i] = SIMD3(0, 0, 1)
-  }
-  forceField.externalForces = externalForces
-  
   var animation: [[Entity]] = []
-  for frameID in 0...120 {
+  for frameID in 0..<180 {
+    print("frame=\(frameID)")
     forceField.simulate(time: 0.100)
     
-    for topologyID in topologies.indices {
-      let positions = Array(forceField.positions[topologyRanges[topologyID]])
-      for i in topologies[topologyID].atoms.indices {
-        topologies[topologyID].atoms[i].position = positions[i]
+    if frameID == 10 {
+      var externalForces = forceField.positions.map { $0 * 0 }
+      for i in topologyRanges[3] {
+        let perAtomForce = 100_000 / Float(logicRod2.topology.atoms.count)
+        externalForces[i] = SIMD3(perAtomForce, 0, 0)
+      }
+      forceField.externalForces = externalForces
+    }
+    if frameID == 35 {
+      var externalForces = forceField.positions.map { $0 * 0 }
+      for i in topologyRanges[3] {
+        let perAtomForce = -1_000_000 / Float(logicRod2.topology.atoms.count)
+        externalForces[i] = SIMD3(perAtomForce, 0, 0)
+      }
+      forceField.externalForces = externalForces
+    }
+    if frameID >= 47 {
+      forceField.velocities = forceField.positions.map { $0 * 0 }
+      forceField.externalForces = forceField.positions.map { $0 * 0 }
+      if frameID % 10 == 0 {
+        forceField.minimize(maxIterations: 30)
       }
     }
-    animation.append(topologies.flatMap(\.atoms))
+    
+    var frame: [Entity] = []
+    for topologyID in topologies.indices {
+      let topology = topologies[topologyID]
+      let range = topologyRanges[topologyID]
+      let positions = Array(forceField.positions[range])
+      for i in positions.indices {
+        let storage = SIMD4(positions[i], topology.atoms[i].storage.w)
+        frame.append(Entity(storage: storage))
+      }
+    }
+    animation.append(frame)
   }
-  
   return animation
 }
 
 struct LogicHousing {
   var topology = Topology()
   
-  init() {
-    createLattice()
+  init(parity: Bool) {
+    createLattice(parity: parity)
     passivateSurfaces()
   }
   
-  // The housing may need to be built out of SiC or GeC, if the atom spacings
-  // are a poor multiple of vdW radius. This could also decrease compute cost,
-  // especially if the housing is anchored in place.
-  mutating func createLattice() {
+  mutating func createLattice(parity: Bool) {
     let lattice = Lattice<Cubic> { h, k, l in
       Bounds { 8 * h + 10 * k + 8 * l }
       Material { .elemental(.carbon) }
@@ -137,12 +169,65 @@ struct LogicHousing {
       
       Volume {
         Convex {
-          Origin { 2 * h + 1.5 * k }
-          cutGroove(direction: h)
+          let direction = parity ? l : h
+          Origin { 2 * direction + 1.5 * k }
+          cutGroove(direction: direction)
         }
         Convex {
-          Origin { 2 * l + 4.25 * k }
-          cutGroove(direction: l)
+          let direction = parity ? h : l
+          Origin { 2 * direction + 4.25 * k }
+          cutGroove(direction: direction)
+        }
+        
+        Convex {
+          Concave {
+            Origin { 1 * h }
+            Plane { -h }
+            Origin { 5.5 * k }
+            Origin { 0.25 * (-h - k) }
+            Plane { -h - k }
+          }
+          Concave {
+            Origin { 7 * h }
+            Plane { h }
+            Origin { 5.5 * k }
+            Origin { 0.25 * (h - k) }
+            Plane { h - k }
+          }
+          
+          Concave {
+            Origin { 1 * l }
+            Plane { -l }
+            Origin { 4.5 * k }
+            Origin { 0.25 * (-l + k) }
+            Plane { -l + k }
+          }
+          Concave {
+            Origin { 7 * l }
+            Plane { l }
+            Origin { 4.5 * k }
+            Origin { 0.25 * (l + k) }
+            Plane { l + k }
+          }
+        }
+        
+        Convex {
+          if parity {
+            Origin { 0.5 * k }
+            Plane { -k }
+          } else {
+            Origin { 9.5 * k }
+            Plane { k }
+          }
+        }
+        
+        Origin { 4 * h + 4 * l }
+        let directions = [h, l, -h, -l]
+        for direction in directions {
+          Convex {
+            Origin { 3.5 * direction }
+            Plane { direction }
+          }
         }
         
         Replace { .empty }
@@ -171,17 +256,17 @@ struct LogicHousing {
 struct LogicRod {
   var topology = Topology()
   
-  init() {
-    createRod()
+  init(length: Int) {
+    createRod(length: length)
     passivateSurfaces()
   }
   
   // Find a static rod thickness that's optimal for the entire system. Then,
   // make the rod length variable.
-  mutating func createRod() {
+  mutating func createRod(length: Int) {
     let lattice = Lattice<Hexagonal> { h, k, l in
       let h2k = h + 2 * k
-      Bounds { 20 * h + 2 * h2k + 4 * l }
+      Bounds { Float(length) * h + 2 * h2k + 4 * l }
       Material { .elemental(.carbon) }
       
       func cutGroove() {
