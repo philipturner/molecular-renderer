6d3bc786abafec02ca8dc7f74b3d139604a89430
Philip Turner <philipturner.AR@gmail.com>
2024-01-20 22:56:45 -0500

    [DMSTooltips] Compiled structure of DCB6-SiGe tooltip

--- Sources/MolecularRendererApp/Scratch/Scratch.swift
+++ Sources/MolecularRendererApp/Scratch/Scratch.swift
@@ -6,6 +6,9 @@ import MM4
 import Numerics
 
 func createGeometry() -> [Entity] {
+  let tooltipLHS: Element = .silicon
+  let tooltipRHS: Element = .germanium
+  
   let lattice = Lattice<Cubic> { h, k, l in
     Bounds { 4 * h + 4 * k + 4 * l }
     Material { .elemental(.carbon) }
@@ -57,46 +60,94 @@ func createGeometry() -> [Entity] {
     }
   }
   
+  var topology = Topology()
+  topology.insert(atoms: lattice.atoms)
+  
   // Center the adamantane at (0, 0, 0).
-  var atoms = lattice.atoms
   var accumulator: SIMD3<Float> = .zero
-  for atom in atoms {
+  for atom in topology.atoms {
     accumulator += atom.position
   }
-  accumulator /= Float(atoms.count)
+  accumulator /= Float(topology.atoms.count)
   
   // Rotate the adamantane and make the three bridge carbons flush.
   let rotation1 = Quaternion<Float>(angle: .pi / 4, axis: [0, 1, 0])
   let rotation2 = Quaternion<Float>(angle: 35.26 * .pi / 180, axis: [0, 0, 1])
   var maxX: Float = -.greatestFiniteMagnitude
-  for i in atoms.indices {
-    var position = atoms[i].position
+  for i in topology.atoms.indices {
+    var position = topology.atoms[i].position
     position -= accumulator
     position = rotation1.act(on: position)
     position = rotation2.act(on: position)
-    atoms[i].position = position
+    topology.atoms[i].position = position
     maxX = max(maxX, position.x)
   }
-  for i in atoms.indices {
-    atoms[i].position.x -= maxX
-    atoms[i].position.x -= Element.carbon.covalentRadius
+  for i in topology.atoms.indices {
+    topology.atoms[i].position.x -= maxX
+    topology.atoms[i].position.x -= Element.carbon.covalentRadius
   }
   
   // Create the second half.
-  atoms += atoms.map {
+  topology.insert(atoms: topology.atoms.map {
     var copy = $0
     copy.position.x = -copy.position.x
-    if copy.atomicNumber == 14 {
-      copy.atomicNumber = 82
-    }
     return copy
-  }
+  })
   
-  for i in atoms.indices {
-    if atoms[i].atomicNumber == 14 {
-      atoms[i].atomicNumber = 50
+  var reactiveSiteAtoms: [Int] = []
+  for i in topology.atoms.indices {
+    if topology.atoms[i].atomicNumber != 6 {
+      reactiveSiteAtoms.append(i)
+      
+      if reactiveSiteAtoms.count == 1 {
+        topology.atoms[i].atomicNumber = tooltipLHS.rawValue
+      } else if reactiveSiteAtoms.count == 2 {
+        topology.atoms[i].atomicNumber = tooltipRHS.rawValue
+      }
     }
   }
   
-  return atoms
+  // Add the hydrogens.
+  let matchRadius = 2 * Element.carbon.covalentRadius
+  let matches = topology.match(
+    topology.atoms, algorithm: .absoluteRadius(1.1 * matchRadius))
+  
+  var insertedBonds: [SIMD2<UInt32>] = []
+  for i in topology.atoms.indices {
+    for j in matches[i] where i < j {
+      if topology.atoms[Int(i)].atomicNumber != 6,
+         topology.atoms[Int(j)].atomicNumber != 6 {
+        continue
+      }
+      insertedBonds.append(SIMD2(UInt32(i), UInt32(j)))
+    }
+  }
+  topology.insert(bonds: insertedBonds)
+  
+  let orbitals = topology.nonbondingOrbitals()
+  let chBondLength = Element.carbon.covalentRadius +
+  Element.hydrogen.covalentRadius
+  
+  var insertedAtoms: [Entity] = []
+  insertedBonds = []
+  for i in topology.atoms.indices {
+    if topology.atoms[i].atomicNumber != 6 {
+      continue
+    }
+    let carbon = topology.atoms[i]
+    for orbital in orbitals[i] {
+      let position = carbon.position + orbital * chBondLength
+      let hydrogen = Entity(position: position, type: .atom(.hydrogen))
+      let hydrogenID = topology.atoms.count + insertedAtoms.count
+      let bond = SIMD2(UInt32(i), UInt32(hydrogenID))
+      insertedAtoms.append(hydrogen)
+      insertedBonds.append(bond)
+    }
+  }
+  topology.insert(atoms: insertedAtoms)
+  topology.insert(bonds: insertedBonds)
+  
+  
+  
+  return topology.atoms
 }
