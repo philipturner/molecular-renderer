ee65899bfebdc805edb88b83486614197718874a
Philip Turner <philipturner.AR@gmail.com>
2024-01-27 22:58:12 -0500

    A major bottleneck in debugging quantum mechanics is the cubic or sextic scaling of the simplest algorithms

--- Sources/MolecularRendererApp/Scratch/Scratch.swift
+++ Sources/MolecularRendererApp/Scratch/Scratch.swift
@@ -13,6 +13,8 @@ func createGeometry() -> [Entity] {
   // - This is still a single-electron system. We need to prove that the
   //   Schrodinger equation produces an orbital strongly reminiscent of the
   //   hydrogen 1s orbital.
+  // - Introduce variable-resolution wave functions during this experiment to
+  //   reduce the compute cost of high-accuracy tests.
   
   return [Entity(position: .zero, type: .atom(.carbon))]
 }
