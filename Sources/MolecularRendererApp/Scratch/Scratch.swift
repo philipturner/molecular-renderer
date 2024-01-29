711576bdeebcd121e44eca00a90072ca02d60f04
Philip Turner <philipturner.AR@gmail.com>
2024-01-28 20:21:32 -0500

    Got a variety of hydrogen orbitals rendering

--- Sources/MolecularRendererApp/Scratch/Scratch.swift
+++ Sources/MolecularRendererApp/Scratch/Scratch.swift
@@ -28,37 +28,26 @@ func createGeometry() -> [Entity] {
   // - Compare constrained fragment count (density threshold) to actual fragment
   //   count, which will be larger.
   
-  let cellCountWidth: Int = 180
-  let gridWidthBohr: Float = 6
+  var descriptor = WavefunctionDescriptor()
+  descriptor.cellCountWidth = 128
+  descriptor.gridWidthBohr = 25
+  descriptor.quantumNumbers = QuantumNumbers(n: 1, l: 0, m: 0)
+  let fragments = createWavefunction(descriptor: descriptor)
   
-  var fragments: [OrbitalFragment] = []
-  for zIndex in -cellCountWidth/2..<cellCountWidth/2 {
-    for yIndex in -cellCountWidth/2..<cellCountWidth/2 {
-      for xIndex in -cellCountWidth/2..<cellCountWidth/2 {
-        let spacing = gridWidthBohr / Float(cellCountWidth)
-        var position = SIMD3<Float>(
-          Float(xIndex) + 0.5,
-          Float(yIndex) + 0.5,
-          Float(zIndex) + 0.5)
-        position *= spacing
-        
-        let numbers = QuantumNumbers(n: 1, l: 0, m: 0)
-        let coordinates = SphericalCoordinates(cartesian: position)
-        let wavefunction = hydrogenWaveFunction(
-          numbers: numbers, coordinates: coordinates)
-        
-        fragments.append(OrbitalFragment(
-          position: position,
-          spacing: spacing,
-          wavefunction: wavefunction))
-      }
-    }
+  var integral: Double = .zero
+  for fragment in fragments {
+    let value = fragment.wavefunction * fragment.wavefunction
+    let microvolume = fragment.spacing * fragment.spacing * fragment.spacing
+    integral += Double(value * microvolume)
   }
+  print("integral:", integral)
   
-  print(fragments.count)
+  // Next, start measuring some observables like kinetic and external energy.
   
-  exit(0)
+  return renderElectron(fragments)
 }
+
+// MARK: - Wavefunction Initialization
 
 struct OrbitalFragment {
   // Center of the fragment, in Bohr.
@@ -72,30 +61,112 @@ struct OrbitalFragment {
   var wavefunction: Float
 }
 
+// A descriptor for a hydrogen wavefunction.
+struct WavefunctionDescriptor {
+  var cellCountWidth: Int?
+  var gridWidthBohr: Float?
+  var quantumNumbers: QuantumNumbers?
+  // future options for enabling variable resolution and max probability
+}
+
+func createWavefunction(
+  descriptor: WavefunctionDescriptor
+) -> [OrbitalFragment] {
+  var fragments: [OrbitalFragment] = []
+  guard let cellCountWidth = descriptor.cellCountWidth,
+        let gridWidthBohr = descriptor.gridWidthBohr,
+        let quantumNumbers = descriptor.quantumNumbers else {
+    fatalError("Incomplete descriptor.")
+  }
+  
+  func traverseOctree(coordinates: SIMD3<Float>, radius: Float) {
+    let lowerInvalid = coordinates - radius .>= Float(cellCountWidth) / 2
+    let upperInvalid = coordinates + radius .<= -Float(cellCountWidth) / 2
+    if any(lowerInvalid .| upperInvalid) {
+      return
+    }
+    
+    if radius < 1 {
+      let spacing = gridWidthBohr / Float(cellCountWidth)
+      let position = coordinates * spacing
+      let coordinates = SphericalCoordinates(cartesian: position)
+      let wavefunction = hydrogenWaveFunction(
+        numbers: quantumNumbers, coordinates: coordinates)
+      
+      fragments.append(OrbitalFragment(
+        position: position,
+        spacing: spacing,
+        wavefunction: wavefunction))
+    } else {
+      @_transparent func traverse(_ permutation: SIMD3<Int8>) {
+        let nextRadius = radius / 2
+        traverseOctree(
+          coordinates: coordinates + SIMD3<Float>(permutation) * nextRadius,
+          radius: nextRadius)
+      }
+      
+      traverse(SIMD3(-1, -1, -1))
+      traverse(SIMD3( 1, -1, -1))
+      traverse(SIMD3(-1,  1, -1))
+      traverse(SIMD3( 1,  1, -1))
+      traverse(SIMD3(-1, -1,  1))
+      traverse(SIMD3( 1, -1,  1))
+      traverse(SIMD3(-1,  1,  1))
+      traverse(SIMD3( 1,  1,  1))
+    }
+  }
+  
+  traverseOctree(coordinates: .zero, radius: 1024 * 1024)
+  return fragments
+}
+
 // MARK: - Hydrogen Wave Function
 
 func laguerrePolynomial(
   alpha: Float, n: Int
 ) -> (_ x: Float) -> Float {
-  guard n == 0 || n == 1 else {
-    fatalError("Unsupported value for n.")
-  }
   if n == 0 {
     return { _ in 1 }
-  } else {
+  } else if n == 1 {
     return { x in 1 + alpha - x }
+  } else if n >= 1 {
+    return { x in
+      var stack: [Float] = []
+      stack.append(1)
+      stack.append(1 + alpha - x)
+      
+      for k in 1...(n - 1) {
+        let coeffLeft = Float(2 * k + 1) + alpha - x
+        let coeffRight = -Float(k) * alpha
+        let numerator = coeffLeft * stack[k] + coeffRight * stack[k - 1]
+        let denominator = Float(k + 1)
+        stack.append(numerator / denominator)
+      }
+      return stack.last!
+    }
   }
+  
+  fatalError("Unsupported value for n.")
 }
 
 func sphericalHarmonic(
   l: Int, m: Int
 ) -> (_ theta: Float, _ phi: Float) -> Float {
-  guard m == 0, l == 0 else {
-    fatalError("Unsupported value for m or l.")
+  if l == 0 {
+    if m == 0 {
+      return { _, _ in
+        1.0 / 2 * (1 / Float.pi).squareRoot()
+      }
+    }
+  } else if l == 1 {
+    if m == 0 {
+      return { theta, _ in
+        1.0 / 2 * (3 / Float.pi).squareRoot() * Float.cos(theta)
+      }
+    }
   }
-  return { _, _ in
-    1.0 / 2 * (1 / Float.pi).squareRoot()
-  }
+  
+  fatalError("Unsupported value for m or l.")
 }
 
 func factorial(_ x: Int) -> Int {
@@ -123,17 +194,18 @@ struct QuantumNumbers {
 
 struct SphericalCoordinates {
   var r: Float
-  var theta: Float
   var phi: Float
+  var theta: Float
   
   init(cartesian: SIMD3<Float>) {
     r = (cartesian * cartesian).sum().squareRoot()
     if r.magnitude < .leastNormalMagnitude {
-      theta = 0
       phi = 0
+      theta = 0
     } else {
-      theta = Float.atan2(y: cartesian.y, x: cartesian.x)
-      phi = Float.acos(cartesian.z / r)
+      // in the physics convention, phi is theta and theta is phi
+      phi = Float.atan2(y: cartesian.y, x: cartesian.x)
+      theta = Float.acos(cartesian.z / r)
     }
   }
 }
@@ -164,5 +236,36 @@ func hydrogenWaveFunction(
   }
   
   let Y = sphericalHarmonic(l: numbers.l, m: numbers.m)
-  return R(coordinates.r) * Y(coordinates.theta, coordinates.phi)
+  let magnitude = R(coordinates.r) * Y(coordinates.theta, coordinates.phi)
+  let parity = pow(-1, Float(numbers.l))
+  return parity * magnitude
+}
+
+// MARK: - Rendering
+
+func renderElectron(_ fragments: [OrbitalFragment]) -> [Entity] {
+  var output: [Entity] = []
+  for fragment in fragments {
+    let microvolume = fragment.spacing * fragment.spacing * fragment.spacing
+    let chargeDensity = fragment.wavefunction * fragment.wavefunction
+    let atomsPerNm3 = min(256, 64 * 300 * chargeDensity)
+    
+    let scale: Float = 1
+    let microvolumeNm3 = microvolume * scale * scale * scale
+    var atomsToGenerate = Int((microvolumeNm3 * atomsPerNm3).rounded(.down))
+    let remainder = microvolumeNm3 * atomsPerNm3 - Float(atomsToGenerate)
+    if Float.random(in: 0..<1) < remainder {
+      atomsToGenerate += 1
+    }
+    
+    for _ in 0..<atomsToGenerate {
+      let range = -fragment.spacing/2..<fragment.spacing/2
+      let offset = SIMD3<Float>.random(in: range)
+      let positionBohr = fragment.position + offset
+      let positionNm = positionBohr * scale
+      output.append(Entity(position: positionNm, type: .atom(.hydrogen)))
+    }
+  }
+  
+  return output
 }
