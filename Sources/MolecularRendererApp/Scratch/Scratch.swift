942ff7555b6f9b97c31930e4e8e65e5014288c4b
Philip Turner <philipturner.AR@gmail.com>
2024-02-24 18:06:28 -0500

    First step should be a working full adder with the proposed clocking mechanism

--- Sources/MolecularRendererApp/Scratch/Scratch.swift
+++ Sources/MolecularRendererApp/Scratch/Scratch.swift
@@ -7,7 +7,7 @@ import Numerics
 import OpenMM
 
 func createGeometry() -> [Entity] {
-  // Sketch all of the moving parts for a flywheel drive system, without the
-  // housing. It's okay if each part is a solid, unmanufacturable piece.
+  // Smaller test system: full adder driven by the signal source. Should have
+  // an interface for inputs and an interface for outputs.
   return [Entity(position: .zero, type: .atom(.carbon))]
 }
