ea04d37b3c458f88af478fcfe31d3b8b390e8faa
Philip Turner <philipturner.AR@gmail.com>
2024-02-29 18:50:31 -0500

    Code for compiling the atoms for this experiment

--- Sources/MolecularRendererApp/Scratch/Scratch.swift
+++ Sources/MolecularRendererApp/Scratch/Scratch.swift
@@ -6,7 +6,288 @@ import MM4
 import Numerics
 
 func createGeometry() -> [Entity] {
-  return [Entity(position: .zero, type: .atom(.carbon))]
+  // Create a nanoreactor for HAbst and HDon reactions.
+  var descriptor = IsobutaneDescriptor()
+  descriptor.bulkElement = .silicon
+  descriptor.tipElement = .tin
+  descriptor.passivation = .hydrogen
+  var leftHandSide = Isobutane(descriptor: descriptor)
+  print(leftHandSide.anchors)
+  print(leftHandSide.anchors.map { leftHandSide.topology.atoms[Int($0)].atomicNumber })
+  print(leftHandSide.tipAtomID)
+  print(leftHandSide.topology.atoms[Int(leftHandSide.tipAtomID)].atomicNumber)
+  
+//  descriptor.bulkElement = .carbon
+//  descriptor.tipElement = .carbon
+//  descriptor.passivation = .radical
+//  var rightHandSide = Isobutane(descriptor: descriptor)
+  
+  return leftHandSide.topology.atoms //+ rightHandSide.topology.atoms
+}
+
+enum Passivation {
+  case acetyleneRadical
+  case hydrogen
+  case radical
+}
+
+struct IsobutaneDescriptor {
+  var bulkElement: Element = .carbon
+  var tipElement: Element = .carbon
+  var passivation: Passivation = .hydrogen
+}
+
+struct Isobutane {
+  var topology = Topology()
+  var anchors: [UInt32] = []
+  var tipAtomID: UInt32 = .max
+  
+  init(descriptor: IsobutaneDescriptor) {
+    compilationPass0(bulkElement: descriptor.bulkElement)
+    compilationPass1()
+    compilationPass2(bulkElement: descriptor.bulkElement)
+    compilationPass3(bulkElement: descriptor.bulkElement)
+    compilationPass4(bulkElement: descriptor.bulkElement)
+    compilationPass5(descriptor: descriptor)
+    compilationPass6(tipElement: descriptor.tipElement)
+  }
+  
+  // Create the center atoms.
+  mutating func compilationPass0(bulkElement: Element) {
+    let lattice = Lattice<Cubic> { h, k, l in
+      Bounds { 2 * (h + k + l) }
+      Material { .elemental(bulkElement) }
+      
+      Volume {
+        Convex {
+          let direction = -h + k + l
+          Origin { direction }
+          Plane { -direction }
+        }
+        Convex {
+          let direction = -h + k + l
+          Origin { 1.2 * direction }
+          Plane { direction }
+        }
+        Replace { .empty }
+        
+        // Mark the apex atom with lead. This will never be used as a bulk or
+        // tip element.
+        Convex {
+          let direction = -h + k + l
+          Origin { 1.05 * direction }
+          Plane { direction }
+        }
+        Replace { .atom(.lead) }
+      }
+    }
+    topology.atoms = lattice.atoms
+  }
+  
+  // Rotate so the tip points right.
+  mutating func compilationPass1() {
+    var basisVector1: SIMD3<Float> = [-1, 1, 1]
+    var basisVector2: SIMD3<Float> = [1, 0, 1]
+    basisVector1 /= (basisVector1 * basisVector1).sum().squareRoot()
+    basisVector2 /= (basisVector2 * basisVector2).sum().squareRoot()
+    
+    var basisVector3 = cross_platform_cross(basisVector1, basisVector2)
+    basisVector3 /= (basisVector3 * basisVector3).sum().squareRoot()
+    
+    for atomID in topology.atoms.indices {
+      var position = topology.atoms[atomID].position
+      let xComponent = (position * basisVector1).sum()
+      let zComponent = (position * basisVector2).sum()
+      let yComponent = (position * basisVector3).sum()
+      position = [xComponent, yComponent, zComponent]
+      topology.atoms[atomID].position = position
+    }
+    
+    // Adjust so the maximum X component is zero.
+    var maxX: Float = -.greatestFiniteMagnitude
+    for atom in topology.atoms {
+      maxX = max(maxX, atom.position.x)
+    }
+    for atomID in topology.atoms.indices {
+      topology.atoms[atomID].position.x -= maxX
+    }
+  }
+  
+  // Remove the pathological atoms.
+  mutating func compilationPass2(bulkElement: Element) {
+    let matches = topology.match(
+      topology.atoms,
+      algorithm: .absoluteRadius(bulkElement.covalentRadius * 2.2))
+    
+    var removedAtoms: [UInt32] = []
+    var insertedBonds: [SIMD2<UInt32>] = []
+    for i in topology.atoms.indices {
+      for j in matches[i] where i < j {
+        let bond = SIMD2(UInt32(i), UInt32(j))
+        insertedBonds.append(bond)
+      }
+      if matches[i].count == 1 {
+        removedAtoms.append(UInt32(i))
+      }
+    }
+    topology.insert(bonds: insertedBonds)
+    topology.remove(atoms: removedAtoms)
+  }
+  
+  // Add the hydrogens pointing in the X direction.
+  // - NOTE: This sets the anchor indices. It assumes the atoms at the
+  //   beginning of the list won't change position.
+  mutating func compilationPass3(bulkElement: Element) {
+    var insertedAtoms: [Entity] = []
+    var insertedBonds: [SIMD2<UInt32>] = []
+    for atomID in topology.atoms.indices {
+      let atom = topology.atoms[atomID]
+      if atom.atomicNumber == Element.lead.rawValue {
+        continue
+      } else {
+        let chBondLength = Self.hydrogenBondLength(element: bulkElement)
+        let orbital: SIMD3<Float> = [-1, 0, 0]
+        let position = atom.position + orbital * chBondLength
+        let hydrogen = Entity(position: position, type: .atom(.hydrogen))
+        
+        let hydrogenID = topology.atoms.count + insertedAtoms.count
+        let bond = SIMD2(UInt32(atomID), UInt32(hydrogenID))
+        insertedAtoms.append(hydrogen)
+        insertedBonds.append(bond)
+        
+        anchors.append(UInt32(hydrogenID))
+      }
+    }
+    topology.insert(atoms: insertedAtoms)
+    topology.insert(bonds: insertedBonds)
+  }
+  
+  // Add hydrogens to the sidewall atoms.
+  mutating func compilationPass4(bulkElement: Element) {
+    let orbitals = topology.nonbondingOrbitals(hybridization: .sp3)
+    
+    var insertedAtoms: [Entity] = []
+    var insertedBonds: [SIMD2<UInt32>] = []
+    for atomID in topology.atoms.indices {
+      let atom = topology.atoms[atomID]
+      if atom.atomicNumber == Element.lead.rawValue {
+        continue
+      } else {
+        for orbital in orbitals[atomID] {
+          let chBondLength = Self.hydrogenBondLength(element: bulkElement)
+          let position = atom.position + orbital * chBondLength
+          let hydrogen = Entity(position: position, type: .atom(.hydrogen))
+          
+          let hydrogenID = topology.atoms.count + insertedAtoms.count
+          let bond = SIMD2(UInt32(atomID), UInt32(hydrogenID))
+          insertedAtoms.append(hydrogen)
+          insertedBonds.append(bond)
+        }
+      }
+    }
+    topology.insert(atoms: insertedAtoms)
+    topology.insert(bonds: insertedBonds)
+  }
+  
+  // Add the passivator.
+  mutating func compilationPass5(descriptor: IsobutaneDescriptor) {
+    var insertedAtoms: [Entity] = []
+    var insertedBonds: [SIMD2<UInt32>] = []
+    for atomID in topology.atoms.indices {
+      let atom = topology.atoms[atomID]
+      if atom.atomicNumber == Element.lead.rawValue {
+        switch descriptor.passivation {
+        case .acetyleneRadical:
+          let xcBondLength = Self.acetyleneBondLength(
+            element: descriptor.tipElement)
+          let orbital1: SIMD3<Float> = [1, 0, 0]
+          let position1 = atom.position + orbital1 * xcBondLength
+          let carbon1 = Entity(position: position1, type: .atom(.carbon))
+          
+          let carbonID1 = topology.atoms.count + insertedAtoms.count
+          let bond1 = SIMD2(UInt32(atomID), UInt32(carbonID1))
+          insertedAtoms.append(carbon1)
+          insertedBonds.append(bond1)
+          
+          let ccBondLength: Float = 1.2100 / 10
+          let orbital2: SIMD3<Float> = [1, 0, 0]
+          let position2 = position1 + orbital2 * ccBondLength
+          let carbon2 = Entity(position: position2, type: .atom(.carbon))
+          
+          let carbonID2 = topology.atoms.count + insertedAtoms.count
+          let bond2 = SIMD2(UInt32(carbonID1), UInt32(carbonID2))
+          insertedAtoms.append(carbon2)
+          insertedBonds.append(bond2)
+        case .hydrogen:
+          let xhBondLength = Self.hydrogenBondLength(
+            element: descriptor.tipElement)
+          let orbital: SIMD3<Float> = [1, 0, 0]
+          let position = atom.position + orbital * xhBondLength
+          let hydrogen = Entity(position: position, type: .atom(.hydrogen))
+          
+          let hydrogenID = topology.atoms.count + insertedAtoms.count
+          let bond = SIMD2(UInt32(atomID), UInt32(hydrogenID))
+          insertedAtoms.append(hydrogen)
+          insertedBonds.append(bond)
+        case .radical:
+          break
+        }
+      } else {
+        continue
+      }
+    }
+    topology.insert(atoms: insertedAtoms)
+    topology.insert(bonds: insertedBonds)
+  }
+  
+  // Transmute the lead marker into the tip element.
+  mutating func compilationPass6(tipElement: Element) {
+    for atomID in topology.atoms.indices {
+      let atom = topology.atoms[atomID]
+      if atom.atomicNumber == Element.lead.rawValue {
+        topology.atoms[atomID].atomicNumber = tipElement.rawValue
+        self.tipAtomID = UInt32(atomID)
+      }
+    }
+  }
+}
+
+extension Isobutane {
+  // The bond length between and sp3 element and hydrogen.
+  static func hydrogenBondLength(element: Element) -> Float {
+    switch element {
+    case .carbon:
+      return 1.1120 / 10
+    case .silicon:
+      return 1.483 / 10
+    case .germanium:
+      return 1.529 / 10
+    case .tin:
+      return 1.6960 / 10
+    default:
+      fatalError("Unexpected element for X-H bond.")
+    }
+  }
+  
+  // The bond length between and sp3 element and sp1 carbon.
+  static func acetyleneBondLength(element: Element) -> Float {
+    switch element {
+    case .carbon:
+      return 1.4700 / 10
+    case .silicon:
+      let sp1sp2Difference: Float = 1.4700 - 1.4990
+      return (1.8540 + sp1sp2Difference) / 10
+    case .germanium:
+      let sp1sp2Difference: Float = 1.4700 - 1.4990
+      return (1.9350 + sp1sp2Difference) / 10
+    case .tin:
+      let sp1sp2Difference: Float = 1.4700 - 1.4990
+      let snGeDifference: Float = 2.1470 - 1.9490
+      return (1.9350 + sp1sp2Difference + snGeDifference) / 10
+    default:
+      fatalError("Unexpected element for X-CC bond.")
+    }
+  }
 }
 
 // MARK: - Utility Functions
