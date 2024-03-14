6261a24b41e1e712302b969aa4d366731d16e96d
Philip Turner <philipturner.AR@gmail.com>
2024-03-14 16:56:18 -0400

    Remove comment about video script

--- Sources/MolecularRendererApp/Scratch/Scratch.swift
+++ Sources/MolecularRendererApp/Scratch/Scratch.swift
@@ -7,36 +7,6 @@ import Numerics
 
 // Create a setup that can test all of the 2-input gates.
 // - Use the new "Collision Detection" level of theory.
-//
-// Speculation about how one would explain the levels of theory in
-// a video:
-//
-// There are four levels of theory:
-// - Semiempirical Quantum Mechanics
-//   - CBN tripod adding carbenic feedstock to logic rod fragment,
-//     with frame interpolation *
-// - Molecular Mechanics
-//   - Obstructed and mobile states of a single logic switch
-// - Rigid Body Mechanics
-//   - Experiment with the SiC drive wall, with a cross-section
-// - Collision Detection
-//   - Clock cycle of a 4-bit RCA, with housing omitted
-//
-// Multiscale simulation enables the design and testing of
-// large nanosystems, in full atomic detail.
-//
-// With only the compute power of a single GPU.
-//
-// * Project idea: simulate the CBN tripod HDon reaction with frame
-//   interpolation. Profile xTB in Xcode, to see whether most of the
-//   time is spent in LAPACK.
-// * Can the carbene idea be used to complete a diamond cage in the
-//   (111) orientation, with 1 DOF?
-//
-// The mechanosynthesis part seems out of scope for such a video, and could
-// take away some very critical time required to get it done (or time that
-// could be spent developing DFT software). Remove this level of theory from
-// such an explanation, resulting in three levels.
 func createGeometry() -> [[Entity]] {
   let inputRodLattice = Lattice<Hexagonal> { h, k, l in
     let h2k = h + 2 * k
@@ -167,18 +137,30 @@ func createGeometry() -> [[Entity]] {
   descriptor.atomsInput1 = atomsInput1
   descriptor.atomsInput2 = atomsInput2
   descriptor.atomsOutput = atomsOutput
-  descriptor.inputBits = [true, true]
   
-  var simulation = Simulation(descriptor: descriptor)
-  for _ in 0..<300 {
-    simulation.step()
-  }
-  print("output:", simulation.result.outputBit)
+  var bitCombinations: [[Bool]] = []
+  bitCombinations.append([false, false])
+  bitCombinations.append([true, false])
+  bitCombinations.append([false, true])
+  bitCombinations.append([true, true])
   
   // Next: print a truth table to the console for each given input. Exit the
   // program early and do not render the output.
+  for bitCombinationID in bitCombinations.indices {
+    let bitCombination = bitCombinations[bitCombinationID]
+    descriptor.inputBits = bitCombination
+    
+    var simulation = Simulation(descriptor: descriptor)
+    for _ in 0..<300 {
+      simulation.step()
+    }
+    
+    print(bitCombination[0] ? "1" : "0", "|", terminator: " ")
+    print(bitCombination[1] ? "1" : "0", "|", terminator: " ")
+    print(simulation.result.outputBit ? "1" : "0")
+  }
   
-  return simulation.result.frames
+  exit(0)
 }
 
 struct SimulationDescriptor {
