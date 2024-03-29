ace6d6d5a07c32f49a2e2e719854e1bc177f86fb
Philip Turner <philipturner.AR@gmail.com>
2024-03-29 17:46:46 -0400

    Set up scene for silicon surface simulation

--- Sources/MolecularRendererApp/Scratch/Scratch.swift
+++ Sources/MolecularRendererApp/Scratch/Scratch.swift
@@ -5,21 +5,23 @@ import HDL
 import MM4
 import Numerics
 
-// For profiling; there are alternative methods on Windows.
-import QuartzCore
-
 func createGeometry() -> [Entity] {
   // MARK: - Initializing Geometry
   
-  setenv("OMP_STACKSIZE", "2G", 1)
-  setenv("OMP_NUM_THREADS", "8", 1)
-  
-  var descriptor = LonsdaleiteRodDescriptor()
-  descriptor.atomicLayerCount = 39
-  let lonsdaleiteRod = LonsdaleiteRod(descriptor: descriptor)
+  let surface = Surface()
+  do {
+    let atoms = surface.topology.atoms
+    print("silicons:", atoms.filter { $0.atomicNumber == 14 }.count)
+    print("hydrogens:", atoms.filter { $0.atomicNumber == 1 }.count)
+    print("anchors:", surface.anchors.count)
+  }
   
   // MARK: - Initializing xTB
   
+  // Configure xTB for the highest performance possible without a custom
+  // linear algebra library.
+  setenv("OMP_STACKSIZE", "2G", 1)
+  setenv("OMP_NUM_THREADS", "8", 1)
   XTBLibrary.loadLibrary(
     path: "/Users/philipturner/Documents/OpenMM/bypass_dependencies/libxtb.6.dylib")
   
@@ -27,245 +29,82 @@ func createGeometry() -> [Entity] {
   let calc = xtb_newCalculator()!
   let res = xtb_newResults()!
   let mol = createMolecule(
-    env: env, atoms: lonsdaleiteRod.topology.atoms, charge: 0, uhf: 0)
+    env: env, atoms: surface.topology.atoms, charge: 0, uhf: 0)
   initializeEnvironment(
     env: env, mol: mol, calc: calc, verbosityLevel: XTB_VERBOSITY_MINIMAL)
   updateMolecule(
-    env: env, mol: mol, atoms: lonsdaleiteRod.topology.atoms)
+    env: env, mol: mol, atoms: surface.topology.atoms)
   
   xtb_singlepoint(env, mol, calc, res)
-  
-  // Find the initial potential energy.
-  var initialPotentialEnergy: Double = .zero
-  xtb_getEnergy(env, res, &initialPotentialEnergy)
+  var startEnergy: Double = .zero
+  xtb_getEnergy(env, res, &startEnergy)
   guard xtb_checkEnvironment(env) == 0 else {
     fatalError("Environment is bad.")
   }
-  initialPotentialEnergy *= 4360
+  print("initial potential energy:", startEnergy)
   
-  // MARK: - Molecular Dynamics Simulation
-  
-  var currentAtoms = lonsdaleiteRod.topology.atoms
-  var velocities = [SIMD3<Float>](
-    repeating: .zero, count: currentAtoms.count)
-  let masses = createMasses(atoms: currentAtoms)
-  
-  var singlepointTimes: [Double] = []
-  var gradientTimes: [Double] = []
-  
-  for frameID in 0..<5 {
-    // Perform the singlepoint calculation.
-    updateMolecule(env: env, mol: mol, atoms: currentAtoms)
-    do {
-      let start = CACurrentMediaTime()
-      xtb_singlepoint(env, mol, calc, res)
-      let end = CACurrentMediaTime()
-      let elapsedTime = end - start
-      
-      // The very first frame is polluted by the initial singlepoint.
-      if frameID > 0 {
-        singlepointTimes.append(elapsedTime)
-      }
-    }
-    
-    // Retrieve the forces.
-    var forces: [SIMD3<Float>]
-    do {
-      let start = CACurrentMediaTime()
-      forces = createForces(
-        env: env, mol: mol, calc: calc, res: res,
-        atomCount: currentAtoms.count)
-      let end = CACurrentMediaTime()
-      let elapsedTime = end - start
-      
-      // The very first frame is polluted by the initial singlepoint.
-      if frameID > 0 {
-        gradientTimes.append(elapsedTime)
-      }
-    }
-    guard xtb_checkEnvironment(env) == 0 else {
-      fatalError("Environment is bad.")
-    }
-    
-    // Determine the potential energy.
-    var potentialEnergy: Double = .zero
-    xtb_getEnergy(env, res, &potentialEnergy)
-    guard xtb_checkEnvironment(env) == 0 else {
-      fatalError("Environment is bad.")
-    }
-    potentialEnergy *= 4360
-    
-    // Determine the kinetic energy.
-    var kineticEnergy: Double = .zero
-    for atomID in currentAtoms.indices {
-      let mass = masses[atomID]
-      let velocity = velocities[atomID]
-      let energy = 0.5 * mass * (velocity * velocity).sum()
-      kineticEnergy += Double(energy)
-    }
-    
-    // Report the energy conservation to the console.
-    let drift = (kineticEnergy + potentialEnergy) - initialPotentialEnergy
-    print("frame:", frameID, terminator: " | ")
-    print(
-      "\(String(format: "%.1f", kineticEnergy)) zJ (kinetic)",
-      terminator: " | ")
-    print(
-      "\(String(format: "%.1f", potentialEnergy)) zJ (potential)",
-      terminator: " | ")
-    print(
-      "\(String(format: "%.1f", drift)) zJ (drift)",
-      terminator: " | ")
-    
-    // Terminate the log statement with a newline.
-    print()
-    
-    // Integrate over time with the velocity Verlet integrator.
-    for atomID in currentAtoms.indices {
-      var atom = currentAtoms[atomID]
-      var velocity = velocities[atomID]
-      let force = forces[atomID]
-      let mass = masses[atomID]
-      
-      // Update the velocity.
-      var momentum = velocity * mass
-      momentum += 0.002 * force
-      velocity = momentum / mass
-      
-      // Update the position.
-      atom.position += velocity * 0.002
-      
-      // Save the atom's state to memory.
-      currentAtoms[atomID] = atom
-      velocities[atomID] = velocity
-    }
-  }
-  
-  func summarizeTimes(_ times: [Double]) {
-    var minimum: Double = 1e38
-    var accumulator: Double = .zero
-    var maximum: Double = .zero
-    
-    for timeID in times.indices {
-      let time = times[timeID]
-      minimum = min(minimum, time)
-      accumulator += time
-      maximum = max(maximum, time)
-    }
-    let average = accumulator / Double(times.count)
-    
-    print("- maximum: \(Int(maximum * 1e6)) μs")
-    print("- average: \(Int(average * 1e6)) μs")
-    print("- minimum: \(Int(minimum * 1e6)) μs")
-  }
-  
-  print()
-  print("system size:")
-  print("- atoms:", currentAtoms.count, terminator: " ")
-  var carbonAtomCount: Int = .zero
-  for atomID in currentAtoms.indices {
-    let atom = currentAtoms[atomID]
-    if atom.atomicNumber == 6 {
-      carbonAtomCount += 1
-    }
-  }
-  print("(\(carbonAtomCount) carbons)")
-  
-  var electronCount: Int = .zero
-  for atomID in currentAtoms.indices {
-    let atom = currentAtoms[atomID]
-    electronCount += Int(atom.atomicNumber)
-  }
-  print("- electrons:", electronCount)
-  print("- orbitals:", electronCount / 2)
-  
-  print()
-  print("singlepoint latency:")
-  summarizeTimes(singlepointTimes)
-  
-  print()
-  print("gradient latency:")
-  summarizeTimes(gradientTimes)
-  
-  // Report the linear algebra metric, GFLOPS/k.
-  let n: Int = electronCount / 2
-  let minLatency = singlepointTimes.min()!
-  let mflopsK = Double(n * n * n) / Double(minLatency) / 1e6
-  print()
-  print("MFLOPS/k:", mflopsK, "* 2 * SCF iters")
-  
-  // The fastest samples typically had 6 self-consistent field iterations.
-  //  24 orbitals | MFLOPS/k: 1.672308637821706 * 2 * SCF iters
-  //  45 orbitals | MFLOPS/k: 2.850558771998712 * 2 * SCF iters
-  //  66 orbitals | MFLOPS/k: 4.264920785466363 * 2 * SCF iters
-  //  87 orbitals | MFLOPS/k: 6.719695497555669 * 2 * SCF iters
-  // 108 orbitals | MFLOPS/k: 7.817894503753794 * 2 * SCF iters
-  // 129 orbitals | MFLOPS/k: 7.040054789433814 * 2 * SCF iters
-  // 150 orbitals | MFLOPS/k: 10.617432539617518 * 2 * SCF iters
-  // 171 orbitals | MFLOPS/k: 11.824962467189183 * 2 * SCF iters
-  // 192 orbitals | MFLOPS/k: 14.864456295740977 * 2 * SCF iters
-  // 213 orbitals | MFLOPS/k: 16.72527140353499 * 2 * SCF iters
-  // 234 orbitals | MFLOPS/k: 18.577699670110036 * 2 * SCF iters
-  // 255 orbitals | MFLOPS/k: 21.400103087540572 * 2 * SCF iters
-  // 276 orbitals | MFLOPS/k: 25.673197216320762 * 2 * SCF iters
-  // 297 orbitals | MFLOPS/k: 26.257907112019282 * 2 * SCF iters
-  // 318 orbitals | MFLOPS/k: 31.986542563476643 * 2 * SCF iters
-  // 339 orbitals | MFLOPS/k: 34.261739645398904 * 2 * SCF iters
-  // 360 orbitals | MFLOPS/k: 39.19743800150395 * 2 * SCF iters
-  // 423 orbitals | MFLOPS/k: 50.09500766413211 * 2 * SCF iters
-  // 528 orbitals | MFLOPS/k: 64.13679686045143 * 2 * SCF iters
-  // 633 orbitals | MFLOPS/k: 83.85597804067446 * 2 * SCF iters
-  // 738 orbitals | MFLOPS/k: 103.02688576953425 * 2 * SCF iters
-  // 801 orbitals | MFLOPS/k: 111.85982029565841 * 2 * SCF iters
-  // 822 orbitals | MFLOPS/k: 126.25993070649956 * 2 * SCF iters
-  // Anything larger crashes at runtime.
-  
-  
-  
-  exit(0)
+  return surface.topology.atoms
 }
 
 // MARK: - Geometry
 
-struct LonsdaleiteRodDescriptor {
-  var atomicLayerCount: Int?
-}
-
-// A structure whose size can be systematically varied to benchmark
-// simulation speed.
-struct LonsdaleiteRod {
+struct Surface {
   var topology = Topology()
+  var anchors: Set<UInt32> = []
   
-  init(descriptor: LonsdaleiteRodDescriptor) {
-    createLattice(descriptor: descriptor)
+  init() {
+    createLattice()
     passivate()
+    minimize()
+    align()
+    depassivate()
   }
   
-  mutating func createLattice(descriptor: LonsdaleiteRodDescriptor) {
-    guard let atomicLayerCount = descriptor.atomicLayerCount else {
-      fatalError("Descriptor not complete.")
-    }
-    let lattice = Lattice<Hexagonal> { h, k, l in
-      let h2k = h + 2 * k
-      let dimensionL = Float((atomicLayerCount + 1) / 2)
-      Bounds { 1 * h + 1 * h2k + dimensionL * l }
-      Material { .elemental(.carbon) }
+  // Add the bulk silicon atoms.
+  mutating func createLattice() {
+    let lattice = Lattice<Cubic> { h, k, l in
+      Bounds { 2 * (h + k + l) }
+      Material { .elemental(.silicon) }
       
-      if atomicLayerCount % 2 == 1 {
-        Volume {
-          Origin { (Float(atomicLayerCount) / 2 - 0.01) * l }
-          Plane { l }
-          Replace { .empty }
+      Volume {
+        Origin { 1 * (h + k + l) }
+        
+        Convex {
+          Origin { 0.5 * (h + k + l) }
+          Plane { h + k + l }
         }
+        Convex {
+          Origin { -0.25 * (h + k + l) }
+          Plane { -h - k - l }
+        }
+        
+        var directions: [SIMD3<Float>] = []
+        directions.append(h - k - l)
+        directions.append(-h + k - l)
+        directions.append(-h - k + l)
+        
+        for direction in directions {
+          Convex {
+            Origin { 0.5 * direction }
+            Plane { direction }
+          }
+        }
+        for direction in directions {
+          Convex {
+            Origin { -0.75 * direction }
+            Plane { -direction }
+          }
+        }
+        Replace { .empty }
       }
     }
     topology.insert(atoms: lattice.atoms)
   }
   
-  // Adds hydrogens and sorts the atoms in Morton order.
+  // Add hydrogens to the surfaces.
   mutating func passivate() {
     var reconstruction = SurfaceReconstruction()
-    reconstruction.material = .elemental(.carbon)
+    reconstruction.material = .elemental(.silicon)
     reconstruction.topology = topology
     reconstruction.removePathologicalAtoms()
     reconstruction.createBulkAtomBonds()
@@ -274,6 +113,77 @@ struct LonsdaleiteRod {
     reconstruction.createHydrogenBonds()
     topology = reconstruction.topology
     topology.sort()
+  }
+  
+  // Align the structure, so its principal axes align with the viewer.
+  mutating func align() {
+    var paramsDesc = MM4ParametersDescriptor()
+    paramsDesc.atomicNumbers = topology.atoms.map(\.atomicNumber)
+    paramsDesc.bonds = topology.bonds
+    let parameters = try! MM4Parameters(descriptor: paramsDesc)
+    
+    var rigidBodyDesc = MM4RigidBodyDescriptor()
+    rigidBodyDesc.parameters = parameters
+    rigidBodyDesc.positions = topology.atoms.map(\.position)
+    let rigidBody = try! MM4RigidBody(descriptor: rigidBodyDesc)
+    
+    let axesFP64 = rigidBody.principalAxes
+    let axesFP32 = (SIMD3<Float>(axesFP64.0),
+                    SIMD3<Float>(axesFP64.1),
+                    SIMD3<Float>(axesFP64.2))
+    let centerOfMass = SIMD3<Float>(rigidBody.centerOfMass)
+    
+    for atomID in topology.atoms.indices {
+      var atom = topology.atoms[atomID]
+      var position = atom.position
+      position -= centerOfMass
+      position = SIMD3((position * axesFP32.2).sum(),
+                       (position * axesFP32.1).sum(),
+                       (position * axesFP32.0).sum())
+      atom.position = position
+      topology.atoms[atomID] = atom
+    }
+  }
+  
+  // Minimize the structure, assuming it's fully passivated.
+  mutating func minimize() {
+    var paramsDesc = MM4ParametersDescriptor()
+    paramsDesc.atomicNumbers = topology.atoms.map(\.atomicNumber)
+    paramsDesc.bonds = topology.bonds
+    let parameters = try! MM4Parameters(descriptor: paramsDesc)
+    
+    var forceFieldDesc = MM4ForceFieldDescriptor()
+    forceFieldDesc.parameters = parameters
+    let forceField = try! MM4ForceField(descriptor: forceFieldDesc)
+    forceField.positions = topology.atoms.map(\.position)
+    forceField.minimize()
+    
+    for atomID in topology.atoms.indices {
+      var atom = topology.atoms[atomID]
+      let position = forceField.positions[atomID]
+      atom.position = position
+      topology.atoms[atomID] = atom
+    }
+  }
+  
+  // Remove hydrogens from the surface.
+  //
+  // Right now, this step does something else. It detects hydrogens that will
+  // be removed. Then, it sets the remaining hydrogens as anchors. In the
+  // future, the setting of anchors should occur strictly after the hydrogens
+  // have been removed.
+  mutating func depassivate() {
+    for atomID in topology.atoms.indices {
+      let atom = topology.atoms[atomID]
+      guard atom.atomicNumber == 1 else {
+        continue
+      }
+      if atom.position.z > 0.22 {
+        // Remove this atom eventually.
+      } else {
+        anchors.insert(UInt32(atomID))
+      }
+    }
   }
 }
 
@@ -375,7 +285,11 @@ func createForces(
   res: xtb_TResults,
   atomCount: Int
 ) -> [SIMD3<Float>] {
-  // WARNING: You must have already called xtb_singlepoint.
+  xtb_singlepoint(env, mol, calc, res)
+  if xtb_checkEnvironment(env) != 0 {
+    fatalError("Call xtb_showEnvironment.")
+  }
+  
   var gradient = [Double](repeating: 0, count: atomCount * 3)
   xtb_getGradient(env, res, &gradient)
   if xtb_checkEnvironment(env) != 0 {
