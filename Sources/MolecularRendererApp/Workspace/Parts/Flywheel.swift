6f973cc25bddf88f3147c02b6bebae3719f9a1be
Philip Turner <philipturner.AR@gmail.com>
2024-04-01 19:50:10 -0400

    Preliminary results of running this through MM4ForceField

--- Sources/MolecularRendererApp/Workspace/Parts/Flywheel.swift
+++ Sources/MolecularRendererApp/Workspace/Parts/Flywheel.swift
@@ -55,14 +55,155 @@ struct Flywheel {
     // - Other values of X are mapped into the angular coordinate with a linear
     //   transformation. Anything outside of the range will overshoot and
     //   potentially overlap another chunk of matter.
-    let perimeter = Float(120 + 1) * latticeConstant
+    let perimeter = Float(120) * latticeConstant
     
     // The distance between Y = 0 in the compiled lattice's coordinate space,
     // and the center of the warped circle.
     let curvatureRadius: Float = 5.0
     
+    for atomID in reconstruction.topology.atoms.indices {
+      var atom = reconstruction.topology.atoms[atomID]
+      var position = atom.position
+      let θ = 2 * Float.pi * (position.x - 0) / perimeter
+      let r = curvatureRadius + position.y
+      position.x = r * Float.cos(θ)
+      position.y = r * Float.sin(θ)
+      atom.position = position
+      reconstruction.topology.atoms[atomID] = atom
+    }
+    
+    // 21828 atoms before
+    // 21600 atoms after
+    reconstruction.topology = deduplicate(topology: reconstruction.topology)
     reconstruction.topology.sort()
     return reconstruction.topology
+  }
+  
+  private static func deduplicate(topology: Topology) -> Topology {
+    let matches = topology.match(
+      topology.atoms, algorithm: .absoluteRadius(0.010))
+    let atomsToAtomsMap = topology.map(.atoms, to: .atoms)
+    var removedAtoms: Set<UInt32> = []
+    var insertedBonds: Set<SIMD2<UInt32>> = []
+    
+    for i in topology.atoms.indices {
+      let atomI = topology.atoms[i]
+      guard matches[i].count > 1 else {
+        continue
+      }
+      precondition(matches[i].count == 2, "Too many overlapping atoms.")
+      
+      var j: Int = -1
+      for match in matches[i] where i != match {
+        j = Int(match)
+      }
+      let atomJ = topology.atoms[j]
+      precondition(atomI.atomicNumber == atomJ.atomicNumber)
+      
+      // Choose the carbon with the lowest index, or the H duplicate associated
+      // with that carbon.
+      let neighborsI = atomsToAtomsMap[i]
+      let neighborsJ = atomsToAtomsMap[j]
+      precondition(neighborsI.count == neighborsJ.count)
+      if atomI.atomicNumber == 1 {
+        precondition(neighborsI.count == 1)
+        guard neighborsI.first! < neighborsJ.first! else {
+          continue
+        }
+      } else {
+        precondition(neighborsI.count == 4)
+        guard i < j else {
+          continue
+        }
+      }
+      
+      if atomI.atomicNumber == 1 {
+        removedAtoms.insert(UInt32(j))
+        continue
+      }
+      
+      struct Orbital {
+        var neighborID: UInt32
+        var neighborElement: UInt8
+        var delta: SIMD3<Float>
+      }
+      func createOrbitals(_ index: Int) -> [Orbital] {
+        let neighbors = atomsToAtomsMap[index]
+        let selfAtom = topology.atoms[index]
+        var output: [Orbital] = []
+        for neighborID in neighbors {
+          let otherAtom = topology.atoms[Int(neighborID)]
+          var delta = otherAtom.position - selfAtom.position
+          delta /= (delta * delta).sum().squareRoot()
+          output.append(Orbital(
+            neighborID: neighborID,
+            neighborElement: otherAtom.atomicNumber,
+            delta: delta))
+        }
+        return output
+      }
+      let orbitalsI = createOrbitals(i)
+      var orbitalsJ = createOrbitals(j)
+      var orbitalJMatches: [Int] = []
+      for orbitalJ in orbitalsJ {
+        var maxScore: Float = -.greatestFiniteMagnitude
+        var maxIndex: Int = -1
+        for indexI in 0..<4 {
+          let orbitalI = orbitalsI[indexI]
+          let score = (orbitalI.delta * orbitalJ.delta).sum()
+          if score > maxScore {
+            maxScore = score
+            maxIndex = indexI
+          }
+        }
+        precondition(maxIndex >= 0)
+        precondition(!orbitalJMatches.contains(maxIndex))
+        orbitalJMatches.append(maxIndex)
+      }
+      let nullOrbital = Orbital(
+        neighborID: 0, neighborElement: 0, delta: .zero)
+      var newOrbitalsJ = Array(repeating: nullOrbital, count: 4)
+      for indexJ in 0..<4 {
+        let maxIndex = orbitalJMatches[indexJ]
+        newOrbitalsJ[maxIndex] = orbitalsJ[indexJ]
+      }
+      orbitalsJ = newOrbitalsJ
+      
+      for (orbitalI, orbitalJ) in zip(orbitalsI, orbitalsJ) {
+        switch (orbitalI.neighborElement, orbitalJ.neighborElement) {
+        case (1, 1):
+          // The overlapping hydrogens should already be removed.
+          break
+        case (6, 6), (6, 32), (32, 6), (32, 32):
+          if orbitalI.neighborID < orbitalJ.neighborID {
+            // The sigma bond to the other carbon was duplicated, and will be
+            // automatically removed.
+            break
+          } else {
+            fatalError("Edge case not handled.")
+          }
+        case (6, 1), (32, 1):
+          // The overlapping hydrogen and carbon are not already removed by
+          // other code.
+          removedAtoms.insert(orbitalJ.neighborID)
+        case (1, 6), (1, 32):
+          // The hydrogen from the first atom must be superseded by the carbon
+          // from the second atom. That carbon is not registered as overlapping
+          // anything, because its position differs from the replaced hydrogen.
+          precondition(!removedAtoms.contains(orbitalJ.neighborID))
+          removedAtoms.insert(orbitalI.neighborID)
+          insertedBonds.insert(SIMD2(UInt32(i), orbitalJ.neighborID))
+        default:
+          fatalError("Unrecognized bond.")
+        }
+      }
+      removedAtoms.insert(UInt32(j))
+    }
+    
+    var output = topology
+    output.insert(bonds: Array(insertedBonds))
+    output.remove(atoms: Array(removedAtoms))
+    return output
   }
   
   static func createRigidBody(topology: Topology) -> MM4RigidBody {
--- Sources/MolecularRendererApp/Workspace/Workspace.swift
+++ Sources/MolecularRendererApp/Workspace/Workspace.swift
@@ -4,7 +4,36 @@ import MM4
 import Numerics
 
 func createGeometry() -> [Entity] {
-  let lattice = Flywheel.createLattice()
-  let topology = Flywheel.createTopology(lattice: lattice)
-  return topology.atoms
+  let flywheel = Flywheel()
+  
+  var forceFieldDesc = MM4ForceFieldDescriptor()
+  forceFieldDesc.parameters = flywheel.rigidBody.parameters
+  forceFieldDesc.integrator = .multipleTimeStep
+  let forceField = try! MM4ForceField(descriptor: forceFieldDesc)
+  forceField.positions = flywheel.rigidBody.positions
+  
+  print(forceField.energy.potential)
+  
+  // 132396232.265625
+  //   3993967.810546875
+  //   2308207.91796875
+  
+  let forces = forceField.forces
+  var maxForceMagnitude: Float = .zero
+  for force in forces {
+    let magnitude = (force * force).sum().squareRoot()
+    maxForceMagnitude = max(maxForceMagnitude, magnitude)
+    if magnitude.isInfinite || magnitude.isNaN {
+      print("NAN force")
+    }
+  }
+  print(maxForceMagnitude)
+  
+  forceField.minimize()
+  print(forceField.energy.potential)
+  
+  // -538450.728515625
+  // -583069.953125
+  
+  exit(0)
 }
