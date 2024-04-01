9dea73ee6920fa8f3d101a393ef4246502882b52
Philip Turner <philipturner.AR@gmail.com>
2024-04-01 08:30:52 -0400

    Surface reconstruction is not complete. That was the source of these struggles with corners.

--- Sources/MolecularRendererApp/Workspace/Workspace.swift
+++ Sources/MolecularRendererApp/Workspace/Workspace.swift
@@ -3,29 +3,33 @@ import HDL
 import MM4
 import Numerics
 
-func createGeometry() -> [Entity] {
+func createGeometry() -> [MM4RigidBody] {
   // Modify the shape of the drive wall. It must move the rods by 1 nm over the
   // clock cycle, while keeping them positionally constrained.
-  let lattice = DriveWall.createLattice()
-  return lattice.atoms
+//  let lattice = DriveWall.createLattice()
+//  let topology = DriveWall.createTopology(lattice: lattice)
+//  return topology.atoms
   
-//  driveWall.rigidBody.centerOfMass.x -= 5.0
-//
-//  // Render the scene.
-//  var output: [MM4RigidBody] = []
-//  output.append(driveWall.rigidBody)
-//  for indexX in 0..<3 {
-//    for indexY in 0..<3 {
-//      var rigidBody = rod.rigidBody
-//      rigidBody.centerOfMass += SIMD3(0, 0.85, 0.91)
-//      
-//      let latticeConstant = Double(Constant(.square) { .elemental(.carbon) })
-//      rigidBody.centerOfMass.z += Double(indexX) * 5.75 * latticeConstant
-//      rigidBody.centerOfMass.y += Double(indexY) * 6 * latticeConstant
-//      output.append(rigidBody)
-//    }
-//  }
-//  return output
+  var driveWall = DriveWall()
+  driveWall.rigidBody.centerOfMass.x -= 5.0
+  
+  var rod = Rod()
+
+  // Render the scene.
+  var output: [MM4RigidBody] = []
+  output.append(driveWall.rigidBody)
+  for indexX in 0..<3 {
+    for indexY in 0..<3 {
+      var rigidBody = rod.rigidBody
+      rigidBody.centerOfMass += SIMD3(0, 0.85, 0.91)
+      
+      let latticeConstant = Double(Constant(.square) { .elemental(.carbon) })
+      rigidBody.centerOfMass.z += Double(indexX) * 6 * latticeConstant
+      rigidBody.centerOfMass.y += Double(indexY) * 6 * latticeConstant
+      output.append(rigidBody)
+    }
+  }
+  return output
 }
 
 // MARK: - Parts
@@ -96,8 +100,75 @@ struct DriveWall {
   // Compiles the carbon centers.
   static func createLattice() -> Lattice<Cubic> {
     Lattice<Cubic> { h, k, l in
-      Bounds { 10 * h + 19 * k + 18 * l }
+      Bounds { 10 * h + 21 * k + 18 * l }
       Material { .elemental(.carbon) }
+      
+      func createHoleX(offset: SIMD3<Float>) {
+        Concave {
+          Origin { offset[0] * h + offset[1] * k + offset[2] * l }
+          Origin { 1.5 * k + 1.5 * l }
+          
+          // Create a groove for the rod.
+          Concave {
+            Plane { k }
+            Plane { l }
+            Origin { 6.25 * k + 4 * l }
+            Plane { -k }
+            Plane { -l }
+          }
+          
+          Concave {
+            Origin { 2 * h }
+            
+            // Create a 45-degree inclined plane.
+            Plane { h - k }
+            
+            // Correct the walls of the shaft.
+            Convex {
+              Origin { 4 * l }
+              Origin { 0.25 * (k - l) }
+              Plane { k - l }
+            }
+            
+            // Correct the walls of the shaft.
+            Convex {
+              Origin { 0.25 * (k + l) }
+              Plane { k + l }
+            }
+            
+            // Correct the concave corner of the drive wall site.
+            Convex {
+              Origin { 1.75 * h }
+              Plane { h }
+            }
+            
+            // Correct the concave corner of the drive wall site.
+            Convex {
+              Origin { 2 * h }
+              Plane { h + k }
+            }
+            
+            // Correct the concave corner of the drive wall site.
+            Convex {
+              Origin { 1.75 * h + 0.5 * (h + k + l) }
+              Plane { h + k + l }
+            }
+          }
+        }
+      }
+      
+      Volume {
+        for indexX in 0..<3 {
+          for indexY in 0..<3 {
+            var offset: SIMD3<Float> = .zero
+            offset.z += Float(indexX) * 6
+            offset.y += Float(indexY) * 6
+            createHoleX(offset: offset)
+          }
+        }
+        
+        Replace { .empty }
+      }
     }
   }
   
