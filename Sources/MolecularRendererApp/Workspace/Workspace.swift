37155df9320f41fec6c8cb93185f63cfd1c079ff
Philip Turner <philipturner.AR@gmail.com>
2024-04-03 20:12:13 -0400

    Save a commit containing the correct method for positioning the tripod

--- Sources/MolecularRendererApp/Workspace/Workspace.swift
+++ Sources/MolecularRendererApp/Workspace/Workspace.swift
@@ -79,32 +79,114 @@ import OpenMM
 // Credits
 // - Author
 // - Music
-// - Inspiration (for mechanosynthesis and computing)
+// - Inspiration (for mechanosynthesis and rod logic)
 
 func createGeometry() -> [Entity] {
-  // First task:
-  // - Find Si-Cl, Cl-Cl bond length with xTB.
-  // - Record Si-H, Si-Cl, Cl-Cl bond length in a source file.
-  //
   // Second task:
   // - Compile and minimize the tripod tooltips.
+  //   - Include the SiH3 group on each leg in the simulation.
+  //   - Anchor every atom in the SiH3 groups.
   // - Serialize the tripods as Swift source code.
   
-  var atoms: [Entity] = []
-  atoms.append(Entity(position: .zero, type: .atom(.chlorine)))
+  var tripod = CBNTripod()
+  tripod.rotateLegs(slantAngleDegrees: 62, swingAngleDegrees: 5)
   
-  let bondLength = 2 * Element.chlorine.covalentRadius
-  let position = SIMD3<Float>(bondLength, 0, 0)
-  atoms.append(Entity(position: position, type: .atom(.chlorine)))
+  #if true
+  for legID in tripod.legs.indices {
+    var topology = tripod.legs[legID].topology
+    
+    // NOTE: Ensure the Si-H bonds are oriented the same way as in the bulk.
+    var insertedAtoms: [Entity] = []
+    var insertedBonds: [SIMD2<UInt32>] = []
+    for atomID in topology.atoms.indices {
+      let atom = topology.atoms[atomID]
+      guard atom.atomicNumber == 14 else {
+        continue
+      }
+      
+      // Two possibilities for energy minimum:
+      // - triangle length = 0.768 nm
+      // - triangle length = 1.016 nm
+      var silicon = atom
+      print(silicon.position)
+      
+      var siliconXZVector = SIMD2(silicon.position.x, silicon.position.z)
+      siliconXZVector /= (siliconXZVector * siliconXZVector).sum().squareRoot()
+      siliconXZVector *= 1.016 / Float(3).squareRoot()
+      silicon.position.x = siliconXZVector[0]
+      silicon.position.z = siliconXZVector[1]
+      topology.atoms[atomID] = silicon
+      print(silicon.position)
+      
+      var baseOrbital: SIMD3<Float> = .init(0, 1, 0)
+      let baseAngle: Float = 109.5 * .pi / 180
+      let baseRotation = Quaternion(angle: baseAngle, axis: [0, 0, 1])
+      baseOrbital = baseRotation.act(on: baseOrbital)
+      
+      for orbitalID in 0..<3 {
+        let angle = Float(orbitalID) * .pi * 2 / 3
+        let orbitalRotation = Quaternion(angle: angle, axis: [0, 1, 0])
+        let orbital = orbitalRotation.act(on: baseOrbital)
+        
+        // Source: MM4 parameters
+        let chBondLength: Float = 1.483 / 10
+        let position = silicon.position + orbital * chBondLength
+        let hydrogen = Entity(position: position, type: .atom(.hydrogen))
+        
+        let hydrogenID = topology.atoms.count + insertedAtoms.count
+        let bond = SIMD2(UInt32(atomID), UInt32(hydrogenID))
+        insertedAtoms.append(hydrogen)
+        insertedBonds.append(bond)
+      }
+    }
+    
+    topology.insert(atoms: insertedAtoms)
+    topology.insert(bonds: insertedBonds)
+    tripod.legs[legID].topology = topology
+  }
+  #endif
+//  tripod.passivateNHGroups(.hydrogen)
   
-  print(atoms)
+  let lattice = Lattice<Hexagonal> { h, k, l in
+    let h2k = h + 2 * k
+    Bounds { 10 * h + 10 * h2k + 3 * l }
+    Material { .elemental(.silicon) }
+  }
+  var latticeAtoms = lattice.atoms
+  for atomID in latticeAtoms.indices {
+    var atom = latticeAtoms[atomID]
+    var position = atom.position
+    position = SIMD3(position.x, position.z, position.y)
+    
+    atom.position = position
+    latticeAtoms[atomID] = atom
+  }
   
-  var solver = XTBSolver(cpuID: 0)
-  solver.atoms = atoms
-  solver.solve(arguments: ["--opt"])
-  solver.load()
+  var tripodAtoms = tripod.createAtoms()
+  for atomID in tripodAtoms.indices {
+    var atom = tripodAtoms[atomID]
+    var position = atom.position
+    
+    let angle: Float = 0 * .pi / 180
+    let rotation = Quaternion(angle: angle, axis: [0, 1, 0])
+    position = rotation.act(on: position)
+    
+    let latticeConstant1 = Constant(.hexagon) { .elemental(.silicon) }
+    let latticeConstant2 = Constant(.prism) { .elemental(.silicon) }
+    position.y += 0.69417167
+    position.z += latticeConstant1 / Float(3).squareRoot()
+    print(Constant(.prism) { .elemental(.silicon) })
+    
+    position.x += latticeConstant1 * 3
+    position.y += latticeConstant2 * 2.625
+    position.z += latticeConstant1 * 3 * Float(3).squareRoot()
+    
+    atom.position = position
+    tripodAtoms[atomID] = atom
+  }
   
-  print(solver.atoms)
-  
-  return solver.atoms
+  var output: [Entity] = []
+  output += latticeAtoms
+  output += tripodAtoms
+  return output
 }
