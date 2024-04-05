a0204f4b4b86657233da905c995f6f591213b896
Philip Turner <philipturner.AR@gmail.com>
2024-04-04 20:31:36 -0400

    Postpone all animation work for now

--- Sources/MolecularRendererApp/Workspace/Workspace.swift
+++ Sources/MolecularRendererApp/Workspace/Workspace.swift
@@ -4,30 +4,12 @@ import MM4
 import Numerics
 import OpenMM
 
-// TODO: Finish the flywheel before doing bootstrapping animation.
-//
-// Tasks:
-// - Get the flywheel working with MD, publish short animation
-// - Don't have to get working method to link up to logic; just explain how
-//   that would work
-// - Don't have to finish patterning all the logic rods in the CLA; the
-//   existing progress is enough
-//
-// Flywheel System Animation:
-// - atoms materialize in Morton order, in exploded view
-// - energy-minimize from compiled to relaxed structure
-//   - show each frame of the minimization, if practical
-// - assemble parts on top of each other
-// - rotate so the flywheel points toward viewer
-// - molecular dynamics simulation (base64-encoded [Entity])
-
 func createGeometry() -> [MM4RigidBody] {
   // First task: fix up the "nano" patterning
   // Second task: set up the exploded view, and the more efficient minimization
   //
-  // It should be possible to finish both of these parts tonight, and get some
-  // initial testing of the MD sim. Tomorrow should be full simulation on AMD
-  // and recording/publishing the animation.
+  // Not yet the right time to publish an animation. Better use of time would
+  // be making more progress on rod logic.
   
   let driveSystem = DriveSystem()
   var output: [MM4RigidBody] = []
