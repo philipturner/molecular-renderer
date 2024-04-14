e0fd59cfce41689a35b674aabfd6f60633b5b795
Philip Turner <philipturner.AR@gmail.com>
2024-04-14 15:25:31 -0400

    A start to simulation

--- Sources/MolecularRendererApp/Workspace/Workspace.swift
+++ Sources/MolecularRendererApp/Workspace/Workspace.swift
@@ -4,36 +4,25 @@ import MM4
 import Numerics
 import OpenMM
 
-func createGeometry() -> [MM4RigidBody] {
-  // TODO: Jump right into experiments measuring flywheel system performance.
-  // Do not waste time creating a serializer.
-  //
-  // Set up the process for determining r and l. It can be scripted without
-  // the high-latency minimization, then recompiled with minimization included.
-  
+func createGeometry() -> [[MM4RigidBody]] {
   var driveSystem = DriveSystem()
+  driveSystem.minimize()
+  driveSystem.setVelocitiesToTemperature(2 * 298)
   
-  let flywheelData = DriveSystemPartData(source: driveSystem.flywheel)
-  let pistonData = DriveSystemPartData(source: driveSystem.piston)
-  let rotationAxis = driveSystem.flywheel.rigidBody.principalAxes.0
-  guard rotationAxis.z > 0.999 else {
-    fatalError("Flywheel was not aligned with expected reference frame.")
+  // [0] = connectingRod
+  // [1] = flywheel
+  // [2] = housing
+  // [3] = piston
+  var rigidBodies = driveSystem.rigidBodies
+  
+  var forceFieldParameters = rigidBodies[0].parameters
+  var forceFieldPositions = rigidBodies[0].positions
+  var forceFieldVelocities = rigidBodies[0].velocities
+  for rigidBody in rigidBodies[1...] {
+    forceFieldParameters.append(contentsOf: rigidBody.parameters)
+    forceFieldPositions.append(contentsOf: rigidBody.positions)
+    forceFieldVelocities.append(contentsOf: rigidBody.velocities)
   }
   
-  var rVector = flywheelData.knobCenter - flywheelData.bodyCenter
-  var lVector = flywheelData.knobCenter - pistonData.knobCenter
-  rVector -= (rVector * rotationAxis).sum() * rotationAxis
-  lVector -= (lVector * rotationAxis).sum() * rotationAxis
-  let r = (rVector * rVector).sum().squareRoot()
-  let l = (lVector * lVector).sum().squareRoot()
-  print("r:", r)
-  print("l:", l)
   
-  let frequencyInGHz: Double = 10.0
-  let ω = frequencyInGHz * 0.001 * (2 * .pi)
-  print("ω_f:", ω)
-  
-  
-  
-  exit(0)
 }
