331582f8e82d72502cd9387cea62ae046702266d
Philip Turner <philipturner.AR@gmail.com>
2024-04-18 06:37:32 -0400

    Comments on which crystal phase to use

--- Sources/MolecularRendererApp/Workspace/Workspace.swift
+++ Sources/MolecularRendererApp/Workspace/Workspace.swift
@@ -7,6 +7,11 @@ import OpenMM
 // Task:
 // - Design a revised system using polygonal bearings. Etch out a circular mask
 //   using the compiler. Cap the knobs to prevent part separation at 2 GHz.
+// - Use hexagonal diamond, which doesn't have as many warping issues. The
+//   bearing surfaces will also be more predictable and easier to control.
+//   - Try both cubic and hexagonal, see which one is more workable for the
+//     design of a single rotary bearing. Measure the friction and whether it
+//     can last for 3 cycles at 2-4 GHz.
 
 func createGeometry() -> [Entity] {
   let lattice = Lattice<Cubic> { h, k, l in
