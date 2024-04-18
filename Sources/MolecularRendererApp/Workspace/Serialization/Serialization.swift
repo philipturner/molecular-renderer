dd1e0d08b898d0cd48b4a74536ecf20282437d8d
Philip Turner <philipturner.AR@gmail.com>
2024-04-17 22:48:02 -0400

    Test the new and improved simulator

--- Sources/MolecularRendererApp/Workspace/Serialization/Serialization.swift
+++ Sources/MolecularRendererApp/Workspace/Serialization/Serialization.swift
@@ -11,120 +11,13 @@ import MM4
 import Numerics
 
 struct Serialization {
-  // Recover the bond topology after serializing the atom positions.
-  //
-  // The search algorithm uses a covalent bond scale of 1.4, instead of the
-  // value of 1.5 from the HDL.
-  static func reconstructBonds(
-    topology: inout Topology,
-    quaternaryAtomIDs: [UInt8]
-  ) {
-    // Only accept the 5 closest neighbors in the list.
-    let matches = topology.match(
-      topology.atoms,
-      algorithm: .covalentBondLength(1.4),
-      maximumNeighborCount: 5)
-    
-    var insertedBonds: [SIMD2<UInt32>] = []
-    for atomID in topology.atoms.indices {
-      let atom = topology.atoms[atomID]
-      let neighborIDs = matches[atomID]
-      if atom.atomicNumber == 1 {
-        if neighborIDs.count != 2 {
-          fatalError("Unexpected bond count: \(neighborIDs.count) != 2")
-        }
-      }
-      if quaternaryAtomIDs.contains(atom.atomicNumber) {
-        if neighborIDs.count < 5 {
-          fatalError("Unexpected bond count: \(neighborIDs.count) < 5")
-        }
-      }
-      
-      for neighborID in neighborIDs where atomID < neighborID {
-        let bond = SIMD2(UInt32(atomID), UInt32(neighborID))
-        insertedBonds.append(bond)
-      }
-    }
-    topology.insert(bonds: insertedBonds)
-  }
   
-  // Stores the raw data as a base-64 string.
-  //
-  // Compresses the information to within a factor of ~2x of the
-  // information-theoretic limit.
-  static func serialize(atoms: [Entity]) -> Data {
-    // Allocate an array for the raw data.
-    var rawData: [SIMD4<UInt16>] = []
-    
-    // Write to the header.
-    var header: SIMD4<UInt16> = .zero
-    header[0] = UInt16(atoms.count % 65536)
-    header[1] = UInt16(atoms.count / 65536)
-    rawData.append(header)
-    
-    for atom in atoms {
-      // Quantize the position to ~1 picometer precision.
-      var position = atom.position
-      position *= 1024
-      position += 32768
-      position.round(.toNearestOrEven)
-      
-      // Check that every atom falls within the bounds: 0 ≤ r < 64 nm.
-      if any(position .< 0) || any(position .>= 65536) {
-        fatalError("Position was out of range.")
-      }
-      
-      let chunk = SIMD4(
-        SIMD3<UInt16>(position), UInt16(atom.atomicNumber))
-      rawData.append(chunk)
-    }
-    
-    // Return the data for further processing in the caller.
-    return Data(bytes: rawData, count: rawData.count * 8)
-  }
-  
-  // Retrieves the raw data from a base-64 string.
-  static func deserialize(atoms: Data?) -> [Entity] {
-    guard let data = atoms else {
-      fatalError("Could not decode base64-encoded string.")
-    }
-    guard data.count % 8 == 0 else {
-      fatalError("Data was not aligned to 8 bytes.")
-    }
-    
-    var rawData = [SIMD4<UInt16>](repeating: .zero, count: data.count / 8)
-    rawData.withUnsafeMutableBytes {
-      let copiedCount = data.copyBytes(to: $0)
-      guard copiedCount == data.count else {
-        fatalError("Incorrect number of bytes was copied.")
-      }
-    }
-    
-    // Read the header.
-    let header = rawData[0]
-    let atomCount = UInt32(header[0]) + 65536 * UInt32(header[1])
-    guard atomCount == rawData.count - 1 else {
-      fatalError("Atom count was incorrect.")
-    }
-    
-    var atoms: [Entity] = []
-    for chunk in rawData[1...] {
-      var position = SIMD3<Float>(
-        unsafeBitCast(chunk, to: SIMD3<UInt16>.self))
-      position -= 32768
-      position /= 1024
-      
-      let atomicNumber = UInt8(chunk[3])
-      guard let element = Element(rawValue: atomicNumber) else {
-        fatalError("Unrecognized atomic number.")
-      }
-      let entity = Entity(position: position, type: .atom(element))
-      atoms.append(entity)
-    }
-    return atoms
-  }
-  
-  // Stores the raw data as a base-64 string.
+}
+
+// MARK: - Bonds
+
+extension Serialization {
+  // Compressed a bond topology into binary data.
   //
   // The typical compression ratio is ~2.5x. This is close to the
   // information-theoretic limit of ~3.5x.
@@ -192,19 +85,11 @@ struct Serialization {
     return Data(bytes: rawData, count: rawData.count * 8)
   }
   
-  // Retrieves the raw data from a base-64 string.
-  static func deserialize(bonds: Data?) -> [SIMD2<UInt32>] {
-    guard let data = bonds else {
-      fatalError("Could not decode base64-encoded string.")
-    }
-    guard data.count % 8 == 0 else {
-      fatalError("Data was not aligned to 8 bytes.")
-    }
-    
-    var rawData = [SIMD2<UInt32>](repeating: .zero, count: data.count / 8)
+  static func deserialize(bonds: Data) -> [SIMD2<UInt32>] {
+    var rawData = [SIMD2<UInt32>](repeating: .zero, count: bonds.count / 8)
     rawData.withUnsafeMutableBytes {
-      let copiedCount = data.copyBytes(to: $0)
-      guard copiedCount == data.count else {
+      let copiedCount = bonds.copyBytes(to: $0)
+      guard copiedCount == bonds.count else {
         fatalError("Incorrect number of bytes was copied.")
       }
     }
@@ -276,5 +161,130 @@ struct Serialization {
       return true
     })
     return bonds
+  }
+}
+
+// MARK: - Atoms
+
+extension Serialization {
+  // Encode an array of atoms, whose block-sparse volume does not exceed
+  // 67 million cubic nanometers. Blocks are 64x64x64 nm.
+  //
+  // Compresses the information to within a factor of ~2x of the
+  // information-theoretic limit.
+  static func serialize(atoms: [Entity]) -> Data {
+    // Allocate a dictionary for the chunk indices.
+    var chunkIDs: [SIMD3<UInt16>: UInt8] = [:]
+    var chunkCount: UInt8 = .zero
+    
+    // Allocate an array for the compressed atoms.
+    var compressedAtoms: [SIMD4<UInt16>] = []
+    
+    // Loop over the atoms.
+    for atom in atoms {
+      // Quantize the position to ~1 picometer precision.
+      var position = atom.position
+      position *= 1024
+      position.round(.toNearestOrEven)
+      
+      // Convert to a 32-bit integer.
+      let integerValue = SIMD3<Int32>(position)
+      let bitPattern = SIMD3<UInt32>(truncatingIfNeeded: integerValue)
+      
+      // Search for a matching sector.
+      let upperBits = SIMD3<UInt16>(truncatingIfNeeded: bitPattern &>> 16)
+      var chunkID: UInt8
+      if let matchingID = chunkIDs[upperBits] {
+        chunkID = matchingID
+      } else if chunkCount < UInt8.max {
+        chunkIDs[upperBits] = chunkCount
+        chunkID = chunkCount
+        chunkCount += 1
+      } else {
+        fatalError("Exceeded available vocabulary of 256 chunks.")
+      }
+      
+      // Encode the lower bits.
+      let lowerBits = SIMD3<UInt16>(truncatingIfNeeded: bitPattern)
+      
+      // Pack the atomic number and chunk ID into 16 bits.
+      let idPair = SIMD2<UInt8>(atom.atomicNumber, chunkID)
+      
+      // Create a 4-lane vector with the data.
+      let vector = SIMD4(lowerBits, unsafeBitCast(idPair, to: UInt16.self))
+      compressedAtoms.append(vector)
+    }
+    
+    // Allocate an array for the output data.
+    var output = [SIMD4<UInt16>](repeating: .zero, count: 257)
+    
+    // Create a header for the serialized data.
+    let atomCount = UInt64(atoms.count)
+    output[0] = unsafeBitCast(atomCount, to: SIMD4<UInt16>.self)
+    
+    // Encode the chunks.
+    for (key, value) in chunkIDs {
+      let vector = SIMD4(key, 0)
+      output[Int(1 + value)] = vector
+    }
+    
+    // Encode the atoms.
+    output += compressedAtoms
+    
+    // Return the data for further processing in the caller.
+    let byteCount = output.count * MemoryLayout<SIMD4<UInt16>>.stride
+    return Data(bytes: output, count: byteCount)
+  }
+  
+  static func deserialize(atoms: Data) -> [Entity] {
+    let wordCount = atoms.count / MemoryLayout<SIMD4<UInt16>>.stride
+    var rawData = [SIMD4<UInt16>](repeating: .zero, count: wordCount)
+    rawData.withUnsafeMutableBytes {
+      let copiedCount = atoms.copyBytes(to: $0)
+      guard copiedCount == atoms.count else {
+        fatalError("Incorrect number of bytes was copied.")
+      }
+    }
+    
+    // Read the header.
+    let header = rawData[0]
+    let atomCount = unsafeBitCast(header, to: UInt64.self)
+    guard 1 + 256 + atomCount == wordCount else {
+      fatalError("Atom count was incorrect.")
+    }
+    
+    // Loop over the atoms.
+    var output: [Entity] = []
+    for atomID in 0..<atomCount {
+      let vector = rawData[Int(257 + atomID)]
+      
+      // Decode the lower bits.
+      let lowerBits = SIMD3(vector[0], vector[1], vector[2])
+      
+      // Decode the atomic number and chunk ID.
+      let idPair = unsafeBitCast(vector[3], to: SIMD2<UInt8>.self)
+      
+      // Locate the matching sector.
+      let chunkID = idPair[1]
+      let chunkVector = rawData[Int(1) + Int(chunkID)]
+      let upperBits = unsafeBitCast(chunkVector, to: SIMD3<UInt16>.self)
+      
+      // Convert to a 32-bit integer.
+      var bitPattern: SIMD3<UInt32> = .zero
+      bitPattern |= SIMD3(truncatingIfNeeded: lowerBits)
+      bitPattern |= SIMD3(truncatingIfNeeded: upperBits) &<< 16
+      let integerValue = SIMD3<Int32>(truncatingIfNeeded: bitPattern)
+      
+      // Dequantize the position, from picometers to nanometers.
+      var position = SIMD3<Float>(integerValue)
+      position /= 1024
+      
+      // Decode the atomic number and convert to an entity.
+      let atomicNumber = UInt8(idPair[0])
+      let storage = SIMD4(position, Float(atomicNumber))
+      output.append(Entity(storage: storage))
+    }
+    
+    return output
   }
 }
--- Sources/MolecularRendererApp/Workspace/Workspace.swift
+++ Sources/MolecularRendererApp/Workspace/Workspace.swift
@@ -4,34 +4,25 @@ import MM4
 import Numerics
 import OpenMM
 
-func createGeometry() -> [[Entity]] {
-  // TODO: Design a revised system using polygonal bearings. Etch out a
-  // circular mask using the compiler. Cap the knobs to prevent part
-  // separation at 2 GHz.
-  
-  // Compile the drive system.
-  var driveSystem = DriveSystem()
-  driveSystem.minimize()
-  driveSystem.setVelocitiesToTemperature(2 * 77)
-  
-  // Transform the list of atoms into an array.
-  var systemAtoms: [Entity] = []
-  for rigidBody in driveSystem.rigidBodies {
-    for atomID in rigidBody.parameters.atoms.indices {
-      let atomicNumber = rigidBody.parameters.atoms.atomicNumbers[atomID]
-      let position = rigidBody.positions[atomID]
-      let entity = Entity(storage: SIMD4(position, Float(atomicNumber)))
-      systemAtoms.append(entity)
-    }
-  }
-  
-  // Create an animation to detect whether atoms are actually sorted.
-  var frames: [[Entity]] = []
-  for frameID in 1...720 {
-    let maxAtomProgress = systemAtoms.count * frameID / 720
-    let arraySlice = systemAtoms[..<maxAtomProgress]
-    frames.append(Array(arraySlice))
-  }
-  return frames
-}
+// Task:
+// - Design a revised system using polygonal bearings. Etch out a circular mask
+//   using the compiler. Cap the knobs to prevent part separation at 2 GHz.
 
+func createGeometry() -> [Entity] {
+  let lattice = Lattice<Cubic> { h, k, l in
+    Bounds { 300 * h + 6 * k + 6 * l }
+    Material { .elemental(.carbon) }
+  }
+  
+  var atoms = lattice.atoms
+  for atomID in atoms.indices {
+    var atom = atoms[atomID]
+    atom.position -= SIMD3(1, 1, 1)
+    atoms[atomID] = atom
+  }
+  
+  let serialized = Serialization.serialize(atoms: atoms)
+  let deserialized = Serialization.deserialize(atoms: serialized)
+  
+  return atoms + deserialized
+}
