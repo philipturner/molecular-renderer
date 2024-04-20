a05695b151e59e6221febf006bdf8bcd850e3c23
Philip Turner <philipturner.AR@gmail.com>
2024-04-20 19:19:26 -0400

    The new code for caching should be good

--- Sources/MolecularRendererApp/Workspace/CLA/CLAHousing.swift
+++ Sources/MolecularRendererApp/Workspace/CLA/CLAHousing.swift
@@ -19,17 +19,27 @@ struct CLAHousing: GenericPart {
   var rigidBody: MM4RigidBody
   
   init(descriptor: CLAHousingDescriptor) {
+    // Load the structure from disk.
     let lattice = Self.createLattice(rods: descriptor.rods)
-    var topology = Self.createTopology(lattice: lattice)
-    topology.bonds = Serialization.deserialize(bonds: Serialization.serialize(bonds: topology.bonds))
+    var cachedStructure: Topology?
+    if let cachePath = descriptor.cachePath {
+      let key = Self.hash(lattice: lattice)
+      cachedStructure = Self.load(key: key, cachePath: cachePath)
+    }
+    
+    // Assign the topology and rigid body.
+    var topology: Topology
+    if let cachedStructure {
+      topology = cachedStructure
+    } else {
+      topology = Self.createTopology(lattice: lattice)
+    }
     rigidBody = Self.createRigidBody(topology: topology)
     
+    // Save the structure to disk, regardless of whether it was already loaded.
     if let cachePath = descriptor.cachePath {
-      let key = Serialization.serialize(atoms: lattice.atoms)
-      let valueAtoms = Serialization.serialize(atoms: topology.atoms)
-      let valueBonds = Serialization.serialize(bonds: topology.bonds)
-      print(lattice.atoms.count, topology.atoms.count, topology.bonds.count)
-      print(key.count, valueAtoms.count, valueBonds.count)
+      let key = Self.hash(lattice: lattice)
+      save(key: key, cachePath: cachePath)
     }
     
     rigidBody.centerOfMass.z -= 18 * 0.3567
@@ -198,5 +208,139 @@ struct CLAHousing: GenericPart {
         Replace { .empty }
       }
     }
+  }
+}
+
+extension CLAHousing {
+  static func hash(lattice: Lattice<Cubic>) -> Data {
+    var xorHash: SIMD4<UInt32> = .zero
+    var rotateHash: SIMD4<UInt32> = .one
+    var floatSum: SIMD4<UInt32> = .zero
+    
+    for atom in lattice.atoms {
+      let storage = atom.storage
+      let storageCasted = unsafeBitCast(storage, to: SIMD4<UInt32>.self)
+      
+      xorHash ^= storageCasted
+      xorHash = (xorHash &<< 3) | (xorHash &>> (32 - 3))
+      
+      rotateHash &*= storageCasted
+      rotateHash &+= 1
+      rotateHash = (rotateHash &<< 9) | (rotateHash &>> (32 - 9))
+      
+      var quantized = atom.storage
+      quantized *= 1024
+      quantized.round(.toNearestOrEven)
+      floatSum &+= unsafeBitCast(quantized, to: SIMD4<UInt32>.self)
+    }
+    
+    var data = Data()
+    func addVector(_ vector: SIMD4<UInt32>) {
+      let castedVector = unsafeBitCast(vector, to: SIMD16<UInt8>.self)
+      for laneID in 0..<16 {
+        let byte = castedVector[laneID]
+        data.append(byte)
+      }
+    }
+    addVector(xorHash)
+    addVector(rotateHash)
+    addVector(floatSum)
+    return data
+  }
+  
+  static func load(key: Data, cachePath: String) -> Topology? {
+    // Load the structure from the disk.
+    let url = URL(fileURLWithPath: cachePath)
+    var data = try? Data(contentsOf: url)
+    guard let data else {
+      print("[CLAHousing] Cache miss: file not found.")
+      return nil
+    }
+    
+    // Decode the header.
+    guard data.count >= 32 else {
+      fatalError("Data had invalid header.")
+    }
+    var headerCasted: SIMD32<UInt8> = .zero
+    for laneID in 0..<32 {
+      let byte = data[laneID]
+      headerCasted[laneID] = byte
+    }
+    let header = unsafeBitCast(headerCasted, to: SIMD4<UInt64>.self)
+    
+    // Check that the file has the correct size.
+    let keySize = header[0]
+    let valueAtomsSize = header[1]
+    let valueBondsSize = header[2]
+    let expectedSize = 32 + keySize + valueAtomsSize + valueBondsSize
+    guard expectedSize == data.count else {
+      fatalError("File had the wrong size.")
+    }
+    
+    // Divide the file into segments.
+    var cursor: UInt64 = 32
+    let keyRange = cursor..<cursor + keySize
+    cursor += keySize
+    let valueAtomsRange = cursor..<cursor + valueAtomsSize
+    cursor += valueAtomsSize
+    let valueBondsRange = cursor..<cursor + valueBondsSize
+    cursor += valueBondsSize
+    guard cursor == data.count else {
+      fatalError("Cursor was invalid.")
+    }
+    
+    // Extract the segments of the file.
+    let cacheKey = Data(data[keyRange])
+    guard key.count == cacheKey.count else {
+      print("[CLAHousing] Cache miss: key mismatch.")
+      return nil
+    }
+    let valueAtoms = Data(data[valueAtomsRange])
+    let valueBonds = Data(data[valueBondsRange])
+    let atoms = Serialization.deserialize(atoms: valueAtoms)
+    let bonds = Serialization.deserialize(bonds: valueBonds)
+    
+    // Create a topology.
+    var topology = Topology()
+    topology.insert(atoms: atoms)
+    topology.insert(bonds: bonds)
+    return topology
+  }
+  
+  func save(key: Data, cachePath: String) {
+    // Extract the atoms and bonds.
+    var rigidBodyAtoms: [Entity] = []
+    for atomID in rigidBody.parameters.atoms.indices {
+      let atomicNumber = rigidBody.parameters.atoms.atomicNumbers[atomID]
+      let position = rigidBody.positions[atomID]
+      let entity = Entity(storage: SIMD4(position, Float(atomicNumber)))
+      rigidBodyAtoms.append(entity)
+    }
+    let rigidBodyBonds = rigidBody.parameters.bonds.indices
+    
+    // Compress the data.
+    let valueAtoms = Serialization.serialize(atoms: rigidBodyAtoms)
+    let valueBonds = Serialization.serialize(bonds: rigidBodyBonds)
+    
+    // Create the header.
+    var header: SIMD4<UInt64> = .zero
+    header[0] = UInt64(key.count)
+    header[1] = UInt64(valueAtoms.count)
+    header[2] = UInt64(valueBonds.count)
+    let headerCasted = unsafeBitCast(header, to: SIMD32<UInt8>.self)
+    
+    // Combine the header and data into a single binary.
+    var data = Data()
+    for laneID in 0..<32 {
+      let byte = headerCasted[laneID]
+      data.append(byte)
+    }
+    data.append(key)
+    data.append(valueAtoms)
+    data.append(valueBonds)
+    
+    // Save the structure to the disk.
+    let url = URL(fileURLWithPath: cachePath)
+    try! data.write(to: url)
   }
 }
