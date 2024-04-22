d941b2d1434a271c2637782f6ddfaaa2e921e4e4
Philip Turner <philipturner.AR@gmail.com>
2024-04-22 13:00:03 -0400

    Not yet sure if working on planetary gear system

--- Sources/MolecularRendererApp/Workspace/Workspace.swift
+++ Sources/MolecularRendererApp/Workspace/Workspace.swift
@@ -4,8 +4,6 @@ import MM4
 import Numerics
 import OpenMM
 
-// Working on the planetary gear system.
-
 func createGeometry() -> [Entity] {
   return [Entity(position: .zero, type: .atom(.carbon))]
 }
