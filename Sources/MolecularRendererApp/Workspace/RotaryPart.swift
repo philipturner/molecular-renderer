6e581980233859503a67a4d214364336bcd8fc03
Philip Turner <philipturner.AR@gmail.com>
2024-04-23 18:06:19 -0400

    Measured the radius of the part

---
+++ Sources/MolecularRendererApp/Workspace/RotaryPart.swift
@@ -0,0 +1,144 @@
+//
+//  RotaryPart.swift
+//  MolecularRendererApp
+//
+//  Created by Philip Turner on 4/23/24.
+//
+
+import Foundation
+import HDL
+import MM4
+import Numerics
+
+// The radius spans from 1.33 nm to 2.73 nm. The outer radius is larger than
+// the desired 2.00 nm. We can fix this some time later.
+struct RotaryPart: GenericPart {
+  var rigidBody: MM4RigidBody
+  
+  init() {
+    let lattice = Self.createLattice()
+    let topology = Self.createTopology(lattice: lattice)
+    rigidBody = Self.createRigidBody(topology: topology)
+  }
+  
+  static func createLattice() -> Lattice<Hexagonal> {
+    Lattice<Hexagonal> { h, k, l in
+      let h2k = h + 2 * k
+      Bounds { 41 * h + 4 * h2k + 6 * l }
+      Material { .checkerboard(.germanium, .carbon) }
+      
+      // Replace the bottom part with air.
+      Volume {
+        Origin { 2 * h2k }
+        Plane { -h2k }
+        Replace { .empty }
+      }
+      
+      // Replace some atoms with carbon.
+      Volume {
+        Origin { 2.5 * h2k }
+        Plane { -h2k }
+        Replace { .atom(.carbon) }
+      }
+      Volume {
+        Concave {
+          Concave {
+            Origin { 2.667 * h2k }
+            Plane { -h2k }
+          }
+          Concave {
+            Origin { 1.5 * l }
+            Plane { l }
+          }
+          Concave {
+            Origin { 4.5 * l }
+            Plane { -l }
+          }
+        }
+        Replace { .atom(.carbon) }
+      }
+      
+      // Replace some atoms with germanium.
+      Volume {
+        Concave {
+          Convex {
+            Origin { 3.333 * h2k }
+            Plane { h2k }
+          }
+          Convex {
+            Convex {
+              Origin { 1.5 * l }
+              Plane { -l }
+            }
+            Convex {
+              Origin { 4.5 * l }
+              Plane { l }
+            }
+          }
+        }
+        Replace { .atom(.germanium) }
+      }
+      Volume {
+        Origin { 3.5 * h2k }
+        Plane { h2k }
+        Replace { .atom(.germanium) }
+      }
+      
+      Volume {
+        Origin { 4 * h2k }
+        Origin { 1 * l }
+        Plane { 1 / 4 * h2k - 1 * l }
+        Replace { .empty }
+      }
+      Volume {
+        Origin { 4 * h2k }
+        Origin { 4.75 * l }
+        Plane { 1 / 4 * h2k + 1 * l }
+        Replace { .empty }
+      }
+    }
+  }
+  
+  static func createTopology(lattice: Lattice<Hexagonal>) -> Topology {
+    var reconstruction = SurfaceReconstruction()
+    reconstruction.material = .checkerboard(.germanium, .carbon)
+    do {
+      var atoms = lattice.atoms
+      atoms.sort { $0.position.x < $1.position.x }
+      reconstruction.topology.insert(atoms: atoms)
+    }
+    reconstruction.compile()
+    reconstruction.topology.sort()
+    var topology = reconstruction.topology
+    
+    // Parameters here are in nm.
+    let latticeConstant = Constant(.hexagon) {
+      .checkerboard(.germanium, .carbon)
+    }
+    
+    // The X coordinate in the original space is mapped onto θ = (0, 2π).
+    // - X = 0 transforms into θ = 0.
+    // - X = 'perimeter' transforms into θ = 2π.
+    // - Other values of X are mapped into the angular coordinate with a linear
+    //   transformation. Anything outside of the range will overshoot and
+    //   potentially overlap another chunk of matter.
+    let perimeter = Float(40) * latticeConstant
+    
+    for atomID in topology.atoms.indices {
+      var atom = topology.atoms[atomID]
+      var position = atom.position
+      
+      let θ = 2 * Float.pi * position.x / perimeter
+      let r = position.y
+      position.x = r * Float.cos(θ)
+      position.y = r * Float.sin(θ)
+      
+      atom.position = position
+      topology.atoms[atomID] = atom
+    }
+    
+    topology = deduplicate(topology: topology)
+    
+    return topology
+  }
+}
--- Sources/MolecularRendererApp/Workspace/Workspace.swift
+++ Sources/MolecularRendererApp/Workspace/Workspace.swift
@@ -5,141 +5,24 @@ import Numerics
 import OpenMM
 
 func createGeometry() -> [MM4RigidBody] {
-//  let lattice = RotaryPart.createLattice()
-//  return lattice.atoms
-  
   var part = RotaryPart()
   part.minimize(bulkAtomIDs: [])
-  return [part.rigidBody]
-}
-
-struct RotaryPart: GenericPart {
-  var rigidBody: MM4RigidBody
   
-  init() {
-    let lattice = Self.createLattice()
-    let topology = Self.createTopology(lattice: lattice)
-    rigidBody = Self.createRigidBody(topology: topology)
-  }
+  var minRadius: Float = .greatestFiniteMagnitude
+  var maxRadius: Float = .zero
+  let center = SIMD3<Float>(part.rigidBody.centerOfMass)
   
-  static func createLattice() -> Lattice<Hexagonal> {
-    Lattice<Hexagonal> { h, k, l in
-      let h2k = h + 2 * k
-      Bounds { 41 * h + 4 * h2k + 6 * l }
-      Material { .checkerboard(.germanium, .carbon) }
-      
-      // Replace the bottom part with air.
-      Volume {
-        Origin { 2 * h2k }
-        Plane { -h2k }
-        Replace { .empty }
-      }
-      
-      // Replace some atoms with carbon.
-      Volume {
-        Origin { 2.5 * h2k }
-        Plane { -h2k }
-        Replace { .atom(.carbon) }
-      }
-      Volume {
-        Concave {
-          Concave {
-            Origin { 2.667 * h2k }
-            Plane { -h2k }
-          }
-          Concave {
-            Origin { 1.5 * l }
-            Plane { l }
-          }
-          Concave {
-            Origin { 4.5 * l }
-            Plane { -l }
-          }
-        }
-        Replace { .atom(.carbon) }
-      }
-      
-      // Replace some atoms with germanium.
-      Volume {
-        Concave {
-          Convex {
-            Origin { 3.333 * h2k }
-            Plane { h2k }
-          }
-          Convex {
-            Convex {
-              Origin { 1.5 * l }
-              Plane { -l }
-            }
-            Convex {
-              Origin { 4.5 * l }
-              Plane { l }
-            }
-          }
-        }
-        Replace { .atom(.germanium) }
-      }
-      Volume {
-        Origin { 3.5 * h2k }
-        Plane { h2k }
-        Replace { .atom(.germanium) }
-      }
-      
-      Volume {
-        Origin { 4 * h2k }
-        Origin { 1 * l }
-        Plane { 1 / 4 * h2k - 1 * l }
-        Replace { .empty }
-      }
-      Volume {
-        Origin { 4 * h2k }
-        Origin { 4.75 * l }
-        Plane { 1 / 4 * h2k + 1 * l }
-        Replace { .empty }
-      }
-    }
+  for atomID in part.rigidBody.parameters.atoms.indices {
+    let position = part.rigidBody.positions[atomID]
+    var delta = position - center
+    delta.z = .zero
+    
+    let r = (delta.x * delta.x + delta.y * delta.y).squareRoot()
+    minRadius = min(minRadius, r)
+    maxRadius = max(maxRadius, r)
   }
+  print(minRadius)
+  print(maxRadius)
   
-  static func createTopology(lattice: Lattice<Hexagonal>) -> Topology {
-    var reconstruction = SurfaceReconstruction()
-    reconstruction.material = .checkerboard(.germanium, .carbon)
-    do {
-      var atoms = lattice.atoms
-      atoms.sort { $0.position.x < $1.position.x }
-      reconstruction.topology.insert(atoms: atoms)
-    }
-    reconstruction.compile()
-    reconstruction.topology.sort()
-    var topology = reconstruction.topology
-    
-    // Parameters here are in nm.
-    let latticeConstant = Constant(.hexagon) {
-      .checkerboard(.germanium, .carbon)
-    }
-    
-    // The X coordinate in the original space is mapped onto θ = (0, 2π).
-    // - X = 0 transforms into θ = 0.
-    // - X = 'perimeter' transforms into θ = 2π.
-    // - Other values of X are mapped into the angular coordinate with a linear
-    //   transformation. Anything outside of the range will overshoot and
-    //   potentially overlap another chunk of matter.
-    let perimeter = Float(40) * latticeConstant
-    
-    for atomID in topology.atoms.indices {
-      var atom = topology.atoms[atomID]
-      var position = atom.position
-      
-      let θ = 2 * Float.pi * position.x / perimeter
-      let r = position.y
-      position.x = r * Float.cos(θ)
-      position.y = r * Float.sin(θ)
-      
-      atom.position = position
-      topology.atoms[atomID] = atom
-    }
-    
-    topology = deduplicate(topology: topology)
-    
-    return topology
-  }
+  exit(0)
 }
