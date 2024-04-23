445293c9a8a8c314a74c06d5d8824101b453485c
Philip Turner <philipturner.AR@gmail.com>
2024-04-23 19:17:12 -0400

    Goals for the next day of development

--- Sources/MolecularRendererApp/Workspace/Workspace.swift
+++ Sources/MolecularRendererApp/Workspace/Workspace.swift
@@ -5,36 +5,8 @@ import Numerics
 import OpenMM
 
 func createGeometry() -> [Entity] {
-  var descriptor = RotaryPartDescriptor()
-  descriptor.cachePath =
-  "/Users/philipturner/Documents/OpenMM/cache/RotaryPart.data"
-  
-  var part = RotaryPart(descriptor: descriptor)
-  
-  var atoms: [Entity] = []
-  let parameters = part.rigidBody.parameters
-  for atomID in parameters.atoms.indices {
-    let atomicNumber = parameters.atoms.atomicNumbers[atomID]
-    let position = part.rigidBody.positions[atomID]
-    let storage = SIMD4(position, Float(atomicNumber))
-    let atom = Entity(storage: storage)
-    atoms.append(atom)
-  }
-  
-  for markerID in 0..<5 {
-    var atom = Entity(position: .zero, type: .atom(.oxygen))
-    
-    let θ = 2 * Float.pi * Float(markerID) / 5
-    var r = Float(1.33)
-    atom.position.x = r * Float.cos(θ)
-    atom.position.y = r * Float.sin(θ)
-    atoms.append(atom)
-    
-    r = Float(2.73)
-    atom.position.x = r * Float.cos(θ)
-    atom.position.y = r * Float.sin(θ)
-    atoms.append(atom)
-  }
-  
-  return atoms
+  // Design an axle with the required radius, and test whether it retains its
+  // kinetic energy for 30 revolutions at 30 GHz. Repeat this process with an
+  // auto-generated housing structure.
+  return [Entity(position: .zero, type: .atom(.carbon))]
 }
