290305159412fa6cc45e8f8056c73db636c766b6
Philip Turner <philipturner.AR@gmail.com>
2024-04-23 21:54:52 -0400

    The simulation seems to be starting off good

--- Sources/MolecularRendererApp/Workspace/Workspace.swift
+++ Sources/MolecularRendererApp/Workspace/Workspace.swift
@@ -4,10 +4,12 @@ import MM4
 import Numerics
 import OpenMM
 
-func createGeometry() -> [MM4RigidBody] {
+func createGeometry() -> [[MM4RigidBody]] {
   // Design a system of two gears and two axles, in a stiff housing. Spin the
   // two gears in opposite directions. Determine how many cycles the kinetic
   // energy survives for, at various temperatures.
+  
+  // MARK: - Initialize Parts
   
   let carrier = Carrier()
   
@@ -30,13 +32,101 @@ func createGeometry() -> [MM4RigidBody] {
     print($0.energy.potential)
   }
   simulation.withForceField {
-    $0.minimize(tolerance: 0.1)
+    $0.minimize(tolerance: 10)
   }
   simulation.withForceField {
     print($0.energy.potential)
   }
+  simulation.setVelocitiesToTemperature(2 * 77)
+  simulation.withForceField {
+    $0.simulate(time: 2)
+  }
   
-  return simulation.rigidBodies
+  // MARK: - Run Simulation
+  
+  // Erase all net momenta.
+  for rigidBodyID in simulation.rigidBodies.indices {
+    var rigidBody = simulation.rigidBodies[rigidBodyID]
+    rigidBody.angularMomentum = .zero
+    rigidBody.linearMomentum = .zero
+    simulation.rigidBodies[rigidBodyID] = rigidBody
+  }
+  
+  // Set each rigid body's angular momentum.
+  for rigidBodyID in simulation.rigidBodies.indices {
+    // Load the rigid body.
+    var rigidBody = simulation.rigidBodies[rigidBodyID]
+    guard rigidBodyID > 0 else {
+      continue
+    }
+    
+    // Check the principal axis.
+    guard (rigidBody.principalAxes.0.z).magnitude > 0.99 else {
+      fatalError("Principal axis was not aligned with the z-axis: \(rigidBody.principalAxes).")
+    }
+    
+    // Determine the angular velocity.
+    let frequencyInGHz: Double = 15
+    var ω = SIMD3<Double>(frequencyInGHz * 0.001 * 2 * .pi, 0, 0)
+    if rigidBody.principalAxes.0.z < 0 {
+      ω = -ω
+    }
+    
+    // Reverse the angular velocity of the second wheel.
+    if rigidBodyID % 2 == 0 {
+      ω = -ω
+    }
+    
+    // Set the angular momentum.
+    rigidBody.angularMomentum = ω * rigidBody.momentOfInertia
+    
+    // Store the rigid body.
+    simulation.rigidBodies[rigidBodyID] = rigidBody
+  }
+  
+  // Record the frames.
+  var frames: [[MM4RigidBody]] = []
+  print("frame: 0")
+  frames.append(simulation.rigidBodies)
+  
+  for frameID in 1...500 {
+    let timeStep: Double = 0.100
+    simulation.withForceField {
+      $0.simulate(time: timeStep)
+    }
+    
+    // Fetch the simulation kinetic energy.
+    var thermalEnergy = simulation.forceField.energy.kinetic
+    
+    // Fetch each rigid body's kinetic energy.
+    var kineticEnergies: [Double] = []
+    for rigidBody in simulation.rigidBodies {
+      let I = rigidBody.momentOfInertia
+      let L = rigidBody.angularMomentum
+      let ω = L / I
+      
+      let K = 0.5 * (ω * I * ω).sum()
+      thermalEnergy -= K
+      kineticEnergies.append(K)
+    }
+    
+    // Compute the kinetic energy per atom.
+    let atomCount = simulation.forceField.positions.count
+    let energyPerAtom = thermalEnergy / Double(atomCount)
+    
+    // Report the frame's data.
+    let time = Double(frameID) * timeStep
+    print(
+      "frame:", frameID, "|",
+      String(format: "%.1f", time), "ps |",
+      "temperature:", String(format: "%.2f", energyPerAtom), "zJ/atom |",
+      String(format: "%.1f", kineticEnergies[0]), "zJ |",
+      String(format: "%.1f", kineticEnergies[1]), "zJ |",
+      String(format: "%.1f", kineticEnergies[2]), "zJ")
+    frames.append(simulation.rigidBodies)
+  }
+  
+  return frames
 }
 
 struct Carrier: GenericPart {
