a44296e19f6be7174d2820038d3b1ba3209e47bd
Philip Turner <philipturner.AR@gmail.com>
2024-04-23 21:20:18 -0400

    Prepared the scene for simulation

--- Sources/MolecularRendererApp/Workspace/Workspace.swift
+++ Sources/MolecularRendererApp/Workspace/Workspace.swift
@@ -4,30 +4,92 @@ import MM4
 import Numerics
 import OpenMM
 
-func createGeometry() -> [Entity] {
+func createGeometry() -> [MM4RigidBody] {
   // Design a system of two gears and two axles, in a stiff housing. Spin the
   // two gears in opposite directions. Determine how many cycles the kinetic
   // energy survives for, at various temperatures.
   
-  let lattice = Lattice<Cubic> { h, k, l in
-    Bounds { 16 * h + 12 * k + 12 * l }
-    Material { .elemental(.carbon) }
-  }
+  let carrier = Carrier()
   
   var rotaryPartDesc = RotaryPartDescriptor()
   rotaryPartDesc.cachePath = "/Users/philipturner/Documents/OpenMM/cache/RotaryPart.data"
-  let rotaryPart = RotaryPart(descriptor: rotaryPartDesc)
+  let rotaryPartBase = RotaryPart(descriptor: rotaryPartDesc)
   
-  var minimumZ: Float = .greatestFiniteMagnitude
-  var maximumZ: Float = -.greatestFiniteMagnitude
-  for position in rotaryPart.rigidBody.positions {
-    minimumZ = min(minimumZ, position.z)
-    maximumZ = max(maximumZ, position.z)
+  var rotaryPart1 = rotaryPartBase
+  var rotaryPart2 = rotaryPartBase
+  rotaryPart1.rigidBody.centerOfMass.x -= 7.625 * 0.3567
+  rotaryPart2.rigidBody.centerOfMass.x += 7.625 * 0.3567
+  rotaryPart2.rigidBody.rotate(angle: 0.07, axis: [0, 0, 1])
+  
+  return [
+    carrier.rigidBody,
+    rotaryPart1.rigidBody,
+    rotaryPart2.rigidBody,
+  ]
+}
+
+struct Carrier: GenericPart {
+  var rigidBody: MM4RigidBody
+  
+  init() {
+    let lattice = Self.createLattice()
+    let topology = Self.createTopology(lattice: lattice)
+    rigidBody = Self.createRigidBody(topology: topology)
+    
+    // Set the center of mass to zero.
+    rigidBody.centerOfMass = .zero
   }
-  print(minimumZ)
-  print(maximumZ)
-  print(maximumZ - minimumZ)
   
-  exit(0)
-  
+  static func createLattice() -> Lattice<Cubic> {
+    Lattice<Cubic> { h, k, l in
+      Bounds { 25 * h + 12 * k + 18 * l }
+      Material { .elemental(.carbon) }
+      
+      func createAxle() {
+        Convex {
+          for degreeIndex in 0..<180 {
+            let θ = Float(degreeIndex) * 2 * (Float.pi / 180)
+            let r = Float(1.1) / 0.3567
+            
+            let x = r * Float.cos(θ)
+            let y = r * Float.sin(θ)
+            Convex {
+              Origin { x * h + y * k }
+              Plane { x * h + y * k }
+            }
+          }
+        }
+      }
+      
+      Volume {
+        Convex {
+          Origin { 24.25 * h }
+          Plane  { h }
+        }
+        
+        Origin { 6 * k }
+        
+        Concave {
+          Concave {
+            Origin { 4 * l }
+            Plane { l }
+          }
+          Concave {
+            Origin { 4.5 * h }
+            createAxle()
+          }
+          Concave {
+            Origin { 19.75 * h }
+            createAxle()
+          }
+          Concave {
+            Origin { 14 * l }
+            Plane { -l }
+          }
+        }
+        
+        Replace { .empty }
+      }
+    }
+  }
 }
