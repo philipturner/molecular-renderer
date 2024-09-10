e6ac96764e53c0f0bea1951f9ae2d25cf6c274e3
Philip Turner <philipturner.AR@gmail.com>
2024-09-10 18:27:26 -0400

    Added Numerics and HDL to the dependencies

--- Package.swift
+++ Package.swift
@@ -9,11 +9,21 @@ let package = Package(
       name: "MolecularRenderer",
       targets: ["MolecularRenderer"]),
   ],
+  dependencies: [
+    .package(
+      url: "https://github.com/philipturner/HDL",
+      branch: "main"),
+    .package(
+      url: "https://github.com/philipturner/swift-numerics",
+      branch: "Quaternions"),
+  ],
   targets: [
     .executableTarget(
       name: "Workspace",
       dependencies: [
-        "MolecularRenderer"
+        .product(name: "HDL", package: "HDL"),
+        "MolecularRenderer",
+        .product(name: "Numerics", package: "swift-numerics"),
       ]),
     .target(
       name: "MolecularRenderer",
