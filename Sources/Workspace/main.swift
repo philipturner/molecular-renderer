fee5ec16a1377be01a60e9cfcdb2bff8add7eee5
Philip Turner <philipturner.AR@gmail.com>
2024-09-10 20:54:27 -0400

    Translated the remaining code

--- Sources/Workspace/main.swift
+++ Sources/Workspace/main.swift
@@ -42,6 +42,15 @@ class AAPLView: NSView, CALayerDelegate {
   var paused: Bool = false
   var delegate: AAPLViewDelegate!
   
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    initCommon()
+  }
+  
+  required init(coder: NSCoder) {
+    fatalError("Coder init not supported.")
+  }
+  
   func initCommon() {
     metalLayer = self.layer! as? CAMetalLayer
     self.layer!.delegate = self
@@ -73,9 +82,157 @@ class AAPLView: NSView, CALayerDelegate {
 
 // MARK: - AAPLViewController
 
-//class AAPLViewController: NSViewController, AAPLViewDelegate {
-//  var renderer: AAPLRenderer!
-//}
+class AAPLViewController: NSViewController, AAPLViewDelegate {
+  var renderer: AAPLRenderer!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let device = MTLCreateSystemDefaultDevice()!
+    let view = self.view as! AAPLView
+    view.metalLayer.device = device
+    view.delegate = self
+    view.metalLayer.pixelFormat = .rgb10a2Unorm
+    
+    renderer = AAPLRenderer(
+      device: device,
+      drawablePixelFormat: view.metalLayer.pixelFormat)
+  }
+  
+  func resizeDrawable(_ size: CGSize) {
+    renderer.resizeDrawable(size)
+  }
+  
+  func renderToMetalLayer(metalLayer layer: CAMetalLayer) {
+    renderer.renderToMetalLayer(metalLayer: layer)
+  }
+}
+
+// MARK: - AAPLNSView
+
+class AAPLNSView: AAPLView {
+  var displayLink: CVDisplayLink!
+  
+  var backingScaleFactor: CGFloat {
+    guard let window = self.window,
+          let screen = window.screen else {
+      fatalError("Could not retrieve screen.")
+    }
+    return screen.backingScaleFactor
+  }
+  
+  override func initCommon() {
+    self.wantsLayer = true
+    self.layerContentsRedrawPolicy = .duringViewResize
+    super.initCommon()
+  }
+  
+  override func makeBackingLayer() -> CALayer {
+    return CAMetalLayer()
+  }
+  
+  override func viewDidMoveToWindow() {
+    super.viewDidMoveToWindow()
+    
+    guard let window = self.window,
+          let screen = window.screen else {
+      fatalError("Could not retrieve screen.")
+    }
+    setupCVDisplayLinkForScreen(screen: screen)
+    resizeDrawable(backingScaleFactor)
+  }
+  
+  func setupCVDisplayLinkForScreen(screen: NSScreen) {
+    // Initialize a display link that can be used with all of the displays.
+    do {
+      let cvReturn = CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
+      guard cvReturn == kCVReturnSuccess else {
+        fatalError("CVReturn was not success.")
+      }
+    }
+    
+    // Set the callback function, supplying this view as its argument.
+    do {
+      let unmanaged = Unmanaged<AAPLNSView>.passUnretained(self)
+      let selfPointer = unmanaged.toOpaque()
+      let cvReturn = CVDisplayLinkSetOutputCallback(
+        displayLink, DispatchRenderLoop, selfPointer)
+      guard cvReturn == kCVReturnSuccess else {
+        fatalError("CVReturn was not success.")
+      }
+    }
+    
+    // Associate the display link with the display where the view is.
+    do {
+      let screen = self.window!.screen!
+      let key = NSDeviceDescriptionKey("NSScreenNumber")
+      guard let screenNumberAny = screen.deviceDescription[key],
+            let screenNumber = screenNumberAny as? NSNumber else {
+        fatalError("Could not retrieve screen number.")
+      }
+      let cvReturn = CVDisplayLinkSetCurrentCGDisplay(
+        displayLink, screenNumber.uint32Value)
+      guard cvReturn == kCVReturnSuccess else {
+        fatalError("CVReturn was not success.")
+      }
+    }
+    
+    // Start the display link.
+    do {
+      let cvReturn = CVDisplayLinkStart(displayLink)
+      guard cvReturn == kCVReturnSuccess else {
+        fatalError("CVReturn was not success.")
+      }
+    }
+    
+    // Register to be notified when the window closes.
+    let notificationCenter = NotificationCenter.default
+    notificationCenter.addObserver(
+      self,
+      selector: #selector(windowWillClose(notification:)),
+      name: NSWindow.willCloseNotification,
+      object: self.window!)
+  }
+  
+  @objc
+  func windowWillClose(notification: NSNotification) {
+    exit(0)
+  }
+  
+  override func viewDidChangeBackingProperties() {
+    super.viewDidChangeBackingProperties()
+    resizeDrawable(backingScaleFactor)
+  }
+  
+  override func setFrameSize(_ newSize: NSSize) {
+    super.setFrameSize(newSize)
+    resizeDrawable(backingScaleFactor)
+  }
+  
+  override func setBoundsSize(_ newSize: NSSize) {
+    super.setBoundsSize(newSize)
+    resizeDrawable(backingScaleFactor)
+  }
+}
+
+// Must be declared in the global scope to get @convention(c) status.
+func DispatchRenderLoop(
+  displayLink: CVDisplayLink,
+  now: UnsafePointer<CVTimeStamp>,
+  outputTime: UnsafePointer<CVTimeStamp>,
+  flagsIn: UInt64,
+  flagsOut: UnsafeMutablePointer<UInt64>,
+  displayLinkContext: UnsafeMutableRawPointer?
+) -> Int32 {
+  guard let displayLinkContext else {
+    fatalError("Could not retrieve display link context.")
+  }
+  let unmanaged = Unmanaged<AAPLNSView>.fromOpaque(displayLinkContext)
+  let customView = unmanaged.takeUnretainedValue()
+  customView.render()
+  
+  return kCVReturnSuccess
+}
 
 // MARK: - AAPLRenderer
 
@@ -109,11 +266,29 @@ class AAPLRenderer: NSObject {
   }
   
   func renderToMetalLayer(metalLayer: CAMetalLayer) {
+    frameNumber += 1
     
+    let commandBuffer = commandQueue.makeCommandBuffer()!
+    let currentDrawable = metalLayer.nextDrawable()
+    guard let currentDrawable else {
+      fatalError("Could not retrieve next drawable.")
+    }
+    
+    let encoder = commandBuffer.makeComputeCommandEncoder()!
+    encoder.setComputePipelineState(computePipelineState)
+    encoder.setTexture(currentDrawable.texture, index: 0)
+    encoder.dispatchThreads(
+      MTLSize(width: 1920, height: 1920, depth: 1),
+      threadsPerThreadgroup: MTLSize(width: 8, height: 8, depth: 1))
+    
+    encoder.endEncoding()
+    commandBuffer.present(currentDrawable)
+    commandBuffer.commit()
   }
   
   func resizeDrawable(_ drawableSize: CGSize) {
-    
+    viewportSize.x = UInt32(drawableSize.width)
+    viewportSize.y = UInt32(drawableSize.height)
   }
 }
 
@@ -167,5 +342,3 @@ withExtendedLifetime(appDelegate) {
   app.activate(ignoringOtherApps: true)
   app.run()
 }
-
-
