e7339b04d89a2c632bddcd5418ccbc54bf9fee55
Philip Turner <philipturner.AR@gmail.com>
2024-09-11 19:13:10 -0400

    Made a new, simplified renderer class

--- Sources/Workspace/main.swift
+++ Sources/Workspace/main.swift
@@ -15,6 +15,95 @@
 // - Access the GPU.
 //   - Modify it to get Metal rendering. [DONE]
 //   - Clean up and simplify the code as much as possible.
-//   - Get timestamps synchronizing properly (moving rainbow banner scene).
+//   - Get timestamps synchronizing properly (moving rainbow banner
+//     scene).
 // - Repeat the same process with COM / D3D12 on Windows.
 //   - Another single-file Swift script that does the same thing.
+
+import AppKit
+
+class Renderer {
+  var device: MTLDevice
+  var commandQueue: MTLCommandQueue
+  var computePipelineState: MTLComputePipelineState
+  var frameID: Int = .zero
+  
+  init() {
+    device = MTLCreateSystemDefaultDevice()!
+    commandQueue = device.makeCommandQueue()!
+    computePipelineState = Renderer
+      .createComputePipelineState(device: device)
+  }
+  
+  func render(layer: CAMetalLayer) {
+    frameID += 1
+    
+    let drawable = layer.nextDrawable()
+    guard let drawable else {
+      fatalError("Drawable timed out after 1 second.")
+    }
+    
+    let commandBuffer = commandQueue.makeCommandBuffer()!
+    let encoder = commandBuffer.makeComputeCommandEncoder()!
+    encoder.setComputePipelineState(computePipelineState)
+    encoder.setBytes(&frameID, length: 4, index: 0)
+    encoder.setTexture(drawable.texture, index: 0)
+    encoder.dispatchThreads(
+      MTLSize(width: 1920, height: 1920, depth: 1),
+      threadsPerThreadgroup: MTLSize(width: 8, height: 8, depth: 1))
+    
+    encoder.endEncoding()
+    commandBuffer.present(drawable)
+    commandBuffer.commit()
+  }
+}
+
+extension Renderer {
+  static func createSource() -> String {
+    """
+    
+    #include <metal_stdlib>
+    using namespace metal;
+    
+    kernel void renderImage(
+      constant uint *frameID [[buffer(0)]],
+      texture2d<half, access::write> drawableTexture [[texture(0)]],
+      ushort2 tid [[thread_position_in_grid]]
+    ) {
+      half4 color;
+      if (tid.x == tid.y || tid.x == 1920 - tid.y) {
+        color = half4(0.00, 0.00, 0.00, 1.00);
+      } else {
+        uint frameModulo = *frameID % 120;
+        half frameNormalized = half(frameModulo) / 120;
+        color = half4(frameNormalized, 0.00, 0.00, 1.00);
+      }
+    
+      drawableTexture.write(color, tid);
+    }
+    
+    """
+  }
+  
+  static func createComputePipelineState(
+    device: MTLDevice
+  ) -> MTLComputePipelineState {
+    let shaderSource = Renderer.createSource()
+    let library = try! device.makeLibrary(source: shaderSource, options: nil)
+    
+    let function = library.makeFunction(name: "renderImage")
+    guard let function else {
+      fatalError("Could not make function.")
+    }
+    let pipeline = try! device.makeComputePipelineState(function: function)
+    return pipeline
+  }
+}
+
+class RendererView: NSView, CALayerDelegate {
+  
+}
+
+class EventResponder: NSResponder {
+  
+}
