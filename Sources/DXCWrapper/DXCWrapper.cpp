331540b688598b3c813566be6f4ff69aa9a342ca
Philip Turner <philipturner.AR@gmail.com>
2025-04-20 21:05:34 -0400

    Fully implemented the first code snippet of the tutorial

--- Sources/DXCWrapper/DXCWrapper.cpp
+++ Sources/DXCWrapper/DXCWrapper.cpp
@@ -1,13 +1,18 @@
-#include <iostream>
+// Imports for DXC symbols.
 #include "dxcapi.h"
+
+// Imports for ComPtr<>.
 #include <wrl.h>
 using namespace Microsoft::WRL;
 
+// Imports for debugging.
+#include <iostream>
 
-
+// Function for testing a tutorial for DXCompiler.
 extern "C"
 __declspec(dllexport)
 int8_t function(int8_t argument) {
+  // Specify the shader source code.
   int8_t* shaderSource = (int8_t*)malloc(5);
   shaderSource[0] = 'h';
   shaderSource[1] = 'j';
@@ -15,26 +20,24 @@ int8_t function(int8_t argument) {
   shaderSource[3] = '\n';
   shaderSource[4] = '0';
   
-  // TODO: Save this file to GitHub. Then, create a new Swift package and copy
-  // over the files from molecular-renderer. Narrow down the crash as much as
-  // possible, even reimplementing SwiftCOM if needed.
-  //
-  // The file works just fine with Clang:
-  // clang++ File.cpp -ldxcompiler -o File
-  // ./File.exe
-  //
-  // I wonder if we can spin up a C library that's compiled on the fly,
-  // linked to the Swift module at runtime, and simply does any compilation
-  // tasks we need. That would take so much less effort than porting the
-  // DirectXShaderCompiler C API to Swift.
+  // MARK: - Code Snippet 1
   
   ComPtr<IDxcUtils> pUtils;
   DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(pUtils.GetAddressOf()));
   ComPtr<IDxcBlobEncoding> pSource;
   pUtils->CreateBlob(shaderSource, 4, CP_UTF8, pSource.GetAddressOf());
   
-  std::cout << "Hello, world!" << std::endl;
-  std::cout << pSource->GetBufferSize() << std::endl;
+  std::cout << "pSource->GetBufferPointer() = " << pSource->GetBufferPointer() << std::endl;
+  std::cout << "pSource->GetBufferSize() = " << pSource->GetBufferSize() << std::endl;
+  {
+    BOOL known;
+    UINT32 codePage;
+    HRESULT result = pSource->GetEncoding(&known, &codePage);
+    std::cout << "pSource->GetEncoding = (" << known;
+    std::cout << ", " << codePage;
+    std::cout << ", " << result;
+    std::cout << ")" << std::endl;
+  }
   
   return argument * argument;
 }
