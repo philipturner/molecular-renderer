889ab821bd23966c32d56c87c3d39893a8106a25
Philip Turner <philipturner.AR@gmail.com>
2025-04-20 21:25:31 -0400

    Added the arguments

--- Sources/DXCWrapper/DXCWrapper.cpp
+++ Sources/DXCWrapper/DXCWrapper.cpp
@@ -7,6 +7,7 @@ using namespace Microsoft::WRL;
 
 // Imports for debugging.
 #include <iostream>
+#include <vector>
 
 // Function for testing a tutorial for DXCompiler.
 extern "C"
@@ -26,6 +27,8 @@ int8_t function(int8_t argument) {
   DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(pUtils.GetAddressOf()));
   ComPtr<IDxcBlobEncoding> pSource;
   pUtils->CreateBlob(shaderSource, 4, CP_UTF8, pSource.GetAddressOf());
+  std::cout << "pUtils = " << pUtils.Get() << std::endl;
+  std::cout << "pSource = " << pSource.Get() << std::endl;
   
   std::cout << "pSource->GetBufferPointer() = " << pSource->GetBufferPointer() << std::endl;
   std::cout << "pSource->GetBufferSize() = " << pSource->GetBufferSize() << std::endl;
@@ -39,5 +42,58 @@ int8_t function(int8_t argument) {
     std::cout << ")" << std::endl;
   }
   
+  // MARK: - Code Snippet 2
+  
+  // Initialize the arguments used in subsequent code.
+  std::vector<LPWSTR> arguments;
+  
+  // Initialize the compiler used in subsequent code.
+  ComPtr<IDxcCompiler3> pCompiler;
+  DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(pCompiler.GetAddressOf()));
+  std::cout << "pCompiler = " << pCompiler.Get() << std::endl;
+  
+  // -E for the entry point (eg. 'main')
+  arguments.push_back(L"-E");
+  arguments.push_back(L"main");
+
+  // -T for the target profile (eg. 'ps_6_6')
+  arguments.push_back(L"-T");
+  arguments.push_back(L"ps_6_6");
+
+  // Strip reflection data and pdbs (see later)
+  arguments.push_back(L"-Qstrip_debug");
+  arguments.push_back(L"-Qstrip_reflect");
+
+  arguments.push_back(DXC_ARG_WARNINGS_ARE_ERRORS); // -WX
+  arguments.push_back(DXC_ARG_DEBUG); // -Zi
+
+  // TODO: Fix the issue with compiling this part.
+    // std::vector<std::wstring> defines = {
+  //   "MACRO1", "MACRO2"
+  // };
+  /*
+  for (const std::wstring& define : defines)
+  {
+      arguments.push_back(L"-D");
+      arguments.push_back((LPWSTR)define.c_str());
+  }
+  */
+
+  DxcBuffer sourceBuffer;
+  sourceBuffer.Ptr = pSource->GetBufferPointer();
+  sourceBuffer.Size = pSource->GetBufferSize();
+  sourceBuffer.Encoding = 0;
+  
+/*
+  ComPtr<IDxcResult> pCompileResult;
+  HR(pCompiler->Compile(&sourceBuffer, arguments.data(), (uint32)arguments.size(), nullptr, IID_PPV_ARGS(pCompileResult.GetAddressOf())));
+
+  // Error Handling. Note that this will also include warnings unless disabled.
+  ComPtr<IDxcBlobUtf8> pErrors;
+  pCompileResult->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(pErrors.GetAddressOf()), nullptr);
+  if (pErrors && pErrors->GetStringLength() > 0) {
+    std::cout << "There was an error." << std::endl;
+  }
+  */
   return argument * argument;
 }
