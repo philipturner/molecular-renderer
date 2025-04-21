f1a0c1f7733a13b353d58ce223d61a37895b47ca
Philip Turner <philipturner.AR@gmail.com>
2025-04-21 14:07:25 -0400

    Added a correctly functioning HLSL shader

--- Sources/DXCWrapper/DXCWrapper.cpp
+++ Sources/DXCWrapper/DXCWrapper.cpp
@@ -9,13 +9,60 @@ using namespace Microsoft::WRL;
 #include <iostream>
 #include <vector>
 
-// Function for testing a tutorial for DXCompiler.
+// Function for testing a tutorial for DXCompiler:
+// https://simoncoenen.com/blog/programming/graphics/DxcCompiling
 extern "C"
 __declspec(dllexport)
 int8_t function(int8_t argument) {
   // Specify the shader source code.
   std::string shaderSource = R"(
-  hjk
+  //--------------------------------------------------------------------------------------
+  // File: BasicCompute11.hlsl
+  //
+  // This file contains the Compute Shader to perform array A + array B
+  //
+  // Copyright (c) Microsoft Corporation.
+  // Licensed under the MIT License (MIT).
+  //--------------------------------------------------------------------------------------
+  
+  #ifdef USE_STRUCTURED_BUFFERS
+  
+  struct BufType
+  {
+      int i;
+      float f;
+  };
+  
+  StructuredBuffer<BufType> Buffer0 : register(t0);
+  StructuredBuffer<BufType> Buffer1 : register(t1);
+  RWStructuredBuffer<BufType> BufferOut : register(u0);
+  
+  [numthreads(1, 1, 1)]
+  void CSMain( uint3 DTid : SV_DispatchThreadID )
+  {
+      BufferOut[DTid.x].i = Buffer0[DTid.x].i + Buffer1[DTid.x].i;
+      BufferOut[DTid.x].f = Buffer0[DTid.x].f + Buffer1[DTid.x].f;
+  }
+  
+  #else // The following code is for raw buffers
+  
+  ByteAddressBuffer Buffer0 : register(t0);
+  ByteAddressBuffer Buffer1 : register(t1);
+  RWByteAddressBuffer BufferOut : register(u0);
+  
+  [numthreads(1, 1, 1)]
+  void CSMain( uint3 DTid : SV_DispatchThreadID )
+  {
+      int i0 = asint( Buffer0.Load( DTid.x*8 ) );
+      float f0 = asfloat( Buffer0.Load( DTid.x*8+4 ) );
+      int i1 = asint( Buffer1.Load( DTid.x*8 ) );
+      float f1 = asfloat( Buffer1.Load( DTid.x*8+4 ) );
+      
+      BufferOut.Store( DTid.x*8, asuint(i0 + i1) );
+      BufferOut.Store( DTid.x*8+4, asuint(f0 + f1) );
+  }
+  
+  #endif // USE_STRUCTURED_BUFFERS
   
   )";
   
@@ -47,8 +94,7 @@ int8_t function(int8_t argument) {
   
   // Initialize the defines used in subsequent code.
   std::vector<std::wstring> defines = {
-    L"MACRO1",
-    L"MACRO2",
+    L"USE_STRUCTURED_BUFFERS",
   };
   
   // Initialize the compiler used in subsequent code.
@@ -58,16 +104,16 @@ int8_t function(int8_t argument) {
   
   // -E for the entry point (eg. 'main')
   arguments.push_back(L"-E");
-  arguments.push_back(L"main");
-
+  arguments.push_back(L"CSMain");
+  
   // -T for the target profile (eg. 'ps_6_6')
   arguments.push_back(L"-T");
-  arguments.push_back(L"ps_6_6");
-
+  arguments.push_back(L"cs_6_6");
+  
   // Strip reflection data and pdbs (see later)
   arguments.push_back(L"-Qstrip_debug");
   arguments.push_back(L"-Qstrip_reflect");
-
+  
   arguments.push_back(DXC_ARG_WARNINGS_ARE_ERRORS); // -WX
   arguments.push_back(DXC_ARG_DEBUG); // -Zi
   
@@ -99,7 +145,6 @@ int8_t function(int8_t argument) {
   }
   
   // Next steps:
-  // - Create a valid HLSL shader.
   // - Proceed to the parts of the tutorial about stripping debug data and
   //   shader reflection.
   
