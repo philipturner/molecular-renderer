b583cbe353b801947926ba43d6d3db0c80cafb5f
Philip Turner <philipturner.AR@gmail.com>
2025-04-30 14:31:27 -0400

    Finished some instance method implementations

--- Sources/MolecularRenderer/Windows/CommandQueue.swift
+++ Sources/MolecularRenderer/Windows/CommandQueue.swift
@@ -28,11 +28,56 @@ public class CommandQueue {
   // MARK: - Public
   
   public init(device: SwiftCOM.ID3D12Device, type: D3D12_COMMAND_LIST_TYPE) {
-    fatalError("Not implemented.")
+    self.fenceValue = 0
+    self.commandListType = type
+    self.d3d12Device = device
+    
+    var commandQueueDesc = D3D12_COMMAND_QUEUE_DESC()
+    commandQueueDesc.Type = type
+    commandQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL.rawValue
+    commandQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE
+    commandQueueDesc.NodeMask = 0
+    
+    d3d12CommandQueue = try! d3d12Device.CreateCommandQueue(commandQueueDesc)
+    d3d12Fence = try! d3d12Device.CreateFence(fenceValue, D3D12_FENCE_FLAG_NONE)
+    
+    let fenceEvent = CreateEventA(nil, false, false, nil)
+    guard let fenceEvent else {
+      fatalError("Failed to create fence event handle.")
+    }
+    self.fenceEvent = fenceEvent
   }
   
   /// Get an available command list from the command queue.
   public func GetCommandList() -> SwiftCOM.ID3D12GraphicsCommandList {
+    // Check whether we can recycle an existing command allocator.
+    var commandAllocatorExists = false
+    if commandAllocatorQueue.count > 0 {
+      let frontEntry = commandAllocatorQueue.first!
+      let frontFenceValue = frontEntry.fenceValue
+      
+      if IsFenceComplete(fenceValue: frontFenceValue) {
+        commandAllocatorExists = true
+      }
+    }
+    
+    // Materialize a command allocator.
+    var commandAllocator: SwiftCOM.ID3D12CommandAllocator
+    if commandAllocatorExists {
+      // Remove the first element from the queue.
+      let frontEntry = commandAllocatorQueue.first!
+      commandAllocatorQueue.removeFirst()
+      
+      // Assign the allocator to the local variable.
+      commandAllocator = frontEntry.commandAllocator
+      try! commandAllocator.Reset()
+    } else {
+      commandAllocator = CreateCommandAllocator()
+    }
+    
+    // Materialize a command list.
+    var commandList: SwiftCOM.ID3D12GraphicsCommandList
+    
     fatalError("Not implemented.")
   }
   
@@ -61,11 +106,13 @@ public class CommandQueue {
   // MARK: - Protected
   
   private func CreateCommandAllocator() -> SwiftCOM.ID3D12CommandAllocator {
-    fatalError("Not implemented.")
+    let commandAllocator: SwiftCOM.ID3D12CommandAllocator = try! d3d12Device.CreateCommandAllocator(commandListType)
+    return commandAllocator
   }
   
   private func CreateCommandList(allocator: SwiftCOM.ID3D12CommandAllocator) -> SwiftCOM.ID3D12GraphicsCommandList {
-    fatalError("Not implemented.")
+    let commandList: SwiftCOM.ID3D12GraphicsCommandList = try! d3d12Device.CreateCommandList(0, commandListType, allocator, nil)
+    return commandList
   }
 }
 
