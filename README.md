3833f60ffbc2ec97041e43a3d4e2407b213e615d
Philip Turner <philipturner.AR@gmail.com>
2025-09-12 10:20:53 -0400

    Making life easier by removing the need to manage a cache for internet downloads

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ![Banner](./Documentation/Banner.png)
 
 # Molecular Renderer
+
+Run `./install-libraries.bat` in Git Bash, at the repo directory. Only run once, the first time the repo is downloaded.
+
+Run `./run.bat` in the VS Code terminal. Run on every program startup.
--- run.bat
+++ run.bat
@@ -1,8 +1,3 @@
 @echo off
-
-:: TODO: Recognize the cache, avoiding the latency of downloading on every
-:: startup. Perhaps Batch scripting has logic capabilities of Bash, and can do
-:: this inside 'install-libraries.bat'.
-CALL install-libraries.bat
 powershell -c "& 'C:\Program Files\Git\bin\bash.exe' compile-dxc-wrapper.sh"
 powershell -c "& 'C:\Program Files\Git\bin\bash.exe' run.sh"
