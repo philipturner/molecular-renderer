faea9c40079ccc37443b426567718902e3d7168d
Philip Turner <philipturner.AR@gmail.com>
2025-09-13 12:33:51 -0400

    Refactored the code

--- Sources/Workspace/main.swift
+++ Sources/Workspace/main.swift
@@ -40,6 +40,23 @@ func createApplication() -> Application {
   return application
 }
 
+func createAtoms() -> [SIMD4<Float>] {
+  return [
+    Atom(position: SIMD3( 2.0186, -0.2175,  0.7985) * 0.1, element: .hydrogen),
+    Atom(position: SIMD3( 1.4201, -0.2502, -0.1210) * 0.1, element: .carbon),
+    Atom(position: SIMD3( 1.6783,  0.6389, -0.7114) * 0.1, element: .hydrogen),
+    Atom(position: SIMD3( 1.7345, -1.1325, -0.6927) * 0.1, element: .hydrogen),
+    Atom(position: SIMD3(-0.0726, -0.3145,  0.1833) * 0.1, element: .carbon),
+    Atom(position: SIMD3(-0.2926, -1.2317,  0.7838) * 0.1, element: .hydrogen),
+    Atom(position: SIMD3(-0.3758,  0.8195,  0.9774) * 0.1, element: .oxygen),
+    Atom(position: SIMD3(-1.3159,  0.8236,  1.0972) * 0.1, element: .hydrogen),
+    Atom(position: SIMD3(-0.8901, -0.3435, -1.1071) * 0.1, element: .carbon),
+    Atom(position: SIMD3(-0.7278,  0.5578, -1.7131) * 0.1, element: .hydrogen),
+    Atom(position: SIMD3(-0.6126, -1.2088, -1.7220) * 0.1, element: .hydrogen),
+    Atom(position: SIMD3(-1.9673, -0.4150, -0.9062) * 0.1, element: .hydrogen),
+  ]
+}
+
 // Set up the application.
 let application = createApplication()
 
@@ -47,7 +64,7 @@ let application = createApplication()
 var shaderDesc = ShaderDescriptor()
 shaderDesc.device = application.device
 shaderDesc.name = "renderImage"
-shaderDesc.source = createRenderImage()
+shaderDesc.source = createRenderImage(atoms: createAtoms())
 #if os(macOS)
 shaderDesc.threadsPerGroup = SIMD3(8, 8, 1)
 #endif
