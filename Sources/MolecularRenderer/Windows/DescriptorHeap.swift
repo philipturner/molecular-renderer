f751e659e8d88c51ec85f5d242b1f2e78bfcc500
Philip Turner <philipturner.AR@gmail.com>
2025-09-14 14:38:20 -0400

    Save progress

--- Sources/MolecularRenderer/Windows/DescriptorHeap.swift
+++ Sources/MolecularRenderer/Windows/DescriptorHeap.swift
@@ -2,19 +2,24 @@
 import SwiftCOM
 import WinSDK
 
-#if false
 struct DescriptorHeapDescriptor {
-  
+  var device: Device?
+  var count: Int?
 }
 
 class DescriptorHeap {
+  private var offset: Int = 0
+  private let incrementSize: Int
   
-  mutating func reset() {
-    fatalError("Not implemented.")
+  init(descriptor: DescriptorHeapDescriptor) {
+    guard let device = descriptor.device,
+          let count = descriptor.count else {
+      fatalError("Descriptor was incomplete.")
+    }
+    incrementSize = Int(
+      try! device.d3d12Device.GetDescriptorHandleIncrementSize(
+        D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV))
   }
-  
-  
 }
-#endif
 
 #endif
