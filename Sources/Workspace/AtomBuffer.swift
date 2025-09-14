ff17b0287ad36038f8c85c78681794f1943ddc71
Philip Turner <philipturner.AR@gmail.com>
2025-09-14 17:56:58 -0400

    Added a copy utility

--- Sources/Workspace/AtomBuffer.swift
+++ Sources/Workspace/AtomBuffer.swift
@@ -1,4 +1,8 @@
 import MolecularRenderer
+#if os(Windows)
+import SwiftCOM
+import WinSDK
+#endif
 
 struct AtomBuffer {
   #if os(Windows)
@@ -31,6 +35,39 @@ struct AtomBuffer {
     atoms: [SIMD4<Float>],
     inFlightFrameID: Int
   ) {
-    
+    atoms.withUnsafeBytes { bufferPointer in
+      let baseAddress = bufferPointer.baseAddress!
+      
+      #if os(macOS)
+      let buffer = nativeBuffers[inFlightFrameID]
+      #else
+      let buffer = inputBuffers[inFlightFrameID]
+      #endif
+      buffer.write(input: baseAddress)
+    }
   }
+  
+  #if os(Windows)
+  func copy(
+    commandList: CommandList,
+    inFlightFrameID: Int
+  ) {
+    let inputBuffer = inputBuffers[inFlightFrameID]
+    let nativeBuffer = nativeBuffers[inFlightFrameID]
+    
+    let copyDestBarrier = nativeBuffer
+      .transition(state: D3D12_RESOURCE_STATE_COPY_DEST)
+    try! commandList.d3d12CommandList.ResourceBarrier(
+      1, [copyDestBarrier])
+    
+    commandList.upload(
+      inputBuffer: inputBuffer,
+      nativeBuffer: nativeBuffer)
+    
+    let unorderedAccessBarrier = nativeBuffer
+      .transition(state: D3D12_RESOURCE_STATE_UNORDERED_ACCESS)
+    try! commandList.d3d12CommandList.ResourceBarrier(
+      1, [unorderedAccessBarrier])
+  }
+  #endif
 }
