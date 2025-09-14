88be653c676a884ebabcea60cf025517fea554b0
Philip Turner <philipturner.AR@gmail.com>
2025-09-14 17:51:51 -0400

    Making some progress with this utility

--- Sources/Workspace/AtomBuffer.swift
+++ Sources/Workspace/AtomBuffer.swift
@@ -1,7 +1,9 @@
 import MolecularRenderer
 
 struct AtomBuffer {
+  #if os(Windows)
   var inputBuffers: [Buffer] = []
+  #endif
   var nativeBuffers: [Buffer] = []
   
   init(
@@ -10,6 +12,25 @@ struct AtomBuffer {
   ) {
     for _ in 0..<3 {
       var bufferDesc = BufferDescriptor()
+      bufferDesc.device = device
+      bufferDesc.size = atomCount * 16
+      
+      #if os(Windows)
+      bufferDesc.type = .input
+      let inputBuffer = Buffer(descriptor: bufferDesc)
+      inputBuffers.append(inputBuffer)
+      #endif
+      
+      bufferDesc.type = .native
+      let nativeBuffer = Buffer(descriptor: bufferDesc)
+      nativeBuffers.append(nativeBuffer)
     }
+  }
+  
+  mutating func write(
+    atoms: [SIMD4<Float>],
+    inFlightFrameID: Int
+  ) {
+    
   }
 }
