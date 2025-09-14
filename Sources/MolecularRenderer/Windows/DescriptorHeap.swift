8ac9090cf2b90d703deb10594abc50b94fa168c7
Philip Turner <philipturner.AR@gmail.com>
2025-09-14 14:19:42 -0400

    Need better handling of the bufferIndex and in-flight index

---
+++ Sources/MolecularRenderer/Windows/DescriptorHeap.swift
@@ -0,0 +1,18 @@
+#if os(Windows)
+import SwiftCOM
+import WinSDK
+
+struct DescriptorHeapDescriptor {
+  
+}
+
+class DescriptorHeap {
+  
+  mutating func reset() {
+    fatalError("Not implemented.")
+  }
+  
+  
+}
+
+#endif
