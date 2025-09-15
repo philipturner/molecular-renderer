f30e7120eefba699499847d12cc79c29754250f1
Philip Turner <philipturner.AR@gmail.com>
2025-09-15 13:09:37 -0400

    Finished drafting the intersect utility

--- Sources/Workspace/Intersect.swift
+++ Sources/Workspace/Intersect.swift
@@ -19,12 +19,28 @@ func createIntersectUtility() -> String {
     float3 rayDirection;
   };
   
-  // WARNING: Set the fourth lane of the 'atom' vector to its radius.
+  // For now, the 'atom' vector contains its atomic number in the 4th lane. In
+  // the future, this will become the radius.
   void intersect(\(resultArgument()),
                  IntersectionQuery query,
-                 float4 atom)
+                 float4 atom,
+                 uint atomID)
   {
-  
+    float3 oc = query.rayOrigin - atom.xyz;
+    float b2 = dot(float3(oc), query.rayDirection);
+    
+    uint atomicNumber = uint(atom[3]);
+    float radius = atomRadii[atomicNumber];
+    float c = dot(oc, oc) - radius * radius;
+    
+    float disc4 = b2 * b2 - c;
+    if (disc4 > 0) {
+      float distance = -disc4 * rsqrt(disc4) - b2;
+      if (distance >= 0 && distance < result.distance) {
+        result.atomID = atomID;
+        result.distance = distance;
+      }
+    }
   }
   """
 }
