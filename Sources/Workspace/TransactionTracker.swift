a152cbc54c900197e5f30069d2b7234f211f8c23
Philip Turner <philipturner.AR@gmail.com>
2025-09-26 17:13:20 -0400

    Some drafting

--- Sources/Workspace/TransactionTracker.swift
+++ Sources/Workspace/TransactionTracker.swift
@@ -12,4 +12,47 @@ struct TransactionTracker {
     self.positions = Array(repeating: .zero, count: atomCount)
     self.occupied = Array(repeating: false, count: atomCount)
   }
+  
+  mutating func register(transaction: Atoms.Transaction) {
+    for i in transaction.removedIDs.indices {
+      let atomID = transaction.removedIDs[i]
+      guard atomID < atomCount else {
+        fatalError("Out of bounds memory access.")
+      }
+      
+      // TODO: work
+    }
+    
+    guard transaction.movedIDs.count ==
+            transaction.movedPositions.count else {
+      fatalError("Incorrect array sizes.")
+    }
+    
+    for i in transaction.movedIDs.indices {
+      let atomID = transaction.movedIDs[i]
+      guard atomID < atomCount else {
+        fatalError("Out of bounds memory access.")
+      }
+      
+      // TODO: work
+    }
+    
+    guard transaction.addedIDs.count ==
+            transaction.addedPositions.count else {
+      fatalError("Incorrect array sizes.")
+    }
+    
+    for i in transaction.addedIDs.indices {
+      let atomID = transaction.addedIDs[i]
+      guard atomID < atomCount else {
+        fatalError("Out of bounds memory access.")
+      }
+      
+      // TODO: work
+    }
+  }
+  
+  func compactedAtoms() -> [SIMD4<Float>] {
+    fatalError("Not implemented.")
+  }
 }
