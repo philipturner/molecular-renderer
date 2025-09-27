1bbf3d29fe86b573df7af0c21b463fcbeca4475a
Philip Turner <philipturner.AR@gmail.com>
2025-09-27 16:55:33 -0400

    Save progress

--- Sources/MolecularRenderer/Application/Application+Render.swift
+++ Sources/MolecularRenderer/Application/Application+Render.swift
@@ -1,5 +1,30 @@
 import func Foundation.tan
 
+#if os(Windows)
+import SwiftCOM
+import WinSDK
+#endif
+
+#if os(Windows)
+private func renderUAVBarrier(
+  resource: SwiftCOM.ID3D12Resource
+) -> D3D12_RESOURCE_BARRIER {
+  // Specify the type of barrier.
+  var barrier = D3D12_RESOURCE_BARRIER()
+  barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_UAV
+  barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE
+  
+  // Specify the UAV barrier's parameters.
+  try! resource.perform(
+    as: WinSDK.ID3D12Resource.self
+  ) { pUnk in
+    barrier.UAV.pResource = pUnk
+  }
+  
+  return barrier
+}
+#endif
+
 extension Application {
   public func render() -> Image {
     let transaction = atoms.registerChanges()
@@ -64,6 +89,24 @@ extension Application {
         }
         commandList.dispatch(groups: createGroupCount32())
       }
+      
+      #if os(Windows)
+      // Ensure the output texture(s) are fully written before future
+      // operations.
+      if renderTarget.upscaleFactor > 1 {
+        let colorTexture = renderTarget.colorTextures[frameID % 2]
+        let depthTexture = renderTarget.depthTextures[frameID % 2]
+        let motionTexture = renderTarget.motionTextures[frameID % 2]
+        
+        let colorBarrier = renderUAVBarrier(resource: colorTexture)
+        let depthBarrier = renderUAVBarrier(resource: depthTexture)
+        let motionBarrier = renderUAVBarrier(resource: motionTexture)
+        
+        let barriers = [colorBarrier, depthBarrier, motionBarrier]
+        try! commandList.d3d12CommandList.ResourceBarrier(
+          UInt32(barriers.count), barriers)
+      }
+      #endif
     }
     
     var output = Image()
