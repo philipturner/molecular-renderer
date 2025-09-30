f0e636e96366223a1269b593eab09492bdf662b6
Philip Turner <philipturner.AR@gmail.com>
2025-09-30 10:48:03 -0400

    Created the createPhaseCount utility

--- Sources/MolecularRenderer/Upscale/JitterOffset.swift
+++ Sources/MolecularRenderer/Upscale/JitterOffset.swift
@@ -12,6 +12,25 @@ public struct JitterOffsetDescriptor {
 }
 
 public struct JitterOffset {
+  public static func createPhaseCount(upscaleFactor: Float) -> Int {
+    #if os(macOS)
+    return 8 * Int(upscaleFactor) * Int(upscaleFactor)
+    #else
+    let jitterPhaseCount = FFXDescriptor<ffxQueryDescUpscaleGetJitterPhaseCount>()
+    jitterPhaseCount.type = FFX_API_QUERY_DESC_TYPE_UPSCALE_GETJITTERPHASECOUNT
+    jitterPhaseCount.value.renderWidth = 1000
+    jitterPhaseCount.value.displayWidth = 1000 * UInt32(upscaleFactor)
+    
+    let pOutPhaseCount: UnsafeMutablePointer<Int32> = .allocate(capacity: 1)
+    defer { pOutPhaseCount.deallocate() }
+    pOutPhaseCount.pointee = 5
+    jitterPhaseCount.value.pOutPhaseCount = pOutPhaseCount
+    
+    FFXContext.query(descriptor: jitterPhaseCount)
+    return Int(pOutPhaseCount.pointee)
+    #endif
+  }
+  
   public static func create(
     descriptor: JitterOffsetDescriptor
   ) -> SIMD2<Float> {
