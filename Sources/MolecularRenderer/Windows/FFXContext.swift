8ccca714d823d4449a495fa2ab458101ab5d2c04
Philip Turner <philipturner.AR@gmail.com>
2025-09-30 08:49:05 -0400

    Save progress

--- Sources/MolecularRenderer/Windows/FFXContext.swift
+++ Sources/MolecularRenderer/Windows/FFXContext.swift
@@ -27,6 +27,37 @@ public class FFXContext {
     // Allocate the CreateContextDescUpscale.
     let createContext = FFXDescriptor<ffxCreateContextDescUpscale>()
     createContext.type = FFX_API_CREATE_CONTEXT_DESC_TYPE_UPSCALE
+    createContext.value.flags = UInt32(
+      FFX_UPSCALE_ENABLE_DEPTH_INVERTED.rawValue)
+    
+    // Set the texture dimensions.
+    func createFFXDimensions(
+      _ input: SIMD2<Int>
+    ) -> FfxApiDimensions2D {
+      var output = FfxApiDimensions2D()
+      output.width = UInt32(input[0])
+      output.height = UInt32(input[1])
+      return output
+    }
+    do {
+      let renderSize = display.frameBufferSize / Int(upscaleFactor)
+      let upscaleSize = display.frameBufferSize
+      createContext.value.maxRenderSize = createFFXDimensions(renderSize)
+      createContext.value.maxUpscaleSize = createFFXDimensions(upscaleSize)
+    }
+    
+     // Set the callback to crash on all warnings.
+    createContext.value.fpMessage = { type, message in
+      print("[FidelityFX] Encountered message of type \(type).")
+
+      if let message {
+        let string = String(decodingCString: message, as: UTF16.self)
+        print("[FidelityFX] \(string)")
+      } else {
+        print("[FidelityFX] Message was a null pointer.")
+      }
+      fatalError()
+    }
     
     // TODO: ffxCreateContext
     fatalError("Not implemented.")
