5370872953c5ff36d2606fda4f9c095f85ebc411
Philip Turner <philipturner.AR@gmail.com>
2025-10-02 14:09:48 -0400

    Narrowed down a minimal reproducer for getting this to work

--- install-libraries.sh
+++ install-libraries.sh
@@ -36,14 +36,19 @@ export MINIFORGE_DIR="/Users/philipturner/miniforge3/lib"
 cp "$MINIFORGE_DIR/libOpenMM.dylib" libOpenMM.dylib
 cp "$MINIFORGE_DIR/libOpenMMOpenCL.dylib" libOpenMMOpenCL.dylib
 
+install_name_tool -id "libOpenMM.dylib" libOpenMM.dylib
+install_name_tool -change "@rpath/libc++.1.dylib" "$MINIFORGE_DIR/libc++.1.dylib" libOpenMM.dylib
+
 install_name_tool -id "libOpenMMOpenCL.dylib" libOpenMMOpenCL.dylib
 install_name_tool -change "@rpath/libc++.1.dylib" "$MINIFORGE_DIR/libc++.1.dylib" libOpenMMOpenCL.dylib
 install_name_tool -change "@rpath/libOpenCL.1.dylib" "$MINIFORGE_DIR/libOpenCL.1.dylib" libOpenMMOpenCL.dylib
 install_name_tool -change "@rpath/libOpenMM.dylib" "$(pwd)/libOpenMM.dylib" libOpenMMOpenCL.dylib
 
+codesign -fs - libOpenMM.dylib
 codesign -fs - libOpenMMOpenCL.dylib
 
 echo "These code-signs should report success:"
+codesign --verify --verbose libOpenMM.dylib
 codesign --verify --verbose libOpenMMOpenCL.dylib
 
 
--- run.sh
+++ run.sh
@@ -6,8 +6,3 @@ export OPENMM_PLUGIN_DIR="$(pwd)"
 
 # Run in release mode with incremental compilation.
 swift run -Xswiftc -Ounchecked
-
-export WORKSPACE_PATH="$(pwd)/.build/arm64-apple-macosx/debug/Workspace"
-install_name_tool -change "@rpath/libOpenMM.dylib" "$(pwd)/libOpenMM.dylib" "$WORKSPACE_PATH"
-codesign -fs - "$WORKSPACE_PATH"
-$WORKSPACE_PATH
