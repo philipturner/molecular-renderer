06e9f5f7a17c6c9d06297822f2d258ad01162f21
Philip Turner <philipturner.AR@gmail.com>
2025-10-02 14:05:02 -0400

    This does work

--- install-libraries.sh
+++ install-libraries.sh
@@ -35,3 +35,15 @@
 export MINIFORGE_DIR="/Users/philipturner/miniforge3/lib"
 cp "$MINIFORGE_DIR/libOpenMM.dylib" libOpenMM.dylib
 cp "$MINIFORGE_DIR/libOpenMMOpenCL.dylib" libOpenMMOpenCL.dylib
+
+install_name_tool -id "libOpenMMOpenCL.dylib" libOpenMMOpenCL.dylib
+install_name_tool -change "@rpath/libc++.1.dylib" "$MINIFORGE_DIR/libc++.1.dylib" libOpenMMOpenCL.dylib
+install_name_tool -change "@rpath/libOpenCL.1.dylib" "$MINIFORGE_DIR/libOpenCL.1.dylib" libOpenMMOpenCL.dylib
+install_name_tool -change "@rpath/libOpenMM.dylib" "$(pwd)/libOpenMM.dylib" libOpenMMOpenCL.dylib
+
+codesign -fs - libOpenMMOpenCL.dylib
+
+echo "These code-signs should report success:"
+codesign --verify --verbose libOpenMMOpenCL.dylib
+
+
