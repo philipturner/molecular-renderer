641079d479e5e98f466825453fdd12344a90bc0e
Philip Turner <philipturner.AR@gmail.com>
2025-10-02 16:10:33 -0400

    Created adamantane

--- Sources/Workspace/main.swift
+++ Sources/Workspace/main.swift
@@ -21,7 +21,65 @@ import SwiftCOM
 import WinSDK
 #endif
 
-/*
+// MARK: - Compile Structure
+
+let lattice = Lattice<Cubic> { h, k, l in
+  Bounds { 1 * (h + k + l) }
+  Material { .elemental(.carbon) }
+}
+var reconstruction = Reconstruction()
+reconstruction.atoms = lattice.atoms
+reconstruction.material = .elemental(.carbon)
+var topology = reconstruction.compile()
+print()
+print(topology.atoms.count)
+print(topology.atoms.filter { $0.element == .carbon }.count)
+print(topology.atoms.filter { $0.element == .hydrogen }.count)
+
+func passivate(topology: inout Topology) {
+  func createHydrogen(
+    atomID: UInt32,
+    orbital: SIMD3<Float>
+  ) -> Atom {
+    let atom = topology.atoms[Int(atomID)]
+    
+    var bondLength = atom.element.covalentRadius
+    bondLength += Element.hydrogen.covalentRadius
+    
+    let position = atom.position + bondLength * orbital
+    return Atom(position: position, element: .hydrogen)
+  }
+  
+  let orbitalLists = topology.nonbondingOrbitals()
+  
+  var insertedAtoms: [Atom] = []
+  var insertedBonds: [SIMD2<UInt32>] = []
+  for atomID in topology.atoms.indices {
+    let orbitalList = orbitalLists[atomID]
+    for orbital in orbitalList {
+      let hydrogen = createHydrogen(
+        atomID: UInt32(atomID),
+        orbital: orbital)
+      let hydrogenID = topology.atoms.count + insertedAtoms.count
+      insertedAtoms.append(hydrogen)
+      
+      let bond = SIMD2(
+        UInt32(atomID),
+        UInt32(hydrogenID))
+      insertedBonds.append(bond)
+    }
+  }
+  topology.atoms += insertedAtoms
+  topology.bonds += insertedBonds
+}
+passivate(topology: &topology)
+print()
+print(topology.atoms.count)
+print(topology.atoms.filter { $0.element == .carbon }.count)
+print(topology.atoms.filter { $0.element == .hydrogen }.count)
+
+// MARK: - Launch Application
+
 @MainActor
 func createApplication() -> Application {
   // Set up the device.
@@ -48,107 +106,23 @@ func createApplication() -> Application {
 }
 let application = createApplication()
 
-// State variable to facilitate atom transactions for the animation.
-enum AnimationState {
-  case isopropanol
-  case silane
-}
-var animationState: AnimationState?
-
-func createIsopropanol() -> [SIMD4<Float>] {
-  return [
-    Atom(position: SIMD3( 2.0186, -0.2175,  0.7985) * 0.1, element: .hydrogen),
-    Atom(position: SIMD3( 1.4201, -0.2502, -0.1210) * 0.1, element: .carbon),
-    Atom(position: SIMD3( 1.6783,  0.6389, -0.7114) * 0.1, element: .hydrogen),
-    Atom(position: SIMD3( 1.7345, -1.1325, -0.6927) * 0.1, element: .hydrogen),
-    Atom(position: SIMD3(-0.0726, -0.3145,  0.1833) * 0.1, element: .carbon),
-    Atom(position: SIMD3(-0.2926, -1.2317,  0.7838) * 0.1, element: .hydrogen),
-    Atom(position: SIMD3(-0.3758,  0.8195,  0.9774) * 0.1, element: .oxygen),
-    Atom(position: SIMD3(-1.3159,  0.8236,  1.0972) * 0.1, element: .hydrogen),
-    Atom(position: SIMD3(-0.8901, -0.3435, -1.1071) * 0.1, element: .carbon),
-    Atom(position: SIMD3(-0.7278,  0.5578, -1.7131) * 0.1, element: .hydrogen),
-    Atom(position: SIMD3(-0.6126, -1.2088, -1.7220) * 0.1, element: .hydrogen),
-    Atom(position: SIMD3(-1.9673, -0.4150, -0.9062) * 0.1, element: .hydrogen),
-  ]
-}
-
-func createSilane() -> [SIMD4<Float>] {
-  return [
-    Atom(position: SIMD3( 0.0000,  0.0000,  0.0000) * 0.1, element: .silicon),
-    Atom(position: SIMD3( 0.8544,  0.8544,  0.8544) * 0.1, element: .hydrogen),
-    Atom(position: SIMD3(-0.8544, -0.8544,  0.8544) * 0.1, element: .hydrogen),
-    Atom(position: SIMD3(-0.8544,  0.8544, -0.8544) * 0.1, element: .hydrogen),
-    Atom(position: SIMD3( 0.8544, -0.8544, -0.8544) * 0.1, element: .hydrogen),
-  ]
-}
-
-@MainActor
-func createTime() -> Float {
-  let elapsedFrames = application.clock.frames
-  let frameRate = application.display.frameRate
-  let seconds = Float(elapsedFrames) / Float(frameRate)
-  return seconds
-}
-
 @MainActor
 func modifyAtoms() {
-  // 0.5 Hz rotation rate
-  let time = createTime()
-  let angleDegrees = 0.5 * time * 360
-  let rotation = Quaternion<Float>(
-    angle: Float.pi / 180 * angleDegrees,
-    axis: SIMD3(0, 1, 0))
-  
-  let roundedDownTime = Int((time / 3).rounded(.down))
-  if roundedDownTime % 2 == 0 {
-    let isopropanol = createIsopropanol()
-    if animationState == .silane {
-      for atomID in 12..<17 {
-        application.atoms[atomID] = nil
-      }
-    }
-    
-    animationState = .isopropanol
-    for i in isopropanol.indices {
-      let atomID = 0 + i
-      var atom = isopropanol[i]
-      atom.position = rotation.act(on: atom.position)
-      application.atoms[atomID] = atom
-    }
-  } else {
-    let silane = createSilane()
-    if animationState == .isopropanol {
-      for atomID in 0..<12 {
-        application.atoms[atomID] = nil
-      }
-    }
-    
-    animationState = .silane
-    for i in silane.indices {
-      let atomID = 12 + i
-      var atom = silane[i]
-      atom.position = rotation.act(on: atom.position)
-      application.atoms[atomID] = atom
-    }
+  let atoms = topology.atoms
+  for atomID in atoms.indices {
+    let atom = atoms[atomID]
+    application.atoms[atomID] = atom
   }
 }
 
 @MainActor
 func modifyCamera() {
-  // 0.1 Hz rotation rate
-  let time = createTime()
-  let angleDegrees = 0.1 * time * 360
-  let rotation = Quaternion<Float>(
-    angle: Float.pi / 180 * angleDegrees,
-    axis: SIMD3(-1, 0, 0))
+  application.camera.position = SIMD3(0, 0, 2.00)
   
-  // Place the camera 1.0 nm away from the origin.
-  application.camera.position = rotation.act(on: SIMD3(0, 0, 1.00))
-  
-  application.camera.basis.0 = rotation.act(on: SIMD3(1, 0, 0))
-  application.camera.basis.1 = rotation.act(on: SIMD3(0, 1, 0))
-  application.camera.basis.2 = rotation.act(on: SIMD3(0, 0, 1))
-  application.camera.fovAngleVertical = Float.pi / 180 * 40
+  application.camera.basis.0 = SIMD3(1, 0, 0)
+  application.camera.basis.1 = SIMD3(0, 1, 0)
+  application.camera.basis.2 = SIMD3(0, 0, 1)
+  application.camera.fovAngleVertical = Float.pi / 180 * 60
 }
 
 // Enter the run loop.
@@ -160,4 +134,3 @@ application.run {
   image = application.upscale(image: image)
   application.present(image: image)
 }
-*/
