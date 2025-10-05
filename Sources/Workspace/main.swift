cc574ad42c7506e01ecc80aed9f796c742dd1935
Philip Turner <philipturner.AR@gmail.com>
2025-10-05 10:53:21 -0400

    There was something very wrong with this

--- Sources/Workspace/main.swift
+++ Sources/Workspace/main.swift
@@ -6,7 +6,7 @@ import xTB
 
 let system: [Atom] = [
   Atom(position: SIMD3(0.000, 0.000, 0.000), element: .nitrogen),
-  Atom(position: SIMD3(1.090, 0.000, 0.000), element: .nitrogen),
+  Atom(position: SIMD3(0.109, 0.000, 0.000), element: .nitrogen),
 ]
 
 // Create the calculator.
@@ -20,30 +20,35 @@ let calculator = xTB_Calculator(descriptor: calculatorDesc)
 calculator.molecule.positions = system.map(\.position)
 
 // Query the energy.
-do {
-  let expectedEnergy: Double = -22755.932
-  let actualEnergy: Double = calculator.energy
-  guard (expectedEnergy - actualEnergy).magnitude < 0.001 else {
-    fatalError("Calculator returned unexpected energy: \(actualEnergy) zJ")
-  }
-}
+// do {
+//   let expectedEnergy: Double = -22755.932
+//   let actualEnergy: Double = calculator.energy
+//   guard (expectedEnergy - actualEnergy).magnitude < 0.001 else {
+//     fatalError("Calculator returned unexpected energy: \(actualEnergy) zJ")
+//   }
+// }
+print(calculator.energy)
 
 // Survey the potential energy curve.
 let candidateBondLengths: [Float] = [
-  1.00, 1.10, 1.20
+  0.90, 1.00, 1.10, 1.20, 1.30
 ]
-var candidateEnergies: [Float] = []
+var candidateEnergies: [Double] = []
 for bondLength in candidateBondLengths {
   let position = SIMD3<Float>(bondLength, 0, 0)
   calculator.molecule.positions[1] = position
   
-  //let energy = calculator.energy
-  //candidateEnergies.append(energy)
+  let energy = calculator.energy
+  candidateEnergies.append(energy)
 }
 for i in candidateBondLengths.indices {
   let bondLength = candidateBondLengths[i]
   let bondLengthRepr = String(format: "%.3f", bondLength)
-  print(bondLengthRepr)
+  
+  let energy = candidateEnergies[i]
+  let energyRepr = String(format: "%.1f", energy)
+  
+  print(bondLength, "nm", "-", energyRepr, "zJ")
 }
 
 // TODO: Numerically calculate the force, compare to analytical force.
