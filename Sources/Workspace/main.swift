b4e06f7e3915e0190386593139ce1f1d07b30c4f
Philip Turner <philipturner.AR@gmail.com>
2025-10-05 10:43:58 -0400

    Save progress

--- Sources/Workspace/main.swift
+++ Sources/Workspace/main.swift
@@ -27,3 +27,23 @@ do {
     fatalError("Calculator returned unexpected energy: \(actualEnergy) zJ")
   }
 }
+
+// Survey the potential energy curve.
+let candidateBondLengths: [Float] = [
+  1.00, 1.10, 1.20
+]
+var candidateEnergies: [Float] = []
+for bondLength in candidateBondLengths {
+  let position = SIMD3<Float>(bondLength, 0, 0)
+  calculator.molecule.positions[1] = position
+  
+  //let energy = calculator.energy
+  //candidateEnergies.append(energy)
+}
+for i in candidateBondLengths.indices {
+  let bondLength = candidateBondLengths[i]
+  let bondLengthRepr = String(format: "%.3f", bondLength)
+  print(bondLengthRepr)
+}
+
+// TODO: Numerically calculate the force, compare to analytical force.
