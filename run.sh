ca7215ea8b0bc1dcfda930108e5687188e72f48c
Philip Turner <philipturner.AR@gmail.com>
2025-10-05 09:45:22 -0400

    Don't forget these lines (exclamation)

--- run.sh
+++ run.sh
@@ -4,5 +4,13 @@ export MTL_HUD_ENABLED=1
 # Redirect the default OpenMM plugins directory.
 export OPENMM_PLUGIN_DIR="$(pwd)"
 
+# Prevent crashes with large systems.
+export OMP_STACKSIZE="2G"
+
+# Only use the performance cores on macOS.
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  export OMP_NUM_THREADS=$(sysctl -n hw.perflevel0.physicalcpu)
+fi
+
 # Run in release mode with incremental compilation.
 swift run -Xswiftc -Ounchecked
