38ed9ad9433a12e2df46ddab8653412a5e5f67c9
Philip Turner <philipturner.AR@gmail.com>
2025-10-09 15:43:07 -0400

    Move atom styles to the top level directory

--- Sources/MolecularRenderer/BVH/Shaders/AddProcess.swift
+++ Sources/MolecularRenderer/BVH/Shaders/AddProcess.swift
@@ -1,2 +1,41 @@
 // First step: update the positions and motion vectors in the address space
 // in the first kernel
+struct AddProcess {
+  static func createSource1() -> String {
+    func importStandardLibrary() -> String {
+      #if os(macOS)
+      """
+      #include <metal_stdlib>
+      using namespace metal;
+      """
+      #else
+      ""
+      #endif
+    }
+    
+    func functionSignature() -> String {
+      #if os(macOS)
+      """
+      kernel void addProcess1(
+        constant TransactionArgs &transactionArgs [[buffer(0)]],
+        device uint *transactionIDs [[buffer(1)]],
+        device float4 *transactionAtoms [[buffer(2)]])
+      """
+      #else
+      
+      #endif
+    }
+    
+    return """
+    \(importStandardLibrary())
+    
+    \(AtomStyles.createAtomRadii(AtomStyles.radii))
+    \(TransactionArgs.shaderDeclaration)
+    
+    \(functionSignature())
+    {
+      
+    }
+    """
+  }
+}
