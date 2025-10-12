bb030290bbb218b0e061841a11c3e1845b6c02a6
Philip Turner <philipturner.AR@gmail.com>
2025-10-12 10:27:56 -0400

    Save progress

--- Sources/MolecularRenderer/Image/Render/DDA.swift
+++ Sources/MolecularRenderer/Image/Render/DDA.swift
@@ -36,6 +36,44 @@ func createDDAUtility(worldDimension: Float) -> String {
       \(Shader.select("ceil(cellBorder)", "floor(cellBorder)", "dtdx >= 0"));
       cellBorder *= 0.25;
     }
+    
+    float3 cellLowerCorner(float3 cellBorder) {
+      float3 output = cellBorder;
+      output += \(Shader.select("dx", float3(0), "dtdx >= 0"));
+      return output;
+    }
+    
+    float3 nextTimes(float3 cellBorder, float3 rayOrigin) {
+      float3 nextBorder = cellBorder + dx;
+      float3 nextTimes = (nextBorder - rayOrigin) * dtdx;
+      return nextTimes;
+    }
+    
+    float3 nextBorder(float3 cellBorder, float3 nextTimes) {
+      float3 output = cellBorder;
+      if (nextTimes[0] < nextTimes[1] &&
+          nextTimes[0] < nextTimes[2]) {
+        output[0] += dx[0];
+      } else if (nextTimes[1] < nextTimes[2]) {
+        output[1] += dx[1];
+      } else {
+        output[2] += dx[2];
+      }
+      return output;
+    }
+    
+    float voxelMaximumHitTime(float3 cellBorder, float3 nextTimes) {
+      float smallestNextTime;
+      if (nextTimes[0] < nextTimes[1] &&
+          nextTimes[0] < nextTimes[2]) {
+        smallestNextTime = nextTimes[0];
+      } else if (nextTimes[1] < nextTimes[2]) {
+        smallestNextTime = nextTimes[1];
+      } else {
+        smallestNextTime = nextTimes[2];
+      }
+      return smallestNextTime;
+    }
   };
   """
 }
