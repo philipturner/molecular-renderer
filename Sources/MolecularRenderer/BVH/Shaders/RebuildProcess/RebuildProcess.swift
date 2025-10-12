b76bf9bf1077a815f5d21adaf23f080e89a61188
Philip Turner <philipturner.AR@gmail.com>
2025-10-12 15:36:41 -0400

    Check that all of these shaders compile on macOS

--- Sources/MolecularRenderer/BVH/Shaders/RebuildProcess/RebuildProcess.swift
+++ Sources/MolecularRenderer/BVH/Shaders/RebuildProcess/RebuildProcess.swift
@@ -1,5 +1,14 @@
 class RebuildProcess {
+  let process1: Shader
+  
   init(device: Device, worldDimension: Float) {
+    var shaderDesc = ShaderDescriptor()
+    shaderDesc.device = device
     
+    shaderDesc.name = "rebuildProcess1"
+    shaderDesc.threadsPerGroup = SIMD3(4, 4, 4)
+    shaderDesc.source = Self.createSource1(
+      worldDimension: worldDimension)
+    self.process1 = Shader(descriptor: shaderDesc)
   }
 }
