d9e6b5ee1c8f20c43f99ddb6dc33351f0b6e977d
Philip Turner <philipturner.AR@gmail.com>
2025-10-12 08:47:46 -0400

    Save progress

--- Sources/MolecularRenderer/BVH/Shaders/RebuildProcess.swift
+++ Sources/MolecularRenderer/BVH/Shaders/RebuildProcess.swift
@@ -19,13 +19,27 @@ struct RebuildProcess {
   //
   // # Phase I
   //
+  // loop over the cuboid bounding box of each atom
+  // atomically accumulate into threadgroupCounters
+  //
   // # Phase II
   //
+  // prefix sum over 512 small voxels (SIMD + group reduction)
+  //   read 4 voxels in a single instruction, on 128 threads in parallel
+  //   save the prefix sum result for Phase IV
   // if reference count is too large, crash w/ diagnostic info
+  // write reference count into memory slot header
   //
   // # Phase III
   //
+  // loop over a 3x3x3 grid of small voxels for each atom
+  // run the cube-sphere test and mask out voxels outside the 2 nm bound
+  // atomically accumulate into threadgroupCounters
+  //
+  //
   // # Phase IV
+  //
+  // restore the prefix sum result
   func createSource2() -> String {
     fatalError("Not implemented.")
   }
