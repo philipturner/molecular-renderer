9d2ae5e533d528462888f0e553acfcd8d5db70ea
Philip Turner <philipturner.AR@gmail.com>
2025-10-13 16:04:24 -0400

    Save progress

--- Sources/MolecularRenderer/BVH/Crash/CrashInfo.swift
+++ Sources/MolecularRenderer/BVH/Crash/CrashInfo.swift
@@ -1,6 +1,7 @@
 struct CrashInfoDescriptor {
   var bufferContents: [UInt32]?
-  var clockTime: Float?
+  var clockFrames: Int?
+  var displayFrameRate: Int?
   var frameID: Int?
   var memorySlotCount: Int?
   var worldDimension: Float?
@@ -9,7 +10,8 @@ struct CrashInfoDescriptor {
 class CrashInfo {
   init(descriptor: CrashInfoDescriptor) {
     guard let bufferContents = descriptor.bufferContents,
-          let clockTime = descriptor.clockTime,
+          let clockFrames = descriptor.clockFrames,
+          let displayFrameRate = descriptor.displayFrameRate,
           let frameID = descriptor.frameID,
           let memorySlotCount = descriptor.memorySlotCount,
           let worldDimension = descriptor.worldDimension else {
@@ -19,5 +21,20 @@ class CrashInfo {
     // Diagnose the wall clock time when the error was registered.
     // Diagnose the frame when it was registered.
     // Diagnose the frame when it was thrown.
+    // Approximate the wall time when the error was thrown.
+  }
+  
+  var message: String {
+    return """
+    Error thrown in shader code.
+    
+    TODO
+    TODO
+    TODO
+    
+    TODO
+    TODO
+    TODO
+    """
   }
 }
