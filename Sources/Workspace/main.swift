5af915005cac9d3969cddd01ad62f266ff7d6003
Philip Turner <philipturner.AR@gmail.com>
2025-10-18 22:32:30 -0400

    Save progress

--- Sources/Workspace/main.swift
+++ Sources/Workspace/main.swift
@@ -4,6 +4,8 @@ import MolecularRenderer
 import QuaternionModule
 
 // Remaining tasks of this PR:
+// - Take a picture of the Google Sheet results and embed it in documentation
+//   for the long distances test.
 // - Implement the "critical pixel count" heuristic to optimize AO cost.
 //   - Just get it over and done with first.
 // - Implement 32 nm scoping to further optimize the per-dense-voxel cost.
