6bf34848b9f6fe0bbfdd49b3b2ec0f19b2fc0460
Philip Turner <philipturner.AR@gmail.com>
2025-10-20 12:45:55 -0400

    Drafted the basis vectors

--- Sources/Workspace/main.swift
+++ Sources/Workspace/main.swift
@@ -10,3 +10,29 @@ import MolecularRenderer
 //   - Use a common lattice dimension for all structures. Start with a small
 //     number, then scale it when other components of the test are working.
 // - Clean up the documentation and implement the remaining tests.
+
+#if false
+// Drafting the (111) basis vectors.
+let basisX = SIMD3<Float>(1, 0, -1) / Float(2).squareRoot()
+let basisY = SIMD3<Float>(-1, 2, -1) / Float(6).squareRoot()
+let basisZ = SIMD3<Float>(1, 1, 1) / Float(3).squareRoot()
+print((basisX * basisX).sum())
+print((basisY * basisY).sum())
+print((basisZ * basisZ).sum())
+print((basisX * basisY).sum())
+print((basisY * basisZ).sum())
+print((basisZ * basisX).sum())
+#endif
+
+#if false
+// Drafting the (110) basis vectors.
+let basisX = SIMD3<Float>(1, 0, 0) / Float(1).squareRoot()
+let basisY = SIMD3<Float>(0, 1, -1) / Float(2).squareRoot()
+let basisZ = SIMD3<Float>(0, 1, 1) / Float(2).squareRoot()
+print((basisX * basisX).sum())
+print((basisY * basisY).sum())
+print((basisZ * basisZ).sum())
+print((basisX * basisY).sum())
+print((basisY * basisZ).sum())
+print((basisZ * basisX).sum())
+#endif
