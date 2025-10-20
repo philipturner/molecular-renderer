cc69ef07d708156b4bf281c0907f8535e33e22a2
Philip Turner <philipturner.AR@gmail.com>
2025-10-20 08:01:49 -0400

    Save progress

--- Sources/MolecularRenderer/BVH/Shaders/DispatchVoxelGroups.swift
+++ Sources/MolecularRenderer/BVH/Shaders/DispatchVoxelGroups.swift
@@ -10,9 +10,9 @@ struct DispatchVoxelGroups {
   // - resetIdle
   //
   // scan for marked voxel groups
-  // create a compact list using the base coordinate in number of 2 nm voxels
+  // create a compact list of cooordinates in grid of 8 nm voxels
   // global counter is the indirect dispatch argument
-  // write to dispatchedVoxelCoords
+  // write to dispatchedCoords8
   static func createSource(worldDimension: Float) -> String {
     func functionSignature() -> String {
       #if os(macOS)
@@ -22,7 +22,7 @@ struct DispatchVoxelGroups {
         device uint *marks1 [[buffer(1)]],
         device uint *marks2 [[buffer(2)]],
         device uint *marks3 [[buffer(3)]],
-        device uint *dispatchedVoxelCoords [[buffer(4)]],
+        device uint *dispatchedCoords8 [[buffer(4)]],
         uint3 globalID [[thread_position_in_grid]],
         uint3 groupID [[threadgroup_position_in_grid]])
       """
@@ -32,9 +32,35 @@ struct DispatchVoxelGroups {
       RWStructuredBuffer<uint> marks1 : register(u1);
       RWStructuredBuffer<uint> marks2 : register(u2);
       RWStructuredBuffer<uint> marks3 : register(u3);
-      RWStructuredBuffer<uint>
+      RWStructuredBuffer<uint> dispatchedCoords : register(u4);
+      
+      [numthreads(4, 4, 4)]
+      [RootSignature(
+        \(CrashBuffer.rootSignatureArguments)
+        "UAV(u1),"
+        "UAV(u2),"
+        "UAV(u3),"
+        "UAV(u4),"
+      )]
+      void dispatchVoxelCoords(
+        uint3 globalID : SV_DispatchThreadID,
+        uint3 groupID : SV_GroupID)
       """
       #endif
     }
+    
+    return """
+    \(Shader.importStandardLibrary)
+    
+    \(functionSignature())
+    {
+      if (crashBuffer[0] != 1) {
+        return;
+      }
+      
+      uint voxelGroup32ID =
+      \(VoxelResources.)
+    }
+    """
   }
 }
