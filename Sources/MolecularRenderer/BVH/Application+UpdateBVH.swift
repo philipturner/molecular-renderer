77b0dfec60e3c0f183df8405080032ace013ffc4
Philip Turner <philipturner.AR@gmail.com>
2025-10-20 15:10:04 -0400

    Found a glitch in the code

--- Sources/MolecularRenderer/BVH/Application+UpdateBVH.swift
+++ Sources/MolecularRenderer/BVH/Application+UpdateBVH.swift
@@ -111,7 +111,7 @@ extension Application {
       Self.upscaleMeter.integrate(upscaleLatency)
       
       // TODO Remove the following code before merging the PR.
-      #if true
+      #if false
       print(
         PerformanceMeter.pad(updateLatency),
         PerformanceMeter.pad(renderLatency),
