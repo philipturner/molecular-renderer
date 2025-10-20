37b46c1f074af3c9d50b9b7ec790cd5d4837f5db
Philip Turner <philipturner.AR@gmail.com>
2025-10-20 16:03:18 -0400

    Decided on a lattice size

--- Sources/Workspace/main.swift
+++ Sources/Workspace/main.swift
@@ -3,26 +3,16 @@ import MolecularRenderer
 
 // Remaining tasks of this PR:
 // - Work on setting up the large scene test.
-//   - Prepare compiled structures
 //   - Measure the largest spatial distance of any atom from the lattice's
 //     center, then prepare a conservative bounding box for the two cases.
+//   - Create a method for generating a random rotational basis.
 //   - Dry run the loading process. A cube almost at the world's dimension
 //     limits, and a hollow sphere inside with a specified radius. Both the
 //     cube side length and sphere radius are specified independently.
 //   - Find a good data distribution between world limit, percentage of
 //     interior volume open to viewing, and atom count.
 
-import HDL
-import MolecularRenderer
-import QuaternionModule
-
-let crossThickness: Int = 16
-let crossSize: Int = 120
-let beamDepth: Int = 50 // 16 is reproducer for a bug, originally 10
-let actualWorldDimension: Float = 96
-let paddedWorldDimension: Float = 128
-
-// MARK: - Compile Structures
+let latticeSize: Float = 23
 
 func passivate(topology: inout Topology) {
   func createHydrogen(
@@ -61,120 +51,6 @@ func passivate(topology: inout Topology) {
   topology.bonds += insertedBonds
 }
 
-func createCross() -> Topology {
-  let lattice = Lattice<Cubic> { h, k, l in
-    Bounds {
-      Float(crossSize) * h +
-      Float(crossSize) * k +
-      Float(2) * l
-    }
-    Material { .checkerboard(.silicon, .carbon) }
-    
-    for isPositiveX in [false, true] {
-      for isPositiveY in [false, true] {
-        let halfSize = Float(crossSize) / 2
-        let center = halfSize * h + halfSize * k
-        
-        let directionX = isPositiveX ? h : -h
-        let directionY = isPositiveY ? k : -k
-        let halfThickness = Float(crossThickness) / 2
-        
-        Volume {
-          Concave {
-            Convex {
-              Origin { center + halfThickness * directionX }
-              Plane { isPositiveX ? h : -h }
-            }
-            Convex {
-              Origin { center + halfThickness * directionY }
-              Plane { isPositiveY ? k : -k }
-            }
-          }
-          Replace { .empty }
-        }
-      }
-    }
-  }
-  
-  var reconstruction = Reconstruction()
-  reconstruction.atoms = lattice.atoms
-  reconstruction.material = .checkerboard(.silicon, .carbon)
-  var topology = reconstruction.compile()
-  passivate(topology: &topology)
-  
-  for atomID in topology.atoms.indices {
-    var atom = topology.atoms[atomID]
-    
-    // This offset captures just one Si and one C for each unit cell on the
-    // (001) surface. By capture, I mean that atom.position.z > 0. We want a
-    // small number of static atoms in a 2 nm voxel that overlaps some moving
-    // atoms.
-    atom.position += SIMD3(0, 0, -0.800)
-    
-    // Shift the origin to allow larger beam depth, with fixed world dimension.
-    atom.position.z -= actualWorldDimension / 2
-    atom.position.z += 8
-    
-    // Shift so the structure is centered in X and Y.
-    let latticeConstant = Constant(.square) {
-      .checkerboard(.silicon, .carbon)
-    }
-    let halfSize = Float(crossSize) / 2
-    atom.position.x -= halfSize * latticeConstant
-    atom.position.y -= halfSize * latticeConstant
-    
-    topology.atoms[atomID] = atom
-  }
-  
-  return topology
-}
-
-func createBeam() -> Topology {
-  let lattice = Lattice<Cubic> { h, k, l in
-    Bounds {
-      Float(crossThickness) * h +
-      Float(crossSize) * k +
-      Float(beamDepth) * l
-    }
-    Material { .checkerboard(.silicon, .carbon) }
-  }
-  
-  var reconstruction = Reconstruction()
-  reconstruction.atoms = lattice.atoms
-  reconstruction.material = .checkerboard(.silicon, .carbon)
-  var topology = reconstruction.compile()
-  passivate(topology: &topology)
-  
-  for atomID in topology.atoms.indices {
-    var atom = topology.atoms[atomID]
-    
-    // Capture just one Si and one C for each unit cell. This time, capturing
-    // happens if atom.position.z < 0.
-    atom.position += SIMD3(0, 0, -0.090)
-    
-    // Shift so both captured surfaces fall in the [0, 2] nm range for sharing
-    // a voxel.
-    atom.position.z += 2
-    
-    // Shift the origin to allow larger beam depth, with fixed world dimension.
-    atom.position.z -= actualWorldDimension / 2
-    atom.position.z += 8
-    
-    // Shift so the structure is centered in X and Y.
-    let latticeConstant = Constant(.square) {
-      .checkerboard(.silicon, .carbon)
-    }
-    let halfThickness = Float(crossThickness) / 2
-    let halfSize = Float(crossSize) / 2
-    atom.position.x -= halfThickness * latticeConstant
-    atom.position.y -= halfSize * latticeConstant
-    
-    topology.atoms[atomID] = atom
-  }
-  
-  return topology
-}
-
 func analyze(topology: Topology) {
   print()
   print("atom count:", topology.atoms.count)
@@ -191,104 +67,24 @@ func analyze(topology: Topology) {
   }
 }
 
-let cross = createCross()
-let beam = createBeam()
-analyze(topology: cross)
-analyze(topology: beam)
-
-// MARK: - Rotation Animation
-
-@MainActor
-func createRotatedBeam(frameID: Int) -> Topology {
-  // 0.5 Hz -> 3 degrees/frame @ 60 Hz
-  //
-  // WARNING: Systems with different display refresh rates may have different
-  // benchmark results. The benchmark should be robust to this variation in
-  // degrees/frame.
-  //
-  // Solution: animate by clock.frames instead of the actual time. On 120 Hz
-  // systems, the benchmark will rotate 2x faster than on 60 Hz systems.
-  let angleDegrees: Float = 3 * Float(frameID)
-  let rotation = Quaternion<Float>(
-    angle: angleDegrees * Float.pi / 180,
-    axis: SIMD3(0, 0, 1))
-  
-  // Circumvent a massive CPU-side bottleneck from 'rotation.act()'.
-  let basis0 = rotation.act(on: SIMD3<Float>(1, 0, 0))
-  let basis1 = rotation.act(on: SIMD3<Float>(0, 1, 0))
-  let basis2 = rotation.act(on: SIMD3<Float>(0, 0, 1))
-  
-  var topology = beam
-  for atomID in topology.atoms.indices {
-    var atom = topology.atoms[atomID]
+func createTopology() -> Topology {
+  let lattice = Lattice<Cubic> { h, k, l in
+    Bounds { latticeSize * (h + k + l) }
     
-    var rotatedPosition: SIMD3<Float> = .zero
-    rotatedPosition += basis0 * atom.position[0]
-    rotatedPosition += basis1 * atom.position[1]
-    rotatedPosition += basis2 * atom.position[2]
-    atom.position = rotatedPosition
-    
-    topology.atoms[atomID] = atom
+    // Invert the atom ordering from previous tests in this repo. Now the
+    // silicon atoms are shown on the surface. It's more ugly, but expands the
+    // test coverage.
+    Material { .checkerboard(.silicon, .carbon) }
   }
+  
+  var reconstruction = Reconstruction()
+  reconstruction.atoms = lattice.atoms
+  reconstruction.material = .checkerboard(.silicon, .carbon)
+  var topology = reconstruction.compile()
+  passivate(topology: &topology)
   
   return topology
 }
 
-// MARK: - Launch Application
-
-@MainActor
-func createApplication() -> Application {
-  // Set up the device.
-  var deviceDesc = DeviceDescriptor()
-  deviceDesc.deviceID = Device.fastestDeviceID
-  let device = Device(descriptor: deviceDesc)
-  
-  // Set up the display.
-  var displayDesc = DisplayDescriptor()
-  displayDesc.device = device
-  displayDesc.frameBufferSize = SIMD2<Int>(1080, 1080)
-  displayDesc.monitorID = device.fastestMonitorID
-  let display = Display(descriptor: displayDesc)
-  
-  // Set up the application.
-  var applicationDesc = ApplicationDescriptor()
-  applicationDesc.device = device
-  applicationDesc.display = display
-  applicationDesc.upscaleFactor = 1
-  
-  applicationDesc.addressSpaceSize = 4_000_000
-  applicationDesc.voxelAllocationSize = 500_000_000
-  applicationDesc.worldDimension = paddedWorldDimension
-  let application = Application(descriptor: applicationDesc)
-  
-  return application
-}
-let application = createApplication()
-
-for atomID in cross.atoms.indices {
-  let atom = cross.atoms[atomID]
-  application.atoms[atomID] = atom
-}
-
-@MainActor
-func addRotatedBeam(frameID: Int) {
-  let rotatedBeam = createRotatedBeam(frameID: frameID)
-  let offset = cross.atoms.count
-  
-  // Circumvent a massive CPU-side bottleneck from @MainActor referencing to
-  // 'application' from the global scope.
-  let applicationCopy = application
-  
-  for atomID in rotatedBeam.atoms.indices {
-    let atom = rotatedBeam.atoms[atomID]
-    applicationCopy.atoms[offset + atomID] = atom
-  }
-}
-
-application.run {
-  addRotatedBeam(frameID: application.clock.frames)
-  application.camera.position = SIMD3(0, 0, (actualWorldDimension / 2) - 8)
-  
-  let image = application.render()
-  application.present(image: image)
-}
+let topology = createTopology()
+analyze(topology: topology)
