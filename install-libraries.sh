be71f440d57dbead8a61d64a4496594c451f7db5
Philip Turner <philipturner.AR@gmail.com>
2025-11-17 15:07:31 -0500

    Save progress

--- install-libraries.sh
+++ install-libraries.sh
@@ -14,20 +14,31 @@ cp -r "/Users/philipturner/Desktop/xtb-macos" "xtb-macos"
 
 cd ../ # balance 'cd .build'
 
+## Establish the vendors directory.
+
+mkdir .build/vendors
+cp ".build/openmm-macos/vendors/apple.icd" ".build/vendors/apple.icd"
+
+## Copy binaries into the package folder.
+
 cp ".build/openmm-macos/libc++.1.dylib" libc++.1.dylib
 cp ".build/openmm-macos/libOpenCL.1.dylib" libOpenCL.1.dylib
 cp ".build/openmm-macos/libOpenMM.dylib" libOpenMM.dylib
 cp ".build/openmm-macos/libOpenMMOpenCL.dylib" libOpenMMOpenCL.dylib
+cp ".build/openmm-macos/libocl_icd_wrapper_apple.dylib" libocl_icd_wrapper_apple.dylib
 cp ".build/xtb-macos/libxtb.dylib" libxtb.dylib
+
+## Fix the symbolic links.
 
 install_name_tool -id "libc++.1.dylib" libc++.1.dylib
 install_name_tool -id "libOpenCL.1.dylib" libOpenCL.1.dylib
 install_name_tool -id "libOpenMM.dylib" libOpenMM.dylib
 install_name_tool -id "libOpenMMOpenCL.dylib" libOpenMMOpenCL.dylib
+install_name_tool -id "libocl_icd_wrapper_apple.dylib" libocl_icd_wrapper_apple.dylib
 
-# Technically not needed, but prevents an issue where macOS flags it as a virus
-# when copying the binary from an external folder without unzipping the source
-# during the script's execution.
+# Technically not needed, but prevents an issue where macOS flags libxtb as a
+# virus when copying the binary from an external folder without unzipping the
+# source during the script's execution.
 install_name_tool -id "libxtb.dylib" libxtb.dylib
 
 install_name_tool -change "@rpath/libc++.1.dylib" "$(pwd)/libc++.1.dylib" libOpenMM.dylib
@@ -35,10 +46,13 @@ install_name_tool -change "@rpath/libc++.1.dylib" "$(pwd)/libc++.1.dylib" libOpe
 install_name_tool -change "@rpath/libOpenCL.1.dylib" "$(pwd)/libOpenCL.1.dylib" libOpenMMOpenCL.dylib
 install_name_tool -change "@rpath/libOpenMM.dylib" "$(pwd)/libOpenMM.dylib" libOpenMMOpenCL.dylib
 
+## Repair the code signature.
+
 codesign -fs - libc++.1.dylib
 codesign -fs - libOpenCL.1.dylib
 codesign -fs - libOpenMM.dylib
 codesign -fs - libOpenMMOpenCL.dylib
+codesign -fs - libocl_icd_wrapper_apple.dylib
 codesign -fs - libxtb.dylib
 
 echo "These code-signs should report success:"
@@ -46,4 +60,5 @@ codesign --verify --verbose libc++.1.dylib
 codesign --verify --verbose libOpenCL.1.dylib
 codesign --verify --verbose libOpenMM.dylib
 codesign --verify --verbose libOpenMMOpenCL.dylib
+codesign --verify --verbose libocl_icd_wrapper_apple.dylib
 codesign --verify --verbose libxtb.dylib
