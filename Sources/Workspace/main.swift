69988552940b8d2de09594727c3aee95c3136040
Philip Turner <philipturner.AR@gmail.com>
2025-11-17 19:31:12 -0500

    Fix the main file

--- Sources/Workspace/main.swift
+++ Sources/Workspace/main.swift
@@ -1,21 +1,7 @@
-import OpenMM
+import HDL
+import MM4
+import MolecularRenderer
+import QuaternionModule
+import xTB
 
-let pluginsDirectory = OpenMM_Platform.defaultPluginsDirectory
-guard let pluginsDirectory else {
-  fatalError("Could not find the OpenMM plugins directory.")
-}
-print("default plugins directory:", pluginsDirectory)
-
-OpenMM_Platform.loadPlugins(directory: pluginsDirectory)
-
-let platforms = OpenMM_Platform.platforms
-print(platforms.count)
-for platform in platforms {
-  print(platform.name)
-}
-
-try! TightBindingTests().testDiamondSystem122()
-try! TightBindingTests().testDiamondSystem222()
-try! ForceFieldTests().testDiamondSystem122()
-try! ForceFieldTests().testDiamondSystem222()
-try! ForceFieldTests().testDiamondSystem233()
+print("Hello, world.")
